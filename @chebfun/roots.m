63112bc62583b340e9e4fcc22c5247dc8da9c196
ricp <ricp@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2007-10-17 17:09:53 +0000

    Fixed the bug in roots: now it can locate roots at endpoints.

--- @chebfun/roots.m
+++ @chebfun/roots.m
@@ -8,9 +8,17 @@ ends = f.ends;
 F = [];
 for i = 1:nfuns
     a = ends(i); b = ends(i+1);
-    r = introots(f.funs{i});
+    lfun = f.funs{i};
+    r = introots(lfun);
     if ~isempty(r)
         F = [F;scale(r,a,b)];
     end
-    % Note: ROOTS cannot identify yet a root if located at a breakpoint.
+    if abs(lfun(1)) <= 1e-15
+        F = [F;b];
+    elseif i < nfuns
+        rfun = f.funs{i+1};
+        if lfun(1)*rfun(-1) < 0
+            F = [F;b];
+        end
+    end    
 end 
