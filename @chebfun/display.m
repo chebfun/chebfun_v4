ca3e4775882c764bba52669d222728cd38a74c67
lnt <lnt@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2007-11-13 11:42:51 +0000

    improved and shortened display.m

--- @chebfun/display.m
+++ @chebfun/display.m
@@ -7,32 +7,23 @@ function display(X)
 % second with the values of the function at those Chebyshev points.
 %
 % Ricardo Pachon and Lloyd N. Trefethen, 2007, Chebfun Version 2.0
-if isequal(get(0,'FormatSpacing'),'compact')
-    disp([inputname(1) ' = column chebfun']);
-    for i = 1:length(X.funs)
-        n = get(X.funs{i},'n');
-        if n == 0,
-            x = X.ends(i+1);
-        else
-            x = scale(sin(pi*([-n:2:n]')/(2*n)),X.ends(i),X.ends(i+1));
-        end      
-        disp(['fun ',num2str(i),': ',num2str(n+1),' points']); 
-        disp([x flipdim(get(X.funs{i},'val'),1)]);
-    end    
-else
-    disp(' ')
-    disp([inputname(1) ' = column chebfun']);
-    for i = 1:length(X.funs)
-        disp(' ')
-        n = get(X.funs{i},'n');
-        if n == 0,
-            x = X.ends(i+1);
-        else
-            x = scale(sin(pi*([-n:2:n]')/(2*n)),X.ends(i),X.ends(i+1));
-        end
-        disp(['fun ',num2str(i),': ',num2str(n+1),' points']); 
-        disp(' ')
-        disp([x flipdim(get(X.funs{i},'val'),1)]);
+loose = ~isequal(get(0,'FormatSpacing'),'compact');
+if loose, disp(' '), end
+disp([inputname(1) ' = column chebfun']);
+if loose, disp(' '), end
+for i = 1:length(X.funs)
+    n = get(X.funs{i},'n');
+    if n == 0,
+        x = X.ends(i+1);
+    else
+        x = scale(sin(pi*([-n:2:n]')/(2*n)),X.ends(i),X.ends(i+1));
     end
+    if n==0
+      disp(['fun ',num2str(i),': ',num2str(n+1),' point']); 
+    else
+      disp(['fun ',num2str(i),': ',num2str(n+1),' points']); 
+    end
+    if loose, disp(' '), end
+    disp([x flipdim(get(X.funs{i},'val'),1)]);
 end
     
