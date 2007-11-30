07910662d49d71b8ce1ab2aa037d334c8dae2dff
lnt <lnt@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2007-11-30 17:42:48 +0000

    corrected a minibug in fixedgrow

--- support/fixedgrow.m
+++ support/fixedgrow.m
@@ -20,27 +20,27 @@ function [f,converged] = fixedgrow(op,ends)
 
 % Nick Trefethen, November 2007
 
-f = fun;                              % start with an empty fun
-for n = 2.^(2:16)                     % try increasing values of n
-   v = op(cheb(n,ends(1),ends(2)));   % function vals at scaled Cheb pts
-   f = set(f,'val',v,'n',n);          % set values and number of pts
-   c = funpoly(f);                    % coeffs of Cheb expansion of f
-   ac = abs(c)/norm(v,inf);           % abs value relative to scale of f
-   Tlen = max(3,n/8);                 % length of tail to test
-   Tmax = 2e-16*Tlen^(2/3);           % maximum permitted size of tail
-   if max(ac(1:Tlen)) < Tmax          % we have converged; now chop tail
-      Tend = min(find(ac>=Tmax))-1;   % pos of last entry below Tmax
-      ac = ac(1:Tend); ac(1) = 5e-17;  
-      for i = 2:Tend                  % compute the cumulative max of
-         ac(i) = max(ac(i),ac(i-1));  %   the tail entries and 5e-17
+f = fun;                               % start with an empty fun
+for n = 2.^(2:16)                      % try increasing values of n
+   v = op(cheb(n,ends(1),ends(2)));    % function vals at scaled Cheb pts
+   f = set(f,'val',v,'n',n);           % set values and number of pts
+   c = funpoly(f);                     % coeffs of Cheb expansion of f
+   ac = abs(c)/norm(v,inf);            % abs value relative to scale of f
+   Tlen = max(3,n/8);                  % length of tail to test
+   Tmax = 2e-16*Tlen^(2/3);            % maximum permitted size of tail
+   if max(ac(1:Tlen)) < Tmax           % we have converged; now chop tail
+      Tend = min(find(ac>=Tmax))-1;    % pos of last entry below Tmax
+      ac = ac(1:Tend); ac(1) = 5e-17;   
+      for i = 2:Tend                   % compute the cumulative max of
+         ac(i) = max(ac(i),ac(i-1));   %   the tail entries and 5e-17
       end
       Tbpb = log(1000*Tmax./ac)./ ...
-         (length(c)-(1:Tend));        % bang/buck of chopping at each pos
-      [foo,Tchop] = max(Tbpb);        % Tchop = pos at which to chop
-      f = fun(c(Tchop+1:end));        % chop the tail
-      converged = 1;                  % successful termination
+         (length(c)-(1:Tend));         % bang/buck of chopping at each pos
+      [foo,Tchop] = max(Tbpb(3:Tend)); % Tchop = pos at which to chop
+      f = fun(c(Tchop+3:end));         % chop the tail
+      converged = 1;                   % successful termination
       return
    end
 end
-converged = 0;                        % unsuccessful termination
+converged = 0;                          % unsuccessful termination
 warning('Function not resolved, using 2^16 pts. Have you tried ''splitting on''?');
