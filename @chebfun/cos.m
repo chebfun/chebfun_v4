38428de6c7859aa582d980182119e04171d955c0
ricp <ricp@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2008-02-01 16:37:06 +0000

    Fixed a minor bug in cos

--- @chebfun/cos.m
+++ @chebfun/cos.m
@@ -3,7 +3,6 @@ function F = cos(f)
 % COS(F) is the cosine of F. Effect of impulses is ignored.
 %
 % Ricardo Pachon and Lloyd N. Trefethen, 2007, Chebfun Version 2.0
-h = f.vals;
 F = f;
 nfuns = length(f.funs);
 for i = 1:nfuns
