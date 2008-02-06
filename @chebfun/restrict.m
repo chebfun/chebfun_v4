5d219ac3a4733caf76ff4c95ad4eef69734f9041
driscoll <driscoll@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2008-02-06 18:22:04 +0000

    Added restrict function for chebfuns. The only possible issue I know of is that impulses that happen to coincide with one of the new endpoints are ignored.

---
+++ @chebfun/restrict.m
@@ -0,0 +1,51 @@
+function g = restrict(f,subint)
+
+% RESTRICT Restrict a chebfun to a subinterval.
+% 
+% G = RESTRICT(F,S) returns a chebfun G whose domain is [S(1) S(2)] and
+% which agrees (to roundoff precision) with F on that interval. 
+
+% Toby Driscoll, 6 February 2008
+
+g = chebfun;
+if subint(1)>subint(2)
+  return                                         % empty result
+end
+
+dom = domain(f);
+if (subint(1)<dom(1)) || (subint(2)>dom(2))
+  error('chebfun:restrict:badinterval','Given interval is not in the domain.')
+end
+
+if subint(2)==subint(1)
+  % Easiest to dispose of this case separately.
+  [a,j] = rescale(subint(1),f.ends);             % locn of new 'domain'
+  g.funs = { fun( f.funs{j}(a) ) };
+  g.nfuns = 1;
+  g.ends = subint;
+  g.imps = [0 0];
+  return
+end
+
+% We now know dom(1)<=subint(1)<subint(2)<=dom(2).
+[a,j] = rescale(subint(1),f.ends);               % locn of new left endpt
+[b,k] = rescale(subint(2),f.ends);               % locn of new right endpt
+
+% If we hit a breakpoint exactly, get at it from the left, not the right.
+if b==-1                                         
+  b = 1;  k = k-1;                               
+end
+
+% Prune data.
+g.funs = f.funs(j:k);
+g.ends = [subint(1) f.ends(j+1:k) subint(2)];
+g.nfuns = k-j+1;
+g.imps = [0 f.imps(j+1:k) 0];                    % assumes no imps at ends!
+
+% Trim off the end funs.
+if j==k
+  g.funs{1} = restrict(g.funs{1},[a b]);         % only one left
+else
+  g.funs{1} = restrict(g.funs{1},[a 1]);         
+  g.funs{end} = restrict(g.funs{end},[-1 b]);
+end
