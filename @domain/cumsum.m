995b781429bc4d5c04763f92bb4344dc76435d95
driscoll <driscoll@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2008-03-31 14:06:06 +0000

    Allow higher order diff and cumsum calls.

--- @domain/cumsum.m
+++ @domain/cumsum.m
@@ -1,3 +1,8 @@
-function J = cumsum(d)
+function J = cumsum(d,m)
+
 J = chebop( @(n) cumsummat(n)*length(d)/2, @(u) cumsum(u), d, -1 );
+if nargin > 1
+  J = mpower(J,m);
+end
+
 end
--- @domain/diff.m
+++ @domain/diff.m
@@ -1,5 +1,8 @@
-function D = diff(d)
+function D = diff(d,m)
 
 D = chebop( @(n) diffmat(n)*2/length(d), @(u) diff(u), d, 1 );
+if nargin > 1
+  D = mpower(D,m);
+end
 
 end
