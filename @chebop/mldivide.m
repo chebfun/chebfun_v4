5709f879956ff2bc968cbb2e5db5bcb3deccbe1f
driscoll <driscoll@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2008-04-22 09:59:27 +0000

    Backslash stores LU factors at different sizes and recalls them if it needs the same matrix sizes for a previously seen operator.

--- @chebop/mldivide.m
+++ @chebop/mldivide.m
@@ -3,6 +3,10 @@ function C = mldivide(A,B)
 % Possible uses (A and B are chebops, f is a chebfun):
 %  A\f
 
+persistent storage
+if isempty(storage), storage = struct([]); end
+use_store = cheboppref('storage');
+
 switch(class(B))
 %  case 'chebop'
 %    C = chebop;
@@ -33,7 +37,18 @@ end
       v = (-1).^(0:N-1)';
       return
     end
-    L = feval(A,N);  % includes row replacements
+    
+    % Retrieve or compute LU factors of the matrix.
+    if use_store && N > 100 && length(storage)>=A.ID ...
+        && length(storage(A.ID).l)>=N && ~isempty(storage(A.ID).l{N})
+     l = storage(A.ID).l{N};
+     u = storage(A.ID).u{N};
+    else
+      L = feval(A,N);  % includes row replacements
+      [l,u] = lu(L);
+    end
+    
+    % Modify RHS as needed.
     f = B(flipud(x));
     for k = 1:length(A.lbc)
       if ~isempty(A.lbc(k).val)
@@ -44,9 +59,24 @@ end
       if ~isempty(A.rbc(k).val)
         f(N+1-k) = A.rbc(k).val;
       end
-    end      
-    v = L\f;
-    v = filter(flipud(v),1e-8);
-  end
+    end
+    
+    % Solve.
+    v = u\(l\f);
+    
+    % Store factors if necessary.
+    if use_store && N > 100
+      % This is very crude garbage collection!
+      ssize = whos('storage');
+      if ssize.bytes > cheboppref('maxstorage')
+        storage = struct([]); 
+      end
+      storage(A.ID).l{N} = l;
+      storage(A.ID).u{N} = u;
+    end
 
+    v = filter(flipud(v),1e-8);
+
+  end
+      
 end
