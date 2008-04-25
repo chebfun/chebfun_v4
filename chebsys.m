2f6215f903108d39d79a669933ed76ac4d7728be
rodp <rodp@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2008-04-25 21:24:09 +0000

    Adding chebsys.m. This function is to be tested with chebops. It generates a chebfun from solutions of systems.

---
+++ chebsys.m
@@ -0,0 +1,71 @@
+function fout = chebsys(op, ends)
+% G = CHEBSYS(F,ENDS)
+% A chebfun constructor for systems. 
+%
+% Example 
+%       myfun = @(x) { sin( length(x{2})*x{1} ), cos(10*x{2}) };
+%       f = chebsys(myfun, [-1 0 1]);           
+%       plot(f)
+%
+
+% This works in splitting off mode only.
+if chebfunpref('splitting')
+    error('splitting mode must be off')
+end
+
+% Global horizontal scale & initializations.
+hs = max(abs(ends([1,end])));
+nfuns = length(ends)-1;
+hpy = zeros(1,nfuns); 
+NN = 8*ones(1,nfuns); 
+
+% allocate memory
+scl.v = 0;
+funs = repmat(fun, 1, nfuns);
+x = cell(1,nfuns);
+
+% search for happiness
+while any(~hpy) && all(NN<2^16)
+    
+    % generate cheb points in each interval and place in a cell array.
+    for i = 1:nfuns
+        a = ends(i); b=ends(i+1);
+        x{i} = .5*( (b-a)*sin(pi*(-NN(i):2:NN(i))/(2*NN(i)))'+b+a );
+    end
+    
+    % get function values.
+    v = op(x);
+    
+    % get vertical scale
+    for k = 1:numel(v), scl.v = max(scl.v, norm(v{i},inf)); end
+    
+    % get funs and test for happiness
+    for i = 1:nfuns
+        scl.h = hs*2/diff(ends(i:i+1));    % fun horizontal scale
+        fn = set(fun(v{i}),'scl',scl);        
+        funs(i) = simplify(fn);        
+
+        hpy(i) = length(funs(i))<NN(i);
+        if ~hpy(i)
+            NN(i) = 2*NN(i);
+        end        
+    end   
+
+end 
+
+if any(~hpy)
+    warning('CHEBFUN:chebsys',['Function not resolved, using 2^16 pts.' ...
+                               ' Have you tried ''splitting on''?']);
+end
+
+% Get values at breakpoints. 
+imps = zeros(size(ends));
+imps(1) = funs(1).vals(1);
+for k = 2:nfuns
+    imps(k) = funs(k).vals(1);
+end
+imps(end) = funs(end).vals(end);
+
+% Get chebfun.
+fout = set(chebfun,'funs',funs,'ends',ends,'imps',imps,'trans',0);
+
