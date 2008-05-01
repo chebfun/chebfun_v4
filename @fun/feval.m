6972e0aa5752758848b65f7439d6ff3caef85e8a
rodp <rodp@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2008-05-01 08:49:44 +0000

    Sorry, the correct length of X in feval must be 2^n+1 in order to avoid a call to bary. This conforms with the size of x used in the fun constructor.

--- @fun/feval.m
+++ @fun/feval.m
@@ -2,7 +2,7 @@ function out = feval(g,x)
 % Y = FEVAL(G,X)
 % Evaluation of a fun G at points X. In the general case, this is done 
 % using the barycentric formula in bary.m. However, if X is a vector of Chebyshev 
-% nodes of length 2^n-1 then the evaluation is done using FFTs through
+% nodes of length 2^n+1 then the evaluation is done using FFTs through
 % prolong.m
 
 % Number of points.
