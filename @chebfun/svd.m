5964e343d02ae644eee10c1438d41c269fec36fa
lnt <lnt@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2008-05-05 16:12:06 +0000

    changes to SVD, including provision for row quasimatrices

--- @chebfun/svd.m
+++ @chebfun/svd.m
@@ -1,20 +1,34 @@
 function varargout = svd(A,econ)
 % SVD	Singular value decomposition
-% [U,S,V]=SVD(A,0) produces a diagonal matrix S with nonnegative diagonal
-% elements in decreasing order, a matrix U whose columns are orthogonal
-% chebfuns and a unitary matrix V such that A=U*S*V'.
+% [U,S,V] = SVD(A,0), where A is a column quasimatrix with n columns,
+% produces an n x n diagonal matrix S with nonnegative diagonal
+% elements in nonincreasing order, a column quasimatrix U with n
+% orthonormal columns, and an n x n  unitary matrix V such that
+% A = U*S*V'.
 %
-% S=SVD(A) produces a vector containing the singular values.
+% If A is a row quasimatrix with n rows, then U is a unitary matrix
+% and V is a row quasimatrix.
+%
+% S = SVD(A) produces a vector containing the singular values.
 
-if ((nargin==2 & econ~=0) | (nargin==1 & nargout>1))
-  error('Use svd(A,0) for economy size decomposition.');
+% The computation is carried out by orthogonalization operations
+% following Battles' 2006 thesis.
+
+if (nargin~=2) | ((nargin==2) & (econ~=0))
+    error('chebfun:svd:twoargs',...
+          'Use svd(A,0) for SVD of quasimatrix.');
+end
+if A(1).trans                 % A is a row quasimatrix
+    [Q,R] = qr(A',0);
+    [V,S,U] = svd(R,0);
+    V = Q*V;
 else
-  [q,r]=qr(A,0);
-  [U,S,V]=svd(r,0);
-  U=q*U;
+    [Q,R] = qr(A,0);          % A is a column quasimatrix
+    [U,S,V] = svd(R,0);
+    U = Q*U;
 end
 if (nargout==3)
-  varargout{1}=U; varargout{2}=S; varargout{3}=V;
+    varargout{1}=U; varargout{2}=S; varargout{3}=V;
 elseif (nargout==1 | nargout==0)
-  varargout{1}=diag(S);
+    varargout{1}=diag(S);
 end
