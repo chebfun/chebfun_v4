619783455d92add9fb875a236a546a0e6a1cbb54
rodp <rodp@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2008-12-01 12:03:46 +0000

    Fixed tolerance in subspacetest. Because of the singularity of asin, only 7 or 8 digits should be expected if the angle is about pi/2

--- chebtests/subspacetest.m
+++ chebtests/subspacetest.m
@@ -8,7 +8,9 @@ A = [vander(exp(-i*theta), 3) vander(exp(i*theta), 3)];
 B = [cos(5*theta) sin(10*theta)];
 
 angle = subspace(A,B);
-pass = abs(angle - pi/2)< 1e-14;
+% Note only single precision can be expected here because of the
+% singularity of asin(x) at x=1.
+pass = abs(angle - pi/2)< 1e-7;
 
 angle = subspace(A,sin(2*theta));
-pass = pass && angle < 1e-14;
+pass = pass && (angle < 1e-14);
