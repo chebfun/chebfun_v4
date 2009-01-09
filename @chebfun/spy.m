ec3fce69b6a1f7a736a8299137bfb4a66206d435
lnt <lnt@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2009-01-09 10:08:57 +0000

    I have added a new SPY command.  Try it!

---
+++ @chebfun/spy.m
@@ -0,0 +1,39 @@
+function spy(A)
+% SPY   spy of a chebfun
+%
+
+% Copyright 2002-2009 by The Chebfun Team. See www.comlab.ox.ac.uk/chebfun.
+
+hold off
+[m,n] = size(A);
+[a,b] = domain(A);
+
+if isinf(m) & ~isinf(n)            % column quasimatrix
+   for j = 1:n
+      plot([j j],[a b],'-b'), hold on
+   end
+   set(gca,'ytick',[a b])
+   yl = get(gca,'yticklabel');
+   set(gca,'yticklabel',flipud(yl))
+   if n<10, set(gca,'xtick',1:n)
+      else, set(gca,'xtick',[1 n]), end
+   axis([0 n+1 a b])
+   ar = get(gca,'plotboxaspectratio');
+   ar(1) = .5*ar(1);
+   set(gca,'plotboxaspectratio',ar)
+end
+
+if ~isinf(m) & isinf(n)            % row quasimatrix
+   for i = 1:m
+      plot([a b],[i i],'-b'), hold on
+   end
+   set(gca,'xtick',[a b])
+   if m<10, set(gca,'ytick',1:m)
+      else, set(gca,'ytick',[1 m]), end
+   yl = get(gca,'yticklabel');
+   set(gca,'yticklabel',flipud(yl))
+   axis([a b 0 m+1])
+   ar = get(gca,'plotboxaspectratio');
+   ar(2) = .4*ar(2);
+   set(gca,'plotboxaspectratio',ar)
+end
