3908bfe1b4ee3ecaaac974f66b35cbde671df02e
driscoll <driscoll@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2009-01-10 21:57:36 +0000

    Chebfun outer products enabled, plus a chebtest for them.

--- @chebfun/mtimes.m
+++ @chebfun/mtimes.m
@@ -1,6 +1,13 @@
 function Fout = mtimes(F1,F2)
-% *	  Scalar multiplication.
-% F*G multiplies a chebfun by a scalar.
+%*	  Chebfun multiplication.
+% c*F or F*c multiplies a chebfun F by a scalar c.
+%
+% F*G, if F is an m-by-Inf row chebfun and G is an Inf-by-n column chebfun, 
+% returns the m-by-n matrix of pairwise inner products. F and G must have
+% the same domain.
+%
+% A=F*G, if F is Inf-by-m and G is m-by-Inf, results in a rank-m chebop A
+% such that A*U=F*(G*U) for any chebfun U. 
 
 % Copyright 2002-2008 by The Chebfun Team. See www.comlab.ox.ac.uk/chebfun.
 
@@ -9,15 +16,45 @@ if (isa(F1,'chebfun') && isa(F2,'chebfun'))
     if size(F1,2) ~= size(F2,1)
         error('Quasimatrix dimensions must agree.')
     end
-    Fout = zeros(size(F1,1),size(F2,2));
-    for k = 1:size(F1,1)
-        for j = 1:size(F2,2)
-            if F1(k).trans && ~F2(j).trans
-                Fout(k,j) = sum((F1(k).').*F2(j));
-            else
-                error('Chebfun dimensions must agree.')
-            end
+    if isinf(size(F1,1))     % outer product
+      splitstate = chebfunpref('splitting');
+      splitting off
+      sampstate = chebfunpref('resample');
+      resample on
+      Fout = 0;
+      d = domain(F1);
+      if ~(d==domain(F2))
+        error('chebfun:mtimes:outerdomain',...
+          'Domains must be identical for outer products.')
+      end
+      for i = 1:size(F1,2)
+        f = F1(:,i);
+        g = F2(i,:);
+        op = @(u) f * (g*u);  % operational form
+        % Matrix form available only for unsplit functions.
+        if f.nfuns==1 && g.nfuns==1 
+          x = @(n) d(1) + (1+sin(pi*(2*(1:n)'-n-1)/(2*n-2)))/2*length(d);
+          C = cumsum(d);
+          w = C(end,:);  % Clenshaw-Curtis weights, any n
+          mat = @(n) feval(f,x(n)) * (w(n) .* feval(g,x(n)).');
+        else
+          mat = [];
         end
+        Fout = Fout + chebop(mat,op,d);
+      end
+      chebfunpref('splitting',splitstate)
+      chebfunpref('resample',sampstate)
+    else      % inner product
+      Fout = zeros(size(F1,1),size(F2,2));
+      for k = 1:size(F1,1)
+        for j = 1:size(F2,2)
+          if F1(k).trans && ~F2(j).trans
+            Fout(k,j) = sum((F1(k).').*F2(j));
+          else
+            error('Chebfun dimensions must agree.')
+          end
+        end
+      end
     end
 
 % Chebfun times double
@@ -41,6 +78,7 @@ elseif isa(F1,'chebfun')
 else
     Fout = mtimes(F2.',F1.').';
 end
+
 
 % ------------------------------------
 function f = mtimescol(a,f)
---
+++ chebtests/outerprod.m
@@ -0,0 +1,21 @@
+function pass = outerprod
+
+% Toby Driscoll
+% Outer product testing
+
+[d,x] = domain(0,1);
+f = [ exp(x), tanh(x) ];
+g = [ exp(x), x./(1+x.^2) ];
+u = x;
+
+A = f*g';
+Au = (exp(x) + (1-pi/4)*tanh(x));
+
+% operational form
+pass1 = norm( Au - A*u ) < 1e-12;
+
+% discrete form
+xx = (1+sin(pi*(2*(1:200)'-200-1)/(400-2)))/2;
+pass2 = norm( Au(xx) - A(200)*u(xx) ) < 1e-12;
+
+pass = pass1 & pass2;
