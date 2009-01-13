736f006acc19418b8f0c9356af9c706425d88b91
rodp <rodp@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2009-01-13 15:40:41 +0000

    New eq.m.  F1 == F2 returns a chebfun which is zero almost everywhere except at the  intersection points of F1 and F2.

--- @chebfun/eq.m
+++ @chebfun/eq.m
@@ -1,17 +1,54 @@
-function out = eq(F1,F2)
+function fout = eq(F1,F2)
 % ==   Equal.
-%  F1 == F2 compares chebfuns F1 and F2 and returns logical true if F1 and
-%  F2 are equal and false otherwise.
+%  F1 == F2 returns a chebfun which is zero almost everywhere except at the
+%  intersection points of F1 and F2. 
+%
 
 % Copyright 2002-2008 by The Chebfun Team. See www.comlab.ox.ac.uk/chebfun.
 
-tol = 10*chebfunpref('eps');
+% Make sure F1 is a chebfun
+if ~isa(F1,'chebfun')
+    Ftemp = F1;
+    F1 = F2;
+    F2 = Ftemp;
+end
 
-if numel(F1) ~= numel(F2) || F1(1).trans ~= F2(1).trans
-    out = false;
-else
-    out = true;
-    for k =1: numel(F1)
-        out = out && norm(F1(k)-F2(k),inf) < tol*min(F1(k).scl,F2(k).scl);
-    end
+if isa(F2,'double')
+    % if F2 is a scalar repeat to match number of columns in F1
+    F2 = repmat(F2,1,numel(F1));
+elseif size(F1) ~= size(F2) 
+    % two quasimatrices must have the same size
+    error('CHEBFUN:eq:wrongsize','Quasimatrix dimensions must agree')
+end
+
+fout = chebfun;
+for k = 1:min(size(F1)) % Do it for each column of F1.
+      fout = [fout  eqcol(F1(k), F2(k))];
+end
+
+function fout = eqcol(f1,f2)
+% Eq for two single chebfuns or one chebfun and one scalar
+% Note: f1 must be a chebfun, f2 may be a scalar
+
+r = roots(f1-f2); % Find points where f1 == f2
+ends = f1.ends;
+fout = chebfun;
+newends = union(r,ends);
+fout.scl = 1;
+fout.ends = newends;
+fout.imps = 1+newends*0;
+fout.nfuns = length(fout.ends)-1;
+fout.trans = f1.trans;
+funs =[];
+for k = 1:fout.nfuns
+    funs = [funs fun(0)];
+end
+fout.funs = funs;
+
+% Check whehter endpoints are also roots:
+if r(1) ~= ends(1)
+    fout.imps(1) = 0;
+end
+if r(end) ~= ends(end)
+    fout.imps(end) = 0;
 end
