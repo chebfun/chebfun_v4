40200b34b28f9d42bdcef04968c48ce0295d649c
rodp <rodp@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2009-02-07 14:24:29 +0000

    Improved C implementation of the barycentric formula (by N. Hale). The new version handles complex values, nan and inf.

--- @fun/private/bary_mex.c
+++ @fun/private/bary_mex.c
@@ -1,430 +1,217 @@
 /* MEX file support for bary.m */
-
 /* Barycentric formula for Chebyshev points and weights */
 
-
-
-#include "math.h"
-
+#include <math.h>
 #include <stdio.h>
-
 #include <malloc.h>
-
 #include "matrix.h"
-
 #include "mex.h"
 
+#define PI 3.1415926535897932384626433
 
-
-#define PI 3.141592653589793
-
-
-
-int bary_mex(int N, double *fR, int n, double *x, double *gvalsR, double *xk)
-
+int bary_mex(int N, double *fR, double *fI, int n, double *xR, double *xI, double *gvalsR, double *gvalsI, double *xk)
 {
-
-  int sgn, j, k, m = n-1, kappa;
-
-  double xj, xx, numR, denom, MPi;
-
-  double *xkptr, *gptrR;
-
- 
-
-  if (n == 1) /* The function is a constant */
-
-  {
-
-      xx = gvalsR[0];
-
-      for (j = 0; j < N; j++)
-
-      {
-
-		*fR++ = xx;
-
-      }
-
-      return(0);
-
-  }
-
-    
-
-  /* Some constants */  
-
-  m = n-1;  
-
-  MPi = (double)m / PI;
-
-  
-
-  /* The main loop */
-
-  for (j = 0; j < N; j++) 
-
-  {
-
-    xj = x[j];
-
-    kappa = floor(MPi * acos(xj) + 0.5); /* this is really 'round' - hopefully*/
-
-    if (xj == xk[m-kappa])         /* xj is a Chebyshev point */
-
-    {
-
-  		*fR++ = gvalsR[m-kappa];;
-
-    }
-
-    else			   /* Barycentric formula */
-
-    {
-
-        /*printf("%d \t %16.16g\n",j, x[j]-xk[m-kappa]);*/
-
-        gptrR = &gvalsR[0]; 
-
-        xkptr = &xk[0];
-
-		xx = .5 / (xj - *xkptr++);
-
-   		numR = xx * *gptrR++;
-
-    	denom = xx;
-
-    	sgn = -1;
-
-    	for (k = 1; k < n-1; k++) 
-
-    	{
-
-    	    xx = (double)sgn / (xj - *xkptr++);
-
-  			numR += xx * *gptrR++;	    
-
-  			denom += xx;
-
-  			sgn = -sgn;
-
-    	}
-
-    	xx = (double)sgn * 0.5 /(xj - *xkptr);
-
-        numR += xx * *gptrR;
-
-    	denom += xx;
-
-    	denom = 1./denom;
-
-    	*fR++ = denom*numR;
-
-    }
-
-  }
-
-  return(0);
-
-}
-
-
-
-
-
-int bary_mexC(int N, double *fR, double *fI, int n, double *x, double *gvalsR, double *gvalsI, double *xk)
-
-{
-
-  int sgn, j, k, m = n-1, kappa;
-
-  double xj, xx, numR, numI, denom, Pi2M, PiM, MPi;
-
+  int sgn, j, k, m = n-1, kappa, chebpt;
+  double xj, yj, xjk, xx, numR, numI, denomR, denomI, Pi2M, PiM, MPi;
   double *xkptr, *gptrR, *gptrI;
-
- 
-
-  if (n == 1) /* The function is a constant */
-
-  {
-
-      for (j = 0; j < N; j++)
-
-      {
-
-	 *fR++ = gvalsR[0];
-
-	 *fI++ = gvalsI[0];
-
-      }
-
-      return(0);
-
-  }
-
-    
-
-  /* Some constants */  
-
-  m = n-1;  
-
-  PiM = PI / (double)m;
-
-  Pi2M = 0.5 * PiM;
-
-  MPi = (double)m / PI;
-
- 
-
-  /* The main loop */
-
-  for (j = 0; j < N; j++) 
-
-  {
-
-    xj = x[j];
-
-    kappa = floor(MPi * acos(xj) + 0.5); /* this is really 'round' - hopefully*/
-
-    if (xj == xk[m-kappa])         /* xj is a Chebyshev point  */
-
-    {
-
-  	*fR++ = gvalsR[m-kappa];
-
- 	*fI++ = gvalsI[m-kappa];
-
-    }
-
-    else			   /* Barycentric formula */
-
-    {
-
-        /*printf("%d \t %16.16g\n",j, x[j]-xk[m-kappa]);*/
-
-        gptrR = &gvalsR[0];
-
-        gptrI = &gvalsI[0];        
-
-        xkptr = &xk[0];
-
-		xx = .5 / (xj - *xkptr++);
-
-   		numR = xx * *gptrR++;
-
-   		numI = xx * *gptrI++;
-
-    	denom = xx;
-
-    	sgn = -1;
-
-    	for (k = 1; k < n-1; k++) 
-
-    	{
-
-    	    xx = (double)sgn / (xj - *xkptr++);
-
-  			numR += xx * *gptrR++;
-
-  			numI += xx * *gptrI++;  	    
-
-  			denom += xx;
-
-  	    sgn = -sgn;
-
-    	}
-
-    	xx = (double)sgn * 0.5 /(xj - *xkptr);
-
-        numR += xx * *gptrR;
-
-        numI += xx * *gptrI;
-
-    	denom += xx;
-
-    	denom = 1./denom;
-
-    	*fR++ = denom*numR;
-
-    	*fI++ = denom*numI;    	
-
-    }
-
-  }
-
   
 
+  if (n == 1) /* The function is a constant */
+  {
+      for (j = 0; j < N; j++)
+      {
+	 *fR++ = gvalsR[0];
+	 *fI++ = gvalsI[0];
+      }
+      return(0);
+  }
+    
+  /* Some constants */  
+  m = n-1;  
+  PiM = PI / (double)m;
+  Pi2M = 0.5 * PiM;
+  MPi = (double)m / PI;
+ 
+  /* The main loop */
+  for (j = 0; j < N; j++) 
+  {
+    chebpt = 1;
+    xj = xR[j];
+    yj = xI[j];
+    
+    
+    if (yj != 0)			/* imag(x) != 0 */
+    {
+    	chebpt = 0;
+    }
+    else
+    {
+    	if (abs(xj) > 1)		/* real(x) > 1 */
+	{
+    		chebpt = 0;
+	}
+    }
+   
+    if (chebpt == 1)
+    {
+        kappa = floor(0.5 + MPi * acos(xj)); /* This should be round? */
+    	if (xj == xk[m-kappa])               /* xj is a Chebyshev point  */
+    	{
+  		*fR++ = gvalsR[m-kappa];
+ 		*fI++ = gvalsI[m-kappa];
+ 	}
+ 	else
+ 	{	
+ 		chebpt = 0;
+ 	}
+    }    
+
+    if (chebpt == 0) 
+    {
+        gptrR = &gvalsR[0];
+        gptrI = &gvalsI[0];        
+        xkptr = &xk[0];
+	xjk = (xj - *xkptr++);
+	
+	xx = 0.5 / (xjk*xjk + yj*yj);
+   	numR = xx * (xjk * *gptrR + yj * *gptrI);
+   	numI = xx * (xjk * *gptrI++ - yj * *gptrR++);
+    	denomR = xx * xjk;
+    	denomI = - xx * yj;
+
+    	sgn = -1;
+    	for (k = 1; k < n-1; k++) 
+    	{
+       	    xjk =  (xj - *xkptr++);
+       	    xx = (double)sgn / (xjk*xjk+yj*yj);
+  	    numR += xx * (xjk * *gptrR + yj * *gptrI);
+  	    numI += xx * (xjk * *gptrI++ - yj * *gptrR++);  	    
+  	    denomR += xx * xjk;
+  	    denomI -= xx * yj;
+  	    sgn = -sgn;
+    	}
+    	
+    	xjk =  (xj - *xkptr);
+    	xx = (double)sgn * 0.5 / (xjk*xjk + yj*yj);
+   	numR += xx * (xjk * *gptrR + yj * *gptrI);
+   	numI += xx * (xjk * *gptrI++ - yj * *gptrR++);
+    	denomR += xx * xjk;
+    	denomI -= xx * yj;
+    	
+    	xx = 1./(denomR*denomR + denomI*denomI);
+    	*fR++ = xx * (numR*denomR + numI*denomI);
+    	*fI++ = xx * (numI*denomR - numR*denomI);
+    }
+  }
+  
   return(0);
-
 }
-
-
 
 /* The gateway routine */
-
 void mexFunction(int nlhs, mxArray *plhs[],
-
                  int nrhs, const mxArray *prhs[])
-
 {
-
   double PiM, Pi2M;
-
-  double *fR, *fI, *x, *gvalsR, *gvalsI, *xk, *xkptr;
-
+  double *fR, *fI, *xR, *xI, *gvalsR, *gvalsI, *xk, *xkptr;
   int N, n, m, k;
-
+  int xreal = 0, greal = 0;
   int status;
-
-  int isreal = 1;
-
   
-
   /*  Check for proper number of arguments. */
-
   /* NOTE: You do not need an else statement when using 
-
      mexErrMsgTxt within an if statement. It will never 
-
      get to the else statement if mexErrMsgTxt is executed. 
-
      (mexErrMsgTxt breaks you out of the MEX-file.) 
-
   */ 
-
   if (nrhs == 1) 
-
     mexErrMsgTxt("Two or Three inputs required.");
-
   if (nlhs != 1) 
-
     mexErrMsgTxt("One output required.");
-
   
-
-  /* Get the input */
-
-  x = mxGetPr(prhs[0]);
-
-  gvalsR = mxGetPr(prhs[1]);
-
-  gvalsI = mxGetPi(prhs[1]);
-
-  if (nrhs == 3) 
-
-  {
-
-      xk = mxGetPr(prhs[2]);
-
-  }
-
- 
-
+  /* Get length of output. */
+  N = mxGetM(prhs[0]);
   /* Get length of chebfun. */
-
   n = mxGetM(prhs[1]);
 
-
-
-  /* Get length of output. */
-
-  N = mxGetM(prhs[0]);
-
-
-
-  /* Construct Chebyshev points */
-
-  if (nrhs == 2)
-
+  
+  if (nrhs == 2)		/* Construct Chebyshev points */
   {
-
       /* Some constants */ 
-
       m = n-1;
-
       PiM = PI / (double)m;
-
-      Pi2M = 0.5 * PiM;
-
-      /* Allocate memory for Chebyshev points*/ 
-
+      Pi2M = 0.5 * PiM;      /* Allocate memory for Chebyshev points*/ 
       xk = (double*)malloc(n*sizeof(double));
-
       xkptr = &xk[0];
-
       /* Define Chebyshev Points */
-
       for (k = -m; k <= m; k+=2) 
-
       {
-
      	*xkptr++ = sin((double)k*Pi2M);
-
       }
-
-      
-
   }
-
-
-
-  if (!mxIsComplex(prhs[1]))
-
+  else				/* Chebyshev points are given */
   {
-
-      /* Set the output pointer to the output matrix. */
-
-      plhs[0] = mxCreateDoubleMatrix(N,1, mxREAL);
-
-  
-
-      /* Create a C pointer to a copy of the output matrix. */
-
-      fR = mxGetPr(plhs[0]);
-
-  
-
-      /* Call the C subroutine. */
-
-      bary_mex(N, fR, n, x, gvalsR, xk);  
-
+  	xk = mxGetPr(prhs[2]);
   }
+  
+  /* Get the input (real parts) */
+  xR = mxGetPr(prhs[0]);
+  gvalsR = mxGetPr(prhs[1]);
 
+  if (mxIsComplex(prhs[0]))	/* Complex evaluation points */
+  {
+  	xI = mxGetPi(prhs[0]);
+  }
   else
-
-  {
-
-      /* Set the output pointer to the output matrix. */
-
-      plhs[0] = mxCreateDoubleMatrix(N,1, mxCOMPLEX);
-
-  
-
-      /* Create a C pointer to a copy of the output matrix. */
-
-      fR = mxGetPr(plhs[0]);
-
-      fI = mxGetPi(plhs[0]);
-
-  
-
-      /* Call the C subroutine. */
-
-      bary_mexC(N, fR, fI, n, x, gvalsR, gvalsI, xk);
-
+  {	
+  	xI = (double*)calloc(1,N*sizeof(double));
+  	xreal = 1;
   }
-
+            
+  if (mxIsComplex(prhs[1]))	/* Complex function */
+  {	
+	gvalsI = mxGetPi(prhs[1]);
+  }  	
+  else
+  {
+  	gvalsI = (double*)calloc(1,n*sizeof(double));
+  	greal = 1;
+  }
+ 
+  if (xreal + greal < 2)	/* Complex output */
+  {
+  	/* Set the output pointer to the output matrix. */
+  	plhs[0] = mxCreateDoubleMatrix(N,1, mxCOMPLEX);  
+  	/* Create a C pointer to a copy of the output matrix. */
+  	fR = mxGetPr(plhs[0]);
+  	fI = mxGetPi(plhs[0]);
+  }
+  else
+  {
+    	/* Set the output pointer to the output matrix. */
+  	plhs[0] = mxCreateDoubleMatrix(N,1, mxREAL);  
+  	/* Create a C pointer to a copy of the output matrix. */
+  	fR = mxGetPr(plhs[0]);
+  	fI = (double*)malloc(N*sizeof(double));
+  }
   
-
+  /* Call the C subroutine. */
+  bary_mex(N, fR, fI, n, xR, xI, gvalsR, gvalsI, xk);
+  
   if (nrhs == 2)
-
   {
-
       free(xk);
-
   }
-
+  if (xreal == 1)
+  {
+      free(xI);
+  }
+  if (greal == 1)
+  {
+      free(gvalsI);
+  }
+  if (xreal + greal == 2)
+  {
+      free(fI);
+  }
   
-
 }
-
