db2aa1070b916bb27b53f6f23631321b21309eb5
lnt <lnt@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2009-03-27 16:23:04 +0000

    Replaced BARY_WEIGHTS with a new Hale-accelerated version, which uses matrices instead of loops for n<300.

--- bary_weights.m
+++ bary_weights.m
@@ -1,8 +1,19 @@
 function w = bary_weights(xk)
+% W = BARY_WEIGHTS(XK)
+% Compute the barycentric weights W for the points XK
+% Ricardo Pachon 2008; Nick Hale 2009
 n = length(xk);
-w = ones(n,1);
-for i = 1:n
-    v = 2*(xk(i)-xk);
-    vv = exp(sum(log(abs(v(find(v))))));    
-    w(i) = 1./(prod(sign(v(find(v))))*vv);
+if n < 300          % for small n using matrices is faster
+   XK = repmat(xk(:),1,n);
+   V = 2*(XK-XK');
+   V(logical(eye(n))) = 1;
+   VV = exp(sum(log(abs(V))));
+   w = 1./(prod(sign(V)).*VV).';
+else               % for large n use a loop
+   w = ones(n,1);
+   for j = 1:n
+       v = 2*(xk(j)-xk);
+       v = v(logical(v));
+       vv = exp(sum(log(abs(v))));           w(j) = 1./(prod(sign(v))*vv);
+   end
 end
