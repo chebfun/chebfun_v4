9c2b44f983e03899d79de96f03dd7525d3f65ffd
lnt <lnt@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2009-05-09 15:21:47 +0000

    If you look at the comments in this code you'll see what it's about.  I hope people don't mind my doing this. I now have MathWorks signed on the the ATAP project as a book done through PUBLISH; this is exciting!

---
+++ ATAPformats.m
@@ -0,0 +1,19 @@
+% ATAPformats.m  Set formats for Trefethen's book "Approximation
+%                Theory and Approximation Practice"
+
+% Nick Trefethen, May 2009
+% This code sets certain default properties and is called at
+% the beginning of each section of the book so that the output
+% from "publish" has a pleasing appearance.  It is included in
+% this directory as part of the chebfun distribution so that
+% readers of the book need only download chebfun to be able
+% to execute each section.
+
+% These formats are a work in progress -- they will go through
+% various changes in the months ahead.
+
+set(0,'defaultfigureposition',[380 320 540 200],...
+'defaultaxeslinewidth',0.9,'defaultaxesfontsize',8,...
+'defaultlinelinewidth',1.2,'defaultpatchlinewidth',1.2,...
+'defaultlinemarkersize',15), format compact, format long
+chebfunpref('factory'); clear all, x = chebfun('x',[-1 1]);
