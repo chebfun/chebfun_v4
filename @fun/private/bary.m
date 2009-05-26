f15f0a929de414d025de18e1adb4269c49de2d76
rodp <rodp@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2009-05-26 15:47:09 +0000

    Added for-loop option in bary. If length(x) < length(xk), loops in the elements of x, otherwise, loops in the elements of xk.

--- @fun/private/bary.m
+++ @fun/private/bary.m
@@ -19,16 +19,29 @@ else
         x = gvals*ones(size(x));
         return;
     end
+    
     xk = chebpts(n);
-    ek = [1/2; ones(n-2,1); 1/2].*(-1).^((0:n-1)');
-    mem = ismember(x,xk);
-    for i = 1:numel(x)
-        if (mem(i))
-            x(i) = gvals(xk==x(i));
-        else
-            xx = ek./(x(i)-xk);
-            x(i) = (xx.'*gvals)/sum(xx);
-        end
+    ek = [1/2; ones(n-2,1); 1/2].*(-1).^((0:n-1)');  
+    [mem,loc] = ismember(x,xk);
+    
+    if length(x) < length(xk)
+        for i = 1:numel(x)
+            if ~mem(i)
+                xx = ek./(x(i)-xk);
+                x(i) = (xx.'*gvals)/sum(xx);
+            end
+        end      
+    else
+         xnew = x(~mem);
+         num = zeros(size(xnew)); denom = num;
+         for i = 1:numel(xk)
+              y = ek(i)./(xnew-xk(i));
+              num = num+(gvals(i)*y);
+              denom = denom+y;
+         end
+         x(~mem) = num./denom;
     end
-       
+    
+    x(mem) = gvals(loc(mem));
+    
 end
