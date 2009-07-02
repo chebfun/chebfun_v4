0be3f03c4256097298262bd08e1146d6a266a67e
nich <nich@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2009-07-02 13:44:50 +0000

    Fixed bug in dirac.m when f has zeros at the ends of the interval.

--- @chebfun/dirac.m
+++ @chebfun/dirac.m
@@ -9,14 +9,18 @@ function d = dirac(f)
 %  Last commit: $Author$: $Rev$:
 %  $Date$:
 
+tol = chebfunpref('eps');
+
 [a b] = domain(f);
 r = roots(f);
 ends = union(r,[a,b]);
 
+if abs(ends(1)-ends(2)) < 10*tol*f.scl, ends(2) = []; end
+if abs(ends(end)-ends(end-1)) < 10*tol*f.scl, ends(end-1) = []; end
+
 d = chebfun(0,ends);
 d.imps(2,2:end-1) = 1;
 
-tol = chebfunpref('eps');
 if abs( feval(f,a) ) < 100*tol*f.scl
     d.imps(2,1) = 1;
 end
