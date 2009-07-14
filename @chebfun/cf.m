a0d7873065f20226bab460f8cdc6e297c86f334d
nich <nich@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2009-07-14 12:07:18 +0000

    Extended CF approximation to arbitrary intervals.

--- @chebfun/cf.m
+++ @chebfun/cf.m
@@ -1,7 +1,8 @@
 function [p,lam] = cf(f,m)
+% Caratheodory-Fejer approximation
 %
-% P = CF(F,M): degree M polynomial CF approximant to chebfun F
-%              (which must consist of just a single fun)
+% P = CF(F,M): degree M polynomial CF approximant to chebfun F defined 
+%              in [a b] (which must consist of just a single fun)
 %
 % [P,LAMBDA] = CF(F,M): same plus LAMBDA, the associated CF
 %           singular value, an approximation to the minimax error
@@ -18,14 +19,13 @@ function [p,lam] = cf(f,m)
 % approximation by the Caratheodory-Fejer method", SIAM J. Numer.
 % Anal. 19 (1982), 358-371.
 %
-% Note: at present this is restricted to [-1,1] and to
-%       polynomial CF approximation only
+% Note: at present this is restricted polynomial CF approximation only.
 %
 % See http://www.comlab.ox.ac.uk/chebfun for chebfun information.
 
 % Copyright 2009 by The Chebfun Team. 
 
-
+d = domain(f);
 M = length(f)-1;                         % degree of f
 if m>=M
    p = f;
@@ -33,7 +33,7 @@ if m>=M
 end
 a = chebpoly(f);
 if m==M-1
-   p = chebfun(chebpolyval(a(2:M+1))); 
+   p = chebfun(chebpolyval(a(2:M+1)),d); 
    return
 end
 c = a(M-m:-1:1);
@@ -45,5 +45,5 @@ b = c;
 for k = m:-1:-m 
    b = [-(b(1:M-m-1)*uu)/u1 b];
 end
-d = a(M-m+1:M+1) - [b(1:m) 0] - b(2*m+1:-1:m+1);
-p = chebfun(chebpolyval(d));
+pk = a(M-m+1:M+1) - [b(1:m) 0] - b(2*m+1:-1:m+1);
+p = chebfun(chebpolyval(pk),d);
