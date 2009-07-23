3833fedd6987242220f98e68172aa1bee392522b
nich <nich@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2009-07-23 13:50:24 +0000

    bary-weights was initially designed to work on [-1,1]. Since it now takes points XK on arbitrary intervals, the scaling parameter C has be to adjusted accordingly (to the capacity of the interval).

--- bary_weights.m
+++ bary_weights.m
@@ -1,25 +1,30 @@
 function w = bary_weights(xk)
 % W = BARY_WEIGHTS(XK)
-% Compute the barycentric weights W for the points XK
-% Ricardo Pachon 2008; Nick Hale 2009
+% Compute the barycentric weights W for the points XK, scaled such that
+% norm(W,inf) == 1.
 %
 % See http://www.comlab.ox.ac.uk/chebfun for chebfun information.
 
-% Copyright 2002-2008 by The Chebfun Team. 
-
+%  Copyright 2002-2009 by The Chebfun Team. 
+%  Last commit: $Author: nich $: $Rev: 458 $:
+%  $Date: 2009-05-10 20:51:03 +0100 (Sun, 10 May 2009) $:
 
 n = length(xk);
+C = 4/(max(xk)-min(xk)); % Capacity of interval
 if n < 300          % for small n using matrices is faster
    XK = repmat(xk(:),1,n);
-   V = 2*(XK-XK');
+   V = C*(XK-XK');
    V(logical(eye(n))) = 1;
    VV = exp(sum(log(abs(V))));
    w = 1./(prod(sign(V)).*VV).';
 else               % for large n use a loop
    w = ones(n,1);
    for j = 1:n
-       v = 2*(xk(j)-xk);
+       v = C*(xk(j)-xk);
        v = v(logical(v));
-       vv = exp(sum(log(abs(v))));           w(j) = 1./(prod(sign(v))*vv);
+       vv = exp(sum(log(abs(v))));           
+       w(j) = 1./(prod(sign(v))*vv);
    end
 end
+% Scaling
+w = w./max(abs(w));
