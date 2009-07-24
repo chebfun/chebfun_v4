8468bc3271bdba9082813351da3cd4cc24af25d2
nich <nich@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2009-07-24 14:32:31 +0000

    Altered ellipj to allow imaginary values (uses transformations, rather than Toby's ellipjc).
    Can now plot stripmaps in chebfun (code below);
    
    % parameters for stripmap
    m = .5; m14 = m^(1/4);
    K = ellipke(m);
    Kp = ellipke(1-m);
    rho = exp(pi*Kp/(4*K));
    
    % make the ellipse
    x = chebfun('x',[0,1,2]);
    circle = rho*exp(1i*pi*x);
    ellipse = .5*(circle+1./circle);
    
    % the stripmap
    splitting on
    strip = atanh(m14*ellipj(2*K/pi*asin(ellipse),m))/atanh(m14)
    
    % plot the two
    plot(ellipse); hold on
    plot(strip,'r')
    axis equal

--- @chebfun/ellipj.m
+++ @chebfun/ellipj.m
@@ -5,7 +5,10 @@ function [sn,cn,dn] = ellipj(u,m,tol)
 %   As currently implemented, M must be a scalar and is limited to 0 <= M <= 1. 
 %
 %   [SN,CN,DN] = ELLIPJ(U,M,TOL) computes the elliptic functions to
-%   the accuracy TOL instead of the default TOL = CHEBFUNPREF('EPS').  
+%   the accuracy TOL instead of the default TOL = CHEBFUNPREF('EPS').
+%
+%   Complex values of U are accepted, but the resulting computation may be
+%   inaccurate. Use ELLIPJC from Driscoll's SC toolbox instead.
 %
 %   Some definitions of the Jacobi elliptic functions use the modulus
 %   k instead of the parameter M.  They are related by M = k^2.
@@ -18,26 +21,34 @@ function [sn,cn,dn] = ellipj(u,m,tol)
 %  Last commit: $Author$: $Rev$:
 %  $Date$:
 
-if ~isreal(u),
-    error('chebfun:ellipj:imaginaryinput', ...
-        'ellipj only accepts real inputs');
-end
-
 if nargin < 3
     tol = chebfunpref('eps');
 end
 
-% SN
-sn = comp(u, @(x) ellipj(x,m,tol));
-
-% CN
-if nargout >= 2
-    cn = comp(u, @(x) cnfun(x,m,tol));
-end
-
-% DN
-if nargout == 3
-    dn = comp(u, @(x) dnfun(x,m,tol));
+if isreal(u)
+    % SN
+    sn = comp(u, @(x) ellipj(x,m,tol));
+    % CN
+    if nargout >= 2
+        cn = comp(u, @(x) cnfun(x,m,tol));
+    end
+    % DN
+    if nargout == 3
+        dn = comp(u, @(x) dnfun(x,m,tol));
+    end
+    
+else
+    % Use imaginary transformations
+    [s c d] = ellipj(real(u),m,tol);        % real values
+    [s1 c1 d1] = ellipj(imag(u),1-m,tol);   % imaginary values
+    denom = c1.^2+m*(s.*s1).^2;
+    % SN
+    sn = (s.*d1+1i*c.*d.*s1.*c1)./denom;
+    % CN
+    if nargout >= 2, cn = (c.*c1-1i*s.*d.*s1.*d1)./denom; end
+    % DN
+    if nargout == 3, dn = (d.*c1.*d1-1i*m*s.*c.*s1)./denom; end  
+    
 end
 
 function cnout = cnfun(u,varargin)
