237d9d450d2b37ebfd5795ea11e02634eb51fa61
nich <nich@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2009-07-28 10:00:17 +0000

    Waterfall, surf and mesh plots for quasimatrices.

--- @chebfun/surf.m
+++ @chebfun/surf.m
@@ -1,7 +1,7 @@
-function varargout = waterfall(u,varargin)
-%WATERFALL Waterfall plot for quasimatrices.
+function varargout = surf(u,varargin)
+%SURF Waterfall plot for quasimatrices.
 %
-%  WATERFALL(U) or WATERFALL(U,T) where LENGTH(T) = MIN(SIZE(U))
+%  SURF(U) or SURF(U,T) where LENGTH(T) = MIN(SIZE(U))
 %
 %  See http://www.comlab.ox.ac.uk/chebfun for chebfun information.
 
@@ -24,47 +24,33 @@ if nargin > 1 && isnumeric(varargin{1}) && length(varargin{1}) == size(u,2)
 end
 
 if length(t)~=n
-    error('chebfun:waterfall:szet', ...
+    error('chebfun:surf:szet', ...
         'Length of T should equal the number of quasimatrices in U');
 end
 
 if ~isreal(u) || ~all(isreal(t))
-    warning('chebfun:waterfall:imaginary',...
+    warning('chebfun:surf:imaginary',...
         'Imaginary parts of complex T and/or U arguments ignored');
     u = real(u); t = real(t);
-end       
+end
 
 % get the data
-[data ignored data3] = plotdata([],u,[],numpts);
+data = plotdata([],u,[],numpts);
 uu = data{2:end};
 xx = repmat(data{1},1,n);
 tt = repmat(t,length(xx(:,1)),1);
 
-
 % mask the NaNs
 mm = find(isnan(uu));
-uu(mm) = uu(mm+1);
+uu(mm) = .5*(uu(mm+1)+uu(mm-1));
 
-% plot the waterfall
+% plot the surface
 if ~trans
-    h = waterfall(xx.',tt.',uu.',varargin{:});
+    h = surf(xx.',tt.',uu.',varargin{:});
 else
-    h = waterfall(xx,tt,uu,varargin{:});
+    h = surf(xx,tt,uu,varargin{:});
 end
 
-% hide the jumps
-if ~trans
-    x = []; y = []; z = [];
-    for k = 1:2:2*n
-        x = [x ; data3{k}];
-        y = [y ; t((k+1)/2)*ones(length(data3{k}),1)];
-        z = [z ; data3{k+1}];
-    end
-    ish = ishold;    hold on
-    plot3(x,y,z,'w');
-    if ~ish, hold off; end
-end
-    
 if nargout > 0
     varargout{1} = h;
 end
