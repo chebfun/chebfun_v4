df92747c686238fadfb48e30793ce9a3fab25609
nich <nich@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2009-09-01 10:52:26 +0000

    Added multiple-pinch maps and fixed multiple slit maps for the new map construction process.
    Both these maps require solving a nonlinear system, and we can't assume the user has fsolve from the optimisation toolbox, so I have also added Kelley's nsold code. There are probably issues with this that need to be discussed.
    
    Example:
    
    wk =   [ 0.08+0.007i
             -0.8+0.044i
             -0.7+0.083i
             0.27+0.04i 
             0.72+0.062i
             0.95+0.082i
             0.15+0.089i
                1+0.094i
             0.11+0.02i 
             0.04+0.026i
            -0.33+0.09i 
            -0.13+0.06i 
            -0.01+0.051i
            -0.85+0.062i
             0.78+0.082i];
    
    [d indx] = sort(real(wk)); e = imag(wk(indx));
    
    f = @(x) exp(x);
    for k = 1:length(wk)
        f =  @(x) f(x)+1./(1+((x-d(k))/e(k)).^2);
    end
    
    ff = chebfun(f)
    ff = chebfun(f,'map',{'mpinch',d+.8i*e})
    plot(ff)

---
+++ @fun/private/mpinch.m
@@ -0,0 +1,208 @@
+function  m = mpinch(par)
+% NH's multiple-pinch maps, which are generally far more stable than
+% multiple-slit maps when there are many slits.
+
+%  Copyright 2002-2009 by The Chebfun Team. 
+%  Last commit: $Author$: $Rev$:
+%  $Date$:
+
+a = par(1);
+b = par(2);
+W = par(3:end);
+
+if length(W) < 2, error('chebfun:mpinch','mpinch is for MULTIPLE (>2) pinches'); end
+
+% scale to handle arbitrary intervals
+scale = @(y) .5*((b-a)*y+b+a);
+scaleder = .5*(b-a);
+scaleinv = @(x) (2*x-(b+a))/(b-a);
+
+[G Gp Gi rho] = mpinchmap(scaleinv(W));
+m.par = [a b W(:).' rho];
+m.name = 'mpinch';
+
+m.for = @(y) scale(G(y));
+m.der = @(y) scaleder*Gp(y);
+m.inv = @(x) Gi(scaleinv(x));
+
+end
+
+function [G Gp Gi rho] = mpinchmap(wk, flag)
+if nargin == 0
+    % for testing
+    close all
+    M = 4;
+    C = 1e-2;
+    wk = 2*(rand(M,1)-.5)+1i*(rand(M,1));
+    wk = ceil(100*real(wk))/100+C*1i*ceil(100*imag(wk))/100;
+end
+
+wk = unique(wk(:));
+Np = length(wk);
+[ignored indx] = sort(real(wk)); 
+wk = wk(indx);
+
+alphak = zeros(Np); ginv_wk = zeros(Np,Np);
+gfor = cell(1,Np); ginv = cell(1,Np); ginvp = cell(1,Np); 
+for k = 1:Np
+    [gfor{k} ginv{k} ginvp{k} alphak(k,1)] = map(wk(k));
+    ginv_wk(:,k) = ginv{k}(wk);
+end
+
+% unconstrain
+a = ones(Np,1)/Np;
+vk = cumsum(a(1:end-1));
+theta = pi*[0;vk;1];
+phi = (theta(2:Np)-theta(1:Np-1))./(theta(3:Np+1)-theta(1:Np-1));
+yk = asin(2*phi-1);
+
+try
+    % fsolve
+    options = optimset('Display','off','Algorithm','Levenberg-marquardt');
+    options = optimset(options, 'TolX',1e-3,'TolFun',(1e-3)*min(abs(imag(wk))));
+    yk = fsolve(@(yk) pfun2(yk,ginv_wk,wk),yk,options);
+catch no_fsolve
+    % nsold
+    yk = nsold(yk,@(v) pfun2(v,ginv_wk,wk),1e-3*[1 min(abs(imag(wk)))]);
+end
+[F rho zk a] = pfun2(yk,ginv_wk,wk);
+
+% inverse map
+Gi = @(w) Ginv(w,a,ginv);
+
+% forward map 
+G = chebfun(@(s) newt(s,a,ginv,ginvp,wk),'minsamples',128,'maxdegree',512);
+Gp = diff(G);
+% G = simplify(G,1e-9);
+
+X = chebpts(length(G));
+G = @(x) bary(x,feval(G,X));
+Gp = @(x) bary(x,feval(Gp,X));
+
+if nargin == 1, return; end
+
+% plotting
+c = exp(pi*1i*linspace(0,1,1001));
+ell = .5*(rho*c+1./(rho*c));
+plot(G(ell),'k','linewidth',2); hold on
+plot(G(x),0*x,'o-k','markerfacecolor','k','markersize',4);
+for k = 1:9
+    rr = k*(rho-1)/10+1; 
+    ell = .5*(rr*c+1./(rr*c));
+    plot(G(ell),'--k');hold on
+end
+plot(wk,'ok','MarkerFaceColor','r');
+x = chebpts(56);
+
+rho_cheb = abs(wk+sqrt(wk.^2-1)); 
+rho_cheb = min(max(rho_cheb,1./rho_cheb))
+rho_pinch = rho
+
+title([ 'n = ', int2str(Np),',   \epsilon ~ ', num2str(C,'%1.0e'),...
+        ',  \rho_{cheb} = ', num2str(rho_cheb,5),',  \rho_{pinch} = ', num2str(rho,4)], ...
+        'fontsize',14)
+axis tight
+ax = [get(gca,'xlim') get(gca,'ylim')];
+axis(1.05*ax+[0 0 -.1*ax(4) 0]);
+end
+
+function [gfor ginv ginvp alphak] = map(wk)
+d = real(wk); e = imag(wk);
+alphak = pi/(asinh((1-d)/e)+asinh((1+d)/e));
+gfor = @(s) d+e*sinh(.5*pi/alphak*(s-1)+asinh((1-d)/e));
+ginv = @(s) 2*alphak/pi*(asinh((s-d)/e)+asinh((1+d)/e))-1;
+ginvp = @(s) 2*alphak./(pi*e*sqrt(1+((s-d)/e).^2));
+end
+
+function [F rho zk a] = pfun2(yk,ginv_wk,wk)
+% revert to constrained variables
+Np = length(yk)+1;
+psi = (sin(yk)+1)/2;
+LHS = diag(1-psi(2:Np-1),-1)-diag(ones(Np-1,1),0)+diag(psi(1:Np-2),1);
+rhs = [zeros(Np-2,1);-pi*psi(Np-1)];
+theta = LHS\rhs;                              
+vk = theta/pi; % vk = cumsum(a)
+a = [vk ; 1];
+for k = 2:length(a)
+    a(k) = a(k) - sum(a(1:k-1));
+end
+
+% preimages of tips
+zk = ginv_wk*a;
+
+% ellipses
+rk = abs(zk+sqrt(zk.^2-1));
+rk = max(rk,1./rk);
+
+[rho indx] = min(rk); rk(indx) = [];
+F = rho - rk;
+
+% scaling
+pp = wk; pp(indx) = [];
+ip = imag(pp); mip = min(imag(wk));
+arp = abs(real(pp));
+F = F.*(1+2*(F>0).*(exp(1e3*F)-1));
+F = F./(1+(F<0).*(exp(10*((arp>1).*(arp-1)+ip-mip))-1));
+end
+
+function H = Ginv(w,a,ginv)
+[m n] = size(w);
+w = w(:);
+H = 0;
+for k = 1:length(a)
+    H = H + a(k)*ginv{k}(w);
+end
+H = reshape(H,m,n);
+end
+
+function Hp = Ginvp(w,a,ginvp)
+[m n] = size(w);
+w = w(:);
+Hp = 0;
+for k = 1:length(a)
+    Hp = Hp + a(k)*ginvp{k}(w);
+end
+Hp = reshape(Hp,m,n);
+end
+
+function [H Hp] = Ginv2(w,a,ginv,ginvp)
+[m n] = size(w);
+w = w(:);
+H = 0; Hp = 0;
+for k = 1:length(a)
+    H = H + a(k)*ginv{k}(w);
+    Hp = Hp + a(k)*ginvp{k}(w);
+end
+H = reshape(H,m,n);
+Hp = reshape(Hp,m,n);    
+end
+
+function w = newt(s,a,ginv,ginvp,wk)
+if length(s) == 2, w = s; return, end 
+tol = min([1e-6 ; abs(imag(wk))]);
+opts = odeset('abstol',tol,'reltol',tol);
+[ignored w] = ode45(@(t,ww) 1./Ginvp(ww,a,ginvp),s,-1,opts);
+k = 0;
+err = inf;
+while err > 1e-10
+    k = k+1;
+    [H Hp] = Ginv2(w,a,ginv,ginvp);
+    step = (H-s)./Hp;
+    w = w-step;
+    err = norm(abs(H-s),inf);
+    if k > 20, 
+%         w = (-1).^[1:length(s)];
+        break;
+    end
+end
+end
+
+
+    
+
+
+
+
+
+
+
