8484cfc310a1a9098aef775456872a5c2a97e351
nich <nich@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2009-09-10 12:59:54 +0000

    Additions and tidying up of maps in @fun/private. Deprecated @fun/compress was removed.

--- @fun/private/mpinch.m
+++ @fun/private/mpinch.m
@@ -1,6 +1,14 @@
-function  m = mpinch(par)
-% NH's multiple-pinch maps, which are generally far more stable than
-% multiple-slit maps when there are many slits.
+function  m = mpinch(par,plotflag)
+% MPINCH - Multiple pinch maps: See Hale's thesis.
+%  M = MPINCH(PAR) returns a map structure for a slit map on the
+%  interval [PAR(1),PAR(2)] with singularities at PAR(3:END).
+%
+%  M = MPINCH(PAR,FLAG) will plot images of ellipses under the map.
+%  This is useful for testing and understanding maps.
+%
+%  See also slit, slitp, compress
+%
+%  See http://www.comlab.ox.ac.uk/chebfun for chebfun information.
 
 %  Copyright 2002-2009 by The Chebfun Team. 
 %  Last commit: $Author$: $Rev$:
@@ -9,53 +17,59 @@ function  m = mpinch(par)
 a = par(1);
 b = par(2);
 W = par(3:end);
-
-if length(W) < 2, error('chebfun:mpinch','mpinch is for MULTIPLE (>2) pinches'); end
-
+  
 % scale to handle arbitrary intervals
 scale = @(y) .5*((b-a)*y+b+a);
 scaleder = .5*(b-a);
 scaleinv = @(x) (2*x-(b+a))/(b-a);
 
-[G Gp Gi rho] = mpinchmap(scaleinv(W));
+if nargin == 2, plotflag = true; else plotflag = false; end
+
+if length(W) == 1
+    [G Gi Gp] = map(W);
+    zk = Gi(W);
+    rho = abs(zk+sqrt(zk^2-1));
+    rho = max(rho,1./rho);
+else
+    [G Gp Gi rho] = mpinchmap(scaleinv(W),plotflag);
+end
 m.par = [a b W(:).' rho];
 m.name = 'mpinch';
 
 m.for = @(y) scale(G(y));
 m.der = @(y) scaleder*Gp(y);
 m.inv = @(x) Gi(scaleinv(x));
+m.extra_pars = rho;
 
 end
 
-function [G Gp Gi rho] = mpinchmap(wk, flag)
-if nargin == 0
-    % for testing
-    close all
-    M = 4;
-    C = 1e-2;
-    wk = 2*(rand(M,1)-.5)+1i*(rand(M,1));
-    wk = ceil(100*real(wk))/100+C*1i*ceil(100*imag(wk))/100;
-end
-
-wk = unique(wk(:));
+function [G Gp Gi rho] = mpinchmap(wk, plotflag)
+wk = unique(wk(:)); 
 Np = length(wk);
 [ignored indx] = sort(real(wk)); 
 wk = wk(indx);
 
-alphak = zeros(Np); ginv_wk = zeros(Np,Np);
-gfor = cell(1,Np); ginv = cell(1,Np); ginvp = cell(1,Np); 
+% allocate space for single maps
+alphak = zeros(Np); 
+ginv_wk = zeros(Np,Np);
+gfor = cell(1,Np); 
+ginv = cell(1,Np); 
+ginvp = cell(1,Np); 
+
+% compute single maps
 for k = 1:Np
     [gfor{k} ginv{k} ginvp{k} alphak(k,1)] = map(wk(k));
     ginv_wk(:,k) = ginv{k}(wk);
 end
 
-% unconstrain
+% unconstrain parameter problem
 a = ones(Np,1)/Np;
 vk = cumsum(a(1:end-1));
 theta = pi*[0;vk;1];
 phi = (theta(2:Np)-theta(1:Np-1))./(theta(3:Np+1)-theta(1:Np-1));
 yk = asin(2*phi-1);
 
+% solve parameter problem
 try
     % fsolve
     wq = warning('query','MATLAB:optimset:InvalidParamName');
@@ -74,50 +88,46 @@ end
 Gi = @(w) Ginv(w,a,ginv);
 
 % forward map 
-G = chebfun(@(s) newt(s,a,ginv,ginvp,wk),'minsamples',128,'maxdegree',512);
+G = chebfun(@(s) newt(s,a,ginv,ginvp,wk),1024);
+G = simplify(G,chebfunpref('eps'));
 Gp = diff(G);
-% G = simplify(G,1e-9);
 
+% construct barycentric interpolants
 X = chebpts(length(G));
 G = @(x) bary(x,feval(G,X));
 Gp = @(x) bary(x,feval(Gp,X));
 
-if nargin == 1, return; end
+if ~plotflag return, end
+
+%%%%%%%%%%%%%%%%%%%%% FOR TESTING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % plotting
 c = exp(pi*1i*linspace(0,1,1001));
 ell = .5*(rho*c+1./(rho*c));
+ish = ishold;
 plot(G(ell),'k','linewidth',2); hold on
-plot(G(x),0*x,'o-k','markerfacecolor','k','markersize',4);
+plot(G(X),0*X,'o-k','markerfacecolor','k','markersize',4);
 for k = 1:9
     rr = k*(rho-1)/10+1; 
     ell = .5*(rr*c+1./(rr*c));
     plot(G(ell),'--k');hold on
 end
 plot(wk,'ok','MarkerFaceColor','r');
-x = chebpts(56);
-
-rho_cheb = abs(wk+sqrt(wk.^2-1)); 
-rho_cheb = min(max(rho_cheb,1./rho_cheb))
-rho_pinch = rho
-
-title([ 'n = ', int2str(Np),',   \epsilon ~ ', num2str(C,'%1.0e'),...
-        ',  \rho_{cheb} = ', num2str(rho_cheb,5),',  \rho_{pinch} = ', num2str(rho,4)], ...
-        'fontsize',14)
 axis tight
 ax = [get(gca,'xlim') get(gca,'ylim')];
 axis(1.05*ax+[0 0 -.1*ax(4) 0]);
+if ~ish, hold off, end
 end
 
-function [gfor ginv ginvp alphak] = map(wk)
-d = real(wk); e = imag(wk);
-alphak = pi/(asinh((1-d)/e)+asinh((1+d)/e));
-gfor = @(s) d+e*sinh(.5*pi/alphak*(s-1)+asinh((1-d)/e));
-ginv = @(s) 2*alphak/pi*(asinh((s-d)/e)+asinh((1+d)/e))-1;
-ginvp = @(s) 2*alphak./(pi*e*sqrt(1+((s-d)/e).^2));
+function [gfor ginv ginvp alphak] = map(wk) % single strip-to-slit maps
+    d = real(wk); e = imag(wk);
+    alphak = pi/(asinh((1-d)/e)+asinh((1+d)/e));
+    gfor = @(s) d+e*sinh(.5*pi/alphak*(s-1)+asinh((1-d)/e));
+    ginv = @(s) 2*alphak/pi*(asinh((s-d)/e)+asinh((1+d)/e))-1;
+    ginvp = @(s) 2*alphak./(pi*e*sqrt(1+((s-d)/e).^2));
 end
 
-function [F rho zk a] = pfun2(yk,ginv_wk,wk)
+function [F rho zk a] = pfun2(yk,ginv_wk,wk) % objective function
 % revert to constrained variables
 Np = length(yk)+1;
 psi = (sin(yk)+1)/2;
@@ -144,11 +154,10 @@ F = rho - rk;
 pp = wk; pp(indx) = [];
 ip = imag(pp); mip = min(imag(wk));
 arp = abs(real(pp));
-F = F.*(1+2*(F>0).*(exp(1e3*F)-1));
 F = F./(1+(F<0).*(exp(10*((arp>1).*(arp-1)+ip-mip))-1));
 end
 
-function H = Ginv(w,a,ginv)
+function H = Ginv(w,a,ginv) % compute G inverse
 [m n] = size(w);
 w = w(:);
 H = 0;
@@ -158,7 +167,7 @@ end
 H = reshape(H,m,n);
 end
 
-function Hp = Ginvp(w,a,ginvp)
+function Hp = Ginvp(w,a,ginvp) % compute G inverse prime
 [m n] = size(w);
 w = w(:);
 Hp = 0;
@@ -168,7 +177,7 @@ end
 Hp = reshape(Hp,m,n);
 end
 
-function [H Hp] = Ginv2(w,a,ginv,ginvp)
+function [H Hp] = Ginv2(w,a,ginv,ginvp)  % compute G inverse and Ginv prime
 [m n] = size(w);
 w = w(:);
 H = 0; Hp = 0;
@@ -180,32 +189,32 @@ H = reshape(H,m,n);
 Hp = reshape(Hp,m,n);    
 end
 
-function w = newt(s,a,ginv,ginvp,wk)
-if length(s) == 2, w = s; return, end 
-tol = min([1e-6 ; abs(imag(wk))]);
-opts = odeset('abstol',tol,'reltol',tol);
-[ignored w] = ode45(@(t,ww) 1./Ginvp(ww,a,ginvp),s,-1,opts);
-k = 0;
-err = inf;
-while err > 1e-10
-    k = k+1;
-    [H Hp] = Ginv2(w,a,ginv,ginvp);
-    step = (H-s)./Hp;
-    w = w-step;
-    err = norm(abs(H-s),inf);
-    if k > 20, 
-%         w = (-1).^[1:length(s)];
-        break;
+% invert the inverse via fsolve or ode45 + Newton iterations
+function w = newt(s,a,ginv,ginvp,wk)  
+    if length(s) == 2, w = s; return, end
+    tol = chebfunpref('eps');
+    try
+        % fsolve
+        options = optimset('Display','off','Algorithm','trust-region-reflective');
+        options = optimset(options, 'TolX',tol,'TolFun',tol);
+        options = optimset(options,'Jacobian','off','JacobPattern',speye(length(s)));
+        w = fsolve(@(ww) Ginv(ww,a,ginv)-s,s,options);
+    catch no_fsolve
+        % ode45 and Newton
+%         w = nsold(s,@(ww) Ginv(ww,a,ginv)-s,[tol tol],[40, 1, 1, 1, 1 1]);  
+        tol2 = min([1e-7 ; abs(imag(wk))]);
+        opts = odeset('abstol',tol,'reltol',tol2,'vectorized',1);
+        [ignored w] = ode45(@(t,ww) 1./Ginvp(ww,a,ginvp),s,-1,opts);
+        norm(Ginv(w,a,ginv)-s,inf)
+        k = 0; err = inf; step = inf;
+        while err > 1e-15 && norm(step,inf) > 1e-14
+            k = k+1;
+            [H Hp] = Ginv2(w,a,ginv,ginvp);
+            step = (H-s)./Hp;
+            step = min(step,.001);
+            w = w-step;
+            err = norm(abs(H-s),inf);
+            if k > 50, break; end
+        end
     end
 end
-end
-
-
-    
-
-
-
-
-
-
-
--- @fun/private/slit.m
+++ @fun/private/slit.m
@@ -1,179 +1,167 @@
-function  m = slit(par)
-% Multiple-slit maps. See Tee's thesis or Hale & Tee 2009.
-
-%  Copyright 2002-2009 by The Chebfun Team. 
-%  Last commit: $Author$: $Rev$:
-%  $Date$:
-
-a = par(1);
-b = par(2);
-W = par(3:end);
-
-[m.for m.der] = slitmap(W,[a b]);
-m.par = [a b W(:).'];
-m.name = 'slit';
-
-end
-
-function varargout = slitmap(w,dom,y)
-% SLITMAP - conformal map to slit region
-%  G = SLITMAP(W,D) returns a chebfun G which maps D to itself, and
-%  conformally maps and ellipse with foci d.ends to entire complex 
-%  plane minus the vertical congugate slits with tips at W. If D is
-%  not given, it is assumed to be domain(-1,1).
+function  m = slit(par,plotflag)
+% SLIT - Slit maps: See Tee's thesis or Hale & Tee 2009.
+%  M = SLIT(PAR) returns a map structure for a slit map on the
+%  interval [PAR(1),PAR(2)] with singularities at PAR(3:END).
 %
-%  [G GP] = SLITMAP(W,D) returns also the derivative values at those 
-%  points.
+%  M = SLIT(PAR,FLAG) will plot images of ellipses under the map.
+%  This is useful for testing and understanding maps.
 %
-%  [G GP RHO] = SLITMAP(W,D) returns also the derivative values at 
-%  the parameter RHO which determines the size of the ellipse.
-%
-%  [G GP RHO Y] = SLITMAP(W,D) returns also a vector Y of the 
-%  paramter values of the map. SLITMAP(W,D,Y) can use such a vector
-%  as an initial guess.
+%  See also mpinch, slitp
 %
 %  See http://www.comlab.ox.ac.uk/chebfun for chebfun information.
 
-%  Copyright 2002-2009 by The Chebfun Team. 
+%  Copyright 2002-2009 by The Chebfun Team.
 %  Last commit: $Author$: $Rev$:
 %  $Date$:
 
-if nargin == 1 
-    if ~isnumeric(w), error('chebfun:domain:slitmap:arg1',...
-        'First argument must be slit positions'); end
-    dom = [-1 1];
-elseif length(dom) > 2
-    y = dom; 
-    dom = [-1 1];
-end
+    a = par(1);
+    b = par(2);
+    w = par(3:end);
 
-% tol = chebfunpref('eps');
-tol = eps;
-
-a = dom(1); b = dom(2);
-l = @(x) .5*(b-a)*(x-1) - a;
-linv = @(x) 2*(x-a)/(b-a) - 1;
-
-if length(w) == 1 % single slit
-    varargout{1} = @(z) l(slitmap1(linv(w),linv(z)));
-    if nargout > 1
-        varargout{2} = @(z) slitmap1(linv(w),linv(z),1);
+    if ~isnumeric(w) || isempty(w),
+        error('chebfun:slit:arg1',...
+            'First argument must be slit positions');
     end
-    if nargout > 2, slitmap1(linv(w),linv([a b]),1); y = []; end
-else  % multiple slits
-    if nargin < 3, % compute map paramters
-        y = []; 
-        slitmapm(linv(w),linv([a b]));
-    end
-    % construct chebfun
-    varargout{1} = @(z) l(slitmapm(linv(w),linv(z),0));
-    if nargout > 1
-        varargout{2} = @(z) slitmapm(linv(w),linv(z),0,1);
-    end
-end
 
-if nargout == 3
-    varargout{3} = rho;
-end
-if nargout == 4
-    varargout{4} = y;
+    if nargin == 2, plotflag = true; else plotflag = false; end
+    
+    % interval scaling
+    l = @(x) .5*(b-a)*(x-1) - a;
+    linv = @(x) 2*(x-a)/(b-a) - 1;
+    
+    if length(w) == 1 % single slit
+        m.for = @(z) l(slitmap1(linv(w),linv(z)));
+        m.der = @(z) slitmap1(linv(w),linv(z),1);
+        [ignored rho] = slitmap1(linv(w),linv(0));   
+        y = [];
+    else   % multiple slits
+        % compute map paramters
+        [ignored rho y] = slitmapm(linv(w),linv([a b]),[],1,0,plotflag);
+        m.for = @(z) l(slitmapm(linv(w),linv(z),y,0,0,0))
+        m.der = @(z) slitmapm(linv(w),linv(z),y,0,1,0);
+    end
+
+    m.par = [a b w(:).'];
+    m.name = 'slit';
+    m.extra_pars = [rho y(:).'];
 end
 
 % ---------------------------------------------------------
 
-    function gout = slitmap1(wk,z,ignored)
-        d = real(wk); e = imag(wk);
-        c = sign(d)*realsqrt(0.5*((d^2+e^2+1)-realsqrt((d^2+e^2+1)^2-4*d^2)));
-        s = realsqrt(1-c^2);
-        m14 = (-e+realsqrt(e^2+s^2))/s;   m = m14^4;
+function [gout rho] = slitmap1(wk,z,ignored) % single pair of slits
+    d = real(wk); e = imag(wk);
+    c = sign(d)*realsqrt(0.5*((d^2+e^2+1)-realsqrt((d^2+e^2+1)^2-4*d^2)));
+    s = realsqrt(1-c^2);
+    m14 = (-e+realsqrt(e^2+s^2))/s;   m = m14^4;
+    try % sc toolbox
+        L = -2*log(m14)/pi;
+        [K Kp] = ellipkkp(L);
+        [sn cn dn] = ellipjc(2*K*asin(z)/pi,L);
+    catch no_sc
+        K = ellipke(m);
+        Kp = ellipke(1-m);
+        [sn cn dn] = ellipj(2*K*asin(z)/pi,m);
+    end
+    h1 = m14*sn;
+    if nargin == 2   % gout = g
+        gout = c/m14+((1-m14^2)/m14)*(h1-c)./(1-h1.^2);
+    else             % gout = gp
+        h1p = 2*K/pi*m14*(cn.*dn)./sqrt(1-z.^2);
+        h1p(abs(real(z))==1) = (2*K/pi)^2*m14*(1-m);
+        gout = (1-m14^2)/m14*h1p.*(1+h1.^2-2*c*h1)./(1-h1.^2).^2;
+    end
+    rho = exp(pi*Kp/(4*K));
+end
+
+% ---------------------------------------------------------
+
+function [gout rho y] = slitmapm(wk,z,y,paramflag,derflag,plotflag) % Multiple slits
+    % options
+    cmax = 25;     % max number of iterations in Newton loop to find preimage of slit tips
+    tol = 1e-10;   % tolerance
+         
+    if nargin < 3, y = []; paramflag = 1; end  % no paramter guess
+    if nargin < 4, paramflag = 1; end   % solve paramter problem
+    if nargin < 5, derflag = 0; end     % return derivative?
+    if nargin < 6, plotflag = 0; end     % return derivative?
+    
+    d = real(wk); e = imag(wk);
+    [d,index] = sort(d(:),'descend'); e = e(index); e = e(:);
+    wk = d(:)+1i*e(:);                        % slit tips (in upper-half plane)
+    n = length(d);
+
+    if length(y) ~= n
+        m14 = 0.5;
+        theta = linspace(0,pi,n+1)';
+        phi = (theta(2:n)-theta(1:n-1))./(theta(3:n+1)-theta(1:n-1));
+        y = asin(2*[m14;phi]-1);
+    end
+
+    if paramflag
+        fdat = {wk,n,tol,cmax};                % package data
         try
-            L = -2*log(m14)/pi;
-            [K Kp] = ellipkkp(L);
-            rho = exp(pi*Kp/(4*K));
-            [sn cn dn] = ellipjc(2*K*asin(z)/pi,L);
-        catch ME1
-            K = ellipke(m);
-            Kp = ellipke(1-m);
-            rho = exp(pi*Kp/(4*K));
-            [sn cn dn] = ellipj(2*K*asin(z)/pi,m);
-        end
-        h1 = m14*sn;
-        if nargin == 2   % gout = g
-            gout = c/m14+((1-m14^2)/m14)*(h1-c)./(1-h1.^2);
-        else             % gout = gp
-            h1p = 2*K/pi*m14*(cn.*dn)./sqrt(1-z.^2);
-            h1p(abs(real(z))==1) = (2*K/pi)^2*m14*(1-m);
-            gout = (1-m14^2)/m14*h1p.*(1+h1.^2-2*c*h1)./(1-h1.^2).^2;
-        end
-    end
-
-% ---------------------------------------------------------
-
-    function gout = slitmapm(wk,z,pflag,ignored) % Multiple slits 
-        
-        % options
-        cmax = 25;     % max number of iterations in Newton loop to find preimage of slit tips
-
-        if nargin < 3, pflag = true; end
-
-        d = real(wk); e = imag(wk);
-        [d,index] = sort(d(:),'descend'); e = e(index); e = e(:);
-        wk = d(:)+1i*e(:);                        % slit tips (in upper-half plane)
-        n = length(d);
-
-        if length(y) ~= n
-            m14 = 0.5;
-            theta = linspace(0,pi,n+1)';
-            phi = (theta(2:n)-theta(1:n-1))./(theta(3:n+1)-theta(1:n-1));
-            y = asin(2*[m14;phi]-1); 
-        end
-
-        if pflag
-            fdat = {wk,n,tol,cmax};               % package data
+            % fsolve
             opts = optimset('tolfun',tol,'tolx',tol,'display','off');
-            [y fval flag] = fsolve(@(x)mapfun(x,fdat),y,opts); % nonlinear system solver
-            if flag~=1, warning('chebfun:slitmap', ...
-                    'Nonlinear equation solver did not terminate normally'); end
-%             if fval>sqrt(tol), warning('chebfun:slitmap', ...
-%                     ['Catastrophic error! ',num2str(norm(fval,inf))]); send
+            y = fsolve(@(x)mapfun(x,fdat),y,opts); % nonlinear system solver
+        catch no_fsolve
+            % nsold
+            y = nsold(y,@(x)mapfun(x,fdat),[tol tol]);
         end
 
-        psi = (sin(y(2:n))+1)/2;
-        m14 = (sin(y(1))+1)/2;          m12 = m14^2;        m = m14^4; 
-        try
-            L = -2*log(m14)/pi;
-            [K Kp] = ellipkkp(L);
-            rho = exp(pi*Kp/(4*K));
-            [sn cn dn] = ellipjc(2*K*asin(z)/pi,L);
-        catch ME2
-            K = ellipke(m);
-            Kp = ellipke(1-m);
-            rho = exp(pi*Kp/(4*K));
-            [sn cn dn] = ellipj(2*K*asin(z)/pi,m);
-        end
-        h1 = m14*sn;        % h1 map
-
-        rhs = [zeros(n-2,1);-pi*psi(n-1)];
-        LHS = diag(1-psi(2:n-1),-1)-diag(ones(n-1,1),0)+diag(psi(1:n-2),1);
-        theta = LHS\rhs;
-        zk = exp(1i*theta);
-
-        ak = diff(d)/pi;
-        p = PHI([-m14;m14],ak,zk);
-        M = (1-m14)^2/(4*m14); M = [2/(m14^2-1) .5 .5;[-1 -M 1+M;1 -1-M M]];
-        lhs = -(1-m12)/(1+m12)*(M*[ak'*(theta-pi)+d(1) ; -1-p(1) ; 1-p(2)]);
-        A = lhs(1); a0 = lhs(2); b0 = lhs(3);
-
-        if nargin < 4
-            gout = A + (a0./(h1-1) + b0./(h1+1)) + PHI(h1,ak,zk);
-        else
-            [ignored, phip] = PHI(h1,ak,zk);
-            h1p = (abs(z)~=1).*(2*K/pi*m14).*cn.*dn./sqrt(1+(abs(z)==1)*eps-z.^2)+(abs(z)==1).*(2*K/pi)^2*m14*(1-m);
-            gout = h1p.*(-a0./(h1-1).^2-b0./(h1+1).^2 + phip);  
-        end
-
-        rho = exp(pi*Kp/(4*K));
     end
+
+    psi = (sin(y(2:n))+1)/2;
+    m14 = (sin(y(1))+1)/2;
+    m12 = m14^2;
+    m = m14^4;
+
+    try % sc toolbox
+        L = -2*log(m14)/pi;
+        [K Kp] = ellipkkp(L);
+        [sn cn dn] = ellipjc(2*K*asin(z)/pi,L);
+    catch no_sc
+        K = ellipke(m);
+        Kp = ellipke(1-m);
+        [sn cn dn] = ellipj(2*K*asin(z)/pi,m);
+    end
+    
+    h1 = m14*sn;        % h1 map
+
+    rhs = [zeros(n-2,1);-pi*psi(n-1)];
+    LHS = diag(1-psi(2:n-1),-1)-diag(ones(n-1,1),0)+diag(psi(1:n-2),1);
+    theta = LHS\rhs;
+    zk = exp(1i*theta);
+
+    ak = diff(d)/pi;
+    p = PHI([-m14;m14],ak,zk);
+    M = (1-m14)^2/(4*m14); M = [2/(m14^2-1) .5 .5;[-1 -M 1+M;1 -1-M M]];
+    lhs = -(1-m12)/(1+m12)*(M*[ak'*(theta-pi)+d(1) ; -1-p(1) ; 1-p(2)]);
+    A = lhs(1); a0 = lhs(2); b0 = lhs(3);
+
+    if ~derflag
+        gout = A + (a0./(h1-1) + b0./(h1+1)) + PHI(h1,ak,zk);
+    else
+        [ignored, phip] = PHI(h1,ak,zk);
+        h1p = (abs(z)~=1).*(2*K/pi*m14).*cn.*dn./sqrt(1+(abs(z)==1)*eps-z.^2)+(abs(z)==1).*(2*K/pi)^2*m14*(1-m);
+        gout = h1p.*(-a0./(h1-1).^2-b0./(h1+1).^2 + phip);
+    end
+
+    rho = exp(pi*Kp/(4*K));
+    
+    if ~plotflag, return, end
+
+    c = .999999*exp(1i*pi*linspace(0,1,10000)).';
+    ell = (1-1e-14)*rho*c; ell = .5*(ell+1./ell);
+    LW = 'LineWidth'; MS = 'MarkerSize';
+    ish = ishold;
+    plot(d,e,'xr',LW,3,MS,8) ; hold on
+    plot(slitmapm(wk,ell,y,0,0,0),'-r',LW,2);
+    axis([pi*[-1,1],2*[0,1]])
+    for k = [1:9]/10
+        ell = (1+k*(rho-1))*c; ell = .5*(ell+1./ell);  
+        plot(slitmapm(wk,ell,y,0,0,0),'--k');
+    end
+    if ~ish, hold off, end
 end
 
 
@@ -182,14 +170,14 @@ function F = mapfun(y,fdat)
     [wk, n, tol, cmax] = deal(fdat{:});                  % package data
 
     m14 = (sin(y(1))+1)/2;
-    m12 = m14^2;
+    m12 = m14^2;        m = m14^4;
     psi = (sin(y(2:n))+1)/2;
     rhs = [zeros(n-2,1);-pi*psi(n-1)];
     LHS = diag(1-psi(2:n-1),-1)-diag(ones(n-1,1),0)+diag(psi(1:n-2),1);
     theta = LHS\rhs;                                     % constrained angles
     zk = exp(1i*theta); zkbar = conj(zk);
 
-    d = real(wk);
+    d = real(wk); e = imag(wk);
     ak = diff(d)/pi;
     p = PHI([-m14;m14],ak,zk);
 
@@ -221,6 +209,7 @@ function F = mapfun(y,fdat)
         z = exp(1i*s);     ZZ = repmat(z.',n-1,1);
         gp = -a0./(z-1).^2-b0./(z+1).^2 + 1i*(1./(ZZ-ZK)-1./(ZZ-ZKbar)).'*ak;
         normgp = norm(abs(gp),inf);
+        ds = norm(imag(gp)./imag(gpp),inf);
     end
 
     if (count > cmax)
@@ -231,10 +220,12 @@ function F = mapfun(y,fdat)
     F = imag(ff-wk);
 end
 
+
+
 function [ff ffp] = PHI(zz,ak,zk)
     sz = size(zz); zz = zz(:);
     nz = length(zz); nzk = length(zk);
-    ZK = repmat(zk,1,nz); 
+    ZK = repmat(zk,1,nz);
     ZKbar = repmat(conj(zk),1,nz);
     ZZ = repmat(zz.',nzk,1);
     ZZ1 = ZZ-ZK; idx1 = find(real(ZZ1)<0 & imag(ZZ1)>=0);
@@ -248,11 +239,3 @@ function [ff ffp] = PHI(zz,ak,zk)
         ffp = reshape(ffp,sz);
     end
 end
-
-
-
-
-
-
-
-
