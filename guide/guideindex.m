f61aec8631e9121b622f5a98c18f4b27baa7dd25
trefethen <trefethen@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2009-11-18 18:10:18 +0000

    Our GUIDE should have an index.  I've started one and will continue to expand it.  This chapter of the guide doesn't get a signature -- feel free to update it yourself if so inclined.
    Is one of you clever enough to install a search facility in
    our guide pages?

---
+++ guide/guideindex.m
@@ -0,0 +1,85 @@
+%% CHEBFUN GUIDE: INDEX
+
+%%
+% Entries are listed by section number.
+
+%% A
+%  ABS, 1.4
+%  accuracy, 1.1
+%  Assheton, Phil, 1.1
+%% B
+%  barycentric interpolation, 1.2
+%  Battles, Zachary, 1.1
+%  Bernstein, Sergey, 1.1
+%  Bessel functions, 1.2
+%  Birkisson, Asgeir, 1.1
+%  Bornemann, Folkmar, 1.1
+%  breakpoints, 1.4
+%% C
+%  CHEBFUN, 1.2
+%  Chebyshev points, 1.2
+%  Chebyshev polynomial, 1.1
+%  Clenshaw-Curtis quadrature, 1.2, 2.1
+%% D
+%  diverging functions, 1.5
+%  DOMAIN, 1.3
+%  Driscoll, Toby, 1.1
+%% E
+%  edge detection, 1.4
+%% F
+%  factory values, 1.4
+%  FFT, 1.1
+%  File Exchange, 1.1
+%  funs, 1.4
+%% G
+%  Gonnet, Pedro, 1.1
+%% H
+%  Hale, Nick, 1.1, 1.5
+%  Higham, Nick, 1.2
+%% I
+%  infinite function values, 1.5
+%  infinite intervals, 1.5
+%  inner product, 1.6
+%  integral, 1.2
+%  interpolation, 1.2
+%% J
+%% K
+%% L
+%  LENGTH, 1.2, 1.4
+%% M
+%  METHODS, 1.3
+%% N
+%% O
+%  Olver, Sheehan, 1.1
+%  overloading, 1.1
+%% P
+%  Pachon, Ricardo, 1.1, 1.4
+%  piecewise smooth chebfuns, 1.4
+%  Platte, Rodrigo, 1.1, 1.4, 1.5
+%  PLOT, 1.2
+%  PLUS, 1.3
+%  POWER, 1.3
+%% Q
+%  quasimatrices, 1.6
+%% R
+%  Richardson, Mark, 1.1, 1.5
+%  ROOTS, 1.1
+%  rounding error, 1.1
+%  row vs. column chebfuns, 1.6
+%  RDIVIDE, 1.3
+%  Runge functions, 1.2
+%% S
+%  Salzer, Herbert, 1.2
+%  splitting, 1.4
+%  STRUCT, 1.4
+%  SUBSREF, 1.3
+%  SUM, 1.2
+%% T
+%  transpose, 1.6
+%% U
+%% V
+%  Van Deun, Joris, 1.1
+%% W
+%% X
+%% Y
+%% Z
