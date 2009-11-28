91eab2fcf6c151fd81ca8ce92df55a3ca3ba3856
trefethen <trefethen@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2009-11-28 11:55:44 +0000

    changed the "scribbles" test a little to check for approximate rather than exact inequality and thus avoid a test failure.

--- chebtests/scribbles.m
+++ chebtests/scribbles.m
@@ -1,9 +1,9 @@
 function pass = scribbles
-% scribblestest.m - uses "scribble" to test various things
+% scribbles.m - uses "scribble" to test various things
 %    related to piecewise defined complex chebfuns
 %    Nick Trefethen November 2009
 
 f = scribble('rex');
 pass(1) = (norm(f)==norm(f'));
 pass(2) = max(imag(f))==-min(imag(f'));
-pass(3) = norm(f,inf)==norm([f;f],inf);
+pass(3) = (norm(f,inf)-norm([f;f],inf)) < 2^(-50);
