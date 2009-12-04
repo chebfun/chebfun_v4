176dbb33c8a62c11b6fd08665afdd81d994c0116
platte <platte@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2009-12-04 16:47:38 +0000

    Improvements in cf.m by Joris.

--- @chebfun/cf.m
+++ @chebfun/cf.m
@@ -1,63 +1,107 @@
-function [p,q,s] = cf(f,m,n,M)
+function [p,q,r,s] = cf(f,m,n,M)
 % Caratheodory-Fejer approximation
 %
-% [P,Q] = CF(F,M,N): degree (M/N) rational CF approximant to chebfun F
-%         defined in [a b] (which must consist of just a single fun)
+% [P,Q,R] = CF(F,M,N): degree (M/N) rational CF approximant to chebfun F
+%         defined in [a b] (which must consist of just a single fun);
+%         P and Q are chebfuns representing the numerator and denominator
+%         polynomials, R is an anonymous function that evaluates P/Q
 %
-% [P,Q,S] = CF(F,M,N): same plus S, the associated CF
-%           singular value, an approximation to the minimax error
+% [P,Q,R,S] = CF(F,M,N): same plus S, the associated CF
+%         singular value, an approximation to the minimax error
 %
 % ... = CF(F,M,N,K): use only K-th partial sum in Chebyshev expansion of F
 %
 % For polynomial CF approximation, use N = 0 or N = [] or only provide two
-% input arguments. If P and S are required, three output arguments must be
+% input arguments. If P and S are required, four output arguments must be
 % specified.
+%
+% If F is a quasimatrix then so are the outputs P & Q, R is a cell array of
+% function handles and s is a vector.
+%
+% Rational CF approximation can be very ill-conditioned for non-smooth
+% functions. If the program detects this, a warning message is given and
+% the results may be wrong.
 %
 % Example:
 %   f = chebfun('log(1.2+cos(exp(2*x)))');
 %   [p,q] = cf(f,10,10);
 %   plot(f-p./q)
 %
-% CF = Caratheodory-Fejer approximation is a near-best approximation
-% that is often indistinguishable from the true best approximation
-% as computed by REMEZ(F,M), but often much faster to compute. 
-% See M. H. Gutknecht and L. N. Trefethen, "Real polynomial Chebyshev
-% approximation by the Caratheodory-Fejer method", SIAM J. Numer.
-% Anal. 19 (1982), 358-371, and L. N. Trefethen and M. H. Gutknecht, 
-% "The Caratheodory-Fejer method for real rational approximation", SIAM J.
+% CF = Caratheodory-Fejer approximation is a near-best approximation that
+% is often indistinguishable from the true best approximation (which for
+% polynomials can be computed using REMEZ(F,M)), but often much faster to
+% compute. See M. H. Gutknecht and L. N. Trefethen, "Real polynomial
+% Chebyshev approximation by the Caratheodory-Fejer method", SIAM J. Numer.
+% Anal. 19 (1982), 358-371, and L. N. Trefethen and M. H. Gutknecht, "The
+% Caratheodory-Fejer method for real rational approximation", SIAM J.
 % Numer. Anal. 20 (1983), 420-436.
 %
 % See http://www.comlab.ox.ac.uk/chebfun for chebfun information.
 
 % Copyright 2009 by The Chebfun Team. 
 
-if numel(f) > 1, error('CHEBFUN:cf:quasi',...
-  'CF does not currently support quasimatrices'); end
-
-if any(get(f,'exps')), error('CHEBFUN:cf:inf',...
-  'CF does not support functions with nonzero exponents'); end
-
-if (nargin < 4), M = length(f) - 1; end
 if (nargin < 3), n = 0; end
 
-tolfft = 1e-14; maxnfft = 2^18;
+if numel(f) > 1, % Deal with quasimatrices    
+    trans = false;
+    if get(f,'trans')
+        f = f.';        trans = true;
+    end
+
+    r = cell(1,numel(f)); p = chebfun; q = chebfun;
+    s = zeros(1,numel(f));
+    % loop over chebfuns
+    for k = 1:numel(f)
+      if (nargin < 4), M = length(f(:,k)) - 1; end
+      [p(:,k) q(:,k) r{k} s(k)] = cf(f(:,k),m,n,M);
+    end
+    if trans
+        r = r.'; p = p.'; q = q.';
+    end
+    return
+end
+
+if f.nfuns > 1, error('CHEBFUN:cf:multiple_funs',...
+  'CF does not support chebfuns consisting of multiple funs.'); end
+
+if any(get(f,'exps')), error('CHEBFUN:cf:inf',...
+  'CF does not support functions with nonzero exponents.'); end
+
+if (nargin < 4), M = length(f) - 1; end
+
+tolfft = 1e-14; maxnfft = 2^17; % relative tolerance and max length fft
+tolcond = 1e13; tolcondG = 1e3; % tolerances to detect ill-conditioning
 
 d = domain(f);
+if any(isinf(d.ends)), error('CHEBFUN:cf:inf_domain',...
+  'CF does not work on infinite domains.'); end
 
 if m >= M,
-  p = f; q = chebfun(1,d); s = 0;
+  p = f; q = chebfun(1,d); r = @(x) feval(p,x); s = 0;
   return
 end
 
 a = chebpoly(f); a = a(end-M:end);
+if any(imag(a) ~= 0),
+  warning('chebfun:cf:complex', ...
+    ['CF does not work for complex valued functions. Taking real part.']);
+  a = real(a);
+end
 
 if (n == 0) | isempty(n),         % polynomial case
   if m==M-1
     p = chebfun(chebpolyval(a(2:M+1)),d); q = chebfun(1,d);
+    r = @(x) feval(p,x); s = abs(a(1));
     return
   end
   c = a(M-m:-1:1);
-  [V,D] = eig(hankel(c));
+  if length(c) > 1024,
+    opts.disp = 0; opts.issym = 1; opts.isreal = 1;
+    opts.v0 = ones(length(c),1)/length(c);
+    [V,D] = eigs(hankel(c),1,'lm',opts);
+  else
+    [V,D] = eig(hankel(c));
+  end
   [s,i] = max(abs(diag(D)));
   u = V(:,i);
   u1 = u(1); uu = u(2:M-m);
@@ -67,9 +111,23 @@ if (n == 0) | isempty(n),         % polynomial case
   end
   pk = a(M-m+1:M+1) - [b(1:m) 0] - b(2*m+1:-1:m+1);
   p = chebfun(chebpolyval(pk),d);
-  q = chebfun(1,d);
+  q = chebfun(1,d); r = @(x) feval(p,x);
 else                              % rational case
   a(end) = 2*a(end);
+  % test for even or odd functions
+  if max(abs(a(end-1:-2:1)))/f.scl < eps, % f is an even function
+    if ~(mod(m,2)||mod(n,2)),
+      m = m + 1;
+    elseif mod(m,2) && mod(n,2),
+      n = n - 1;
+    end
+  elseif max(abs(a(end:-2:1)))/f.scl < eps, % f is an odd function
+    if mod(m,2) && ~mod(n,2),
+      m = m + 1;
+    elseif ~mod(m,2) && mod(n,2),
+      n = n - 1;
+    end
+  end
   
   % obtain eigenvalues and block structure
   [s,u,k,l,rflag] = getblock(a,m,n,M);
@@ -103,9 +161,14 @@ else                              % rational case
   for j = 1:n,
     b(j+1) = -(b(1:j)*ac(end-j:end-1))/j;
   end
-  rho = 1/max(abs(roots(b)));
-  b = hankel(b)*b.'/sum(b.^2); b = b(end:-1:1); b(end) = b(end)/2;
-  q = simplify(chebfun(chebpolyval(b),d));
+  z = roots(b);
+  if any(abs(z)>1), warning('chebfun:cf:ill_conditioned', ...
+      ['Ill-conditioning detected. Results may be inaccurate.']);
+  end
+  z = z(abs(z)<1); rho = 1/max(abs(z)); z = .5*(z+1./z);
+  % compute q from the roots for stability reasons
+  qt = chebfun(@(x) real(prod(x-z)/prod(-z)),'vectorize');
+  q = chebfun; q = define(q,d,qt);
  
   % compute Chebyshev coeffs of approximation Rt from Laurent coeffs of
   % Blaschke product using fft
@@ -133,9 +196,14 @@ else                              % rational case
   % domain, then G is ill-conditioned and accuracy is lost
   A = gam(1:m,1:m); B = gam(1:m,m+1);
   C = gam(1:m,end:-1:m+2); G = A+C-2*B*B'/gam(1,1);
+  if (cond(G)/s > tolcond) && (cond(G) > tolcondG),
+    warning('chebfun:cf:ill_conditioned', ...
+      ['Ill-conditioning detected. Results may be inaccurate.']);
+  end
   bc = G\(-2*(B*ct(1)/gam(1,1)-ct(m+1:-1:2)'));
   bc0 = (ct(1)-B'*bc)/gam(1,1); bc = [bc0, bc(end:-1:1)'];
   p = chebfun(chebpolyval(bc(end:-1:1)),d);
+  r = @(x) feval(p,x)./feval(q,x);
 end
 
 
--- chebtests/cftest.m
+++ chebtests/cftest.m
@@ -1,8 +1,8 @@
 function pass = cf
 f = chebfun('exp(x)');
-[p,q,lam] = cf(f,2);
+[p,q,r,lam] = cf(f,2);
 pass(1) = (abs(lam-0.045017)<1e-4);
 f = chebfun('exp(-1+x/2)',[0 4]);
-[p,q,lam] = cf(f,2);
+[p,q,r,lam] = cf(f,2);
 pass(2) = (abs(lam-0.045017)<1e-4);
 
