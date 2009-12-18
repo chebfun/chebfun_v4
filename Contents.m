2e49198d9affc8fe747ecd29d5d785d109caf867
platte <platte@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2009-12-18 18:45:47 +0000

    Updated index.html and helptoc.xml with new guide chapters. Updated version number in Contents.m Preparing to generate version 3 release ...

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Chebfun
-% Version 3.0(alpha) 19-Jan-2009
+% Version 3.1000 18-Dec-2009
 %
 % The chebfun system extends familiar powerful methods of numerical
 % computation involving numbers to continuous or piecewise-continuous
