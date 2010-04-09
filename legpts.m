6795a8db41c1376020bb0bda0fedc8b72026fd9a
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2010-04-09 17:22:31 +0000

    Some tweaks to legpts. The nodes are (absolutely) accurate to 16 digits (tested using VPA in MAPLE) even for 2048 points. It's hard to test any higher as maple takes too long...

--- legpts.m
+++ legpts.m
@@ -34,64 +34,61 @@ function [x,w] = legpts(n,varargin)
 %       on Scientific Computing", 29(4):1420-1438:, 2007.
 
 if n <= 0
-    error('CHEBFUN:legpts:n','First input should be a positive number');
+    error('CHEBFUN:legpts:n','First input should be a positive number.');
 end
 
-% defaults
+% Defaults
 interval = [-1,1];
 method = 'default';
 
-% check inputs
-if nargin > 1
-    if isa(varargin{1},'double') && length(varargin{1}) == 2
-        interval = varargin{1};
-    elseif isa(varargin{1},'domain')
-        interval = varargin{1}.ends;
-    elseif isa(varargin{1},'char')
-        method = varargin{1}; 
-    end
-    if length(varargin) == 2,
-        if isa(varargin{2},'double') && length(varargin{2}) == 2
-            interval = varargin{2};
-        elseif isa(varargin{1},'domain')
-            interval = varargin{2}.ends;
-        elseif isa(varargin{2},'char')
-            method = varargin{2}; 
+% Check inputs
+% Check the inputs
+while ~isempty(varargin)
+    s = varargin{1}; varargin(1) = [];
+    if ischar(s)
+        if strcmpi(s,'GW'), method = 'GW';
+        elseif strcmpi(s,'fast'), method = 'fast';   
+        else error('CHEBFUN:legpts:inputs',['Unrecognised input string.', s]); 
         end
+    elseif isa(s,domain), interval = s.ends;
+    elseif isa(varargin{2},'double') && length(varargin{2}) == 2, interval = s;
+    else error('CHEBFUN:legpts:input','Unrecognised input.');
     end
 end
 
-% decide to use GW or FAST
-if (n < 128 || strcmpi(method,'GW')) && ~strcmpi(method,'fast') % GW, see [1]
-   beta = .5./sqrt(1-(2*(1:n-1)).^(-2));   % 3-term recurrence coeffs
+% Decide to use GW or FAST
+if (n < 128 || strcmpi(method,'GW')) && ~strcmpi(method,'fast') 
+% GW, see [1]
+   beta = .5./sqrt(1-(2*(1:n-1)).^(-2)); % 3-term recurrence coeffs
    T = diag(beta,1) + diag(beta,-1);     % Jacobi matrix
-   [V,D] = eig(T);                       % eigenvalue decomposition
+   [V,D] = eig(T);                       % Eigenvalue decomposition
    x = diag(D); [x,i] = sort(x);         % Legendre points
-   w = 2*V(1,i).^2;                      % weights
-   
-   % enforce symmetry
+   w = 2*V(1,i).^2;                      % Weights
+   % Enforce symmetry
    ii = 1:floor(n/2);  x = x(ii);  w = w(ii);
    if mod(n,2)
         x = [x ; 0 ; -flipud(x)];  w = [w  2-sum(2*w) fliplr(w)];
    else
         x = [x ; -flipud(x)];      w = [w fliplr(w)];
    end
-else                                                            % Fast, see [2]
-   [x ders] = alg0_Leg(n);               % nodes and P_n'(x)
-   w = 2./((1-x.^2).*ders.^2)';          % weights
+else
+% Fast, see [2]
+   [x ders] = alg0_Leg(n);               % Nodes and P_n'(x)
+   w = 2./((1-x.^2).*ders.^2)';          % Weights
 end
-w = (2/sum(w))*w;                        % normalise so that sum(w) = 2
 
-% rescale to arbitrary interval
+% Normalise so that sum(w) = 2
+w = (2/sum(w))*w;           
+
+% Rescale to arbitrary interval
 if ~all(interval == [-1 1])
-    dab = diff(interval);
-    
     if ~any(isinf(interval))
-        % finite interval
+    % Finite interval
+        dab = diff(interval);
         x = (x+1)/2*dab + interval(1);
         w = dab*w/2;
     else
-        % infinite interval
+    % Infinite interval
         m = maps(fun,{'unbounded'},interval); % use default map
         if nargout > 1
             w = w.*m.der(x.');
@@ -101,104 +98,100 @@ if ~all(interval == [-1 1])
     end
 end
 
-
 % -------------------- Routines for FAST algorithm ------------------------
 
-function [roots ders] = alg0_Leg(n) % driver for 'Fast'.
-% Compute coefficients of P_m(0), Pm'(0), m = 0,..,N
-Pm2 = 0; Pm1 = 1; Ppm2 = 0; Ppm1 = 0;
-for k = 0:n-1
-    P = -k*Pm2/(k+1);
-    Pp = ((2*k+1)*Pm1-k*Ppm2)/(k+1);
-    Pm2 = Pm1; Pm1 = P; Ppm2 = Ppm1; Ppm1 = Pp;
-end
+function [roots ders] = alg0_Leg(n) % Driver for 'Fast'.
 
-roots = zeros(n,1); ders = zeros(n,1);                      % allocate storage
-if mod(n,2),roots((n-1)/2) = 0; ders((n+1)/2) = Pp;         % zero is a root
-else [roots(n/2+1) ders(n/2+1)] = alg2_Leg(P,n); end        % find first root
+% Compute coefficients of P_m(0), m = 0,..,N via recurrence relation.
+Pm2 = 0; Pm1 = 1; 
+for k = 0:n-1, P = -k*Pm2/(k+1); Pm2 = Pm1; Pm1 = P; end
 
-[roots ders] = alg1_Leg(roots,ders); % compute roots and derivatives
+% Get the first roots and derivative values to initialise.
+roots = zeros(n,1); ders = zeros(n,1);                      % Allocate storage
+if mod(n,2)                                                 % n is odd
+    roots((n-1)/2) = 0;                                     % Zero is a root
+    ders((n+1)/2) = n*Pm2;                                  % P'(0)    
+else                                                        % n is even
+    [roots(n/2+1) ders(n/2+1)] = alg2_Leg(P,n);             % Find first root
+end        
+[roots ders] = alg1_Leg(roots,ders);          % Other roots and derivatives
 
 % -------------------------------------------------------------------------
 
-function [roots ders] = alg1_Leg(roots,ders) % main algorithm for 'Fast'
+function [roots ders] = alg1_Leg(roots,ders)  % Main algorithm for 'Fast'
 n = length(roots);
-if mod(n,2), N = (n-1)/2; s = 1;
-else N = n/2; s = 0; end
+if mod(n,2), N = (n-1)/2; s = 1; else N = n/2; s = 0; end   
 
-% Approximate roots via asymptotic formula
-k = (n-2+s)/2:-1:1;
-theta = pi*(4*k-1)/(4*n+2);
+% Approximate roots via asymptotic formula.
+k = (n-2+s)/2:-1:1; theta = pi*(4*k-1)/(4*n+2);
 roots(((n+4-s)/2):end) = (1-(n-1)/(8*n^3)-1/(384*n^4)*(39-28./sin(theta).^2)).*cos(theta);
+x = roots(N+1);
 
-m = 30; % number of terms in Taylor expansion
-hh1 = ones(m+1,1); u = zeros(1,m+1); up = zeros(1,m+1);
+% Number of terms in Taylor expansion.
+m = 30;
+
+% Storage
+hh1 = ones(m+1,1); zz = zeros(m,1); u = zeros(1,m+1); up = zeros(1,m+1);
+
+% Loop over all the roots we want to find (using symmetry).
 for j = N+1:n-1
-    x = roots(j); % previous root
-    
-    % initial approx (via asymptotic foruma)
+    % Distance to initial approx for next root (from asymptotic foruma).
     h = roots(j+1) - x;
-           
-    % scaling
-    M = 1/h;
 
-    % recurrence relation for Legendre polynomials (scaled)
-    c1 = 2*x/M; c2 = (1-x.^2);
-    u(1) = 0;   u(2) = ders(j)/M;  up(1) = u(2); up(m+1) = 0;
+    % Recurrence Taylor coefficients (scaled & incl factorial terms).
+    M = 1/h;                           % Scaling
+    c1 = 2*x/M; c2 = 1./(1-x^2);       % Some constants
+    u([1 2]) = [0 ders(j)/M];  up(1) = u(2);
     for k = 0:m-2
-        u(k+3) = (c1*(k+1)*u(k+2)+(k-n*(n+1)/(k+1))*u(k+1)/M^2)./(c2*(k+2));
-        up(k+2) = (k+2)*u(k+3)*M;
+%         u(k+3) = (c1*(k+1)*u(k+2)+(k-n*(n+1)/(k+1))*u(k+1)/M^2)/(c2*(k+2));
+        u(k+3) = (c1*(k+1)*u(k+2)+(k-n*(n+1)/(k+1))*u(k+1)/M^2)*c2/(k+2);
+        up(k+2) = (k+2)*u(k+3);
     end
-    
-    % flip for more accuracy in inner product calculation
+    up(m+1) = 0;   
+
+    % Flip for more accuracy in inner product calculation.
     u = u(m+1:-1:1);  up = up(m+1:-1:1);
     
     % Newton iteration
-    hh = hh1; step = inf;  l = 0;
+    hh = hh1; step = inf;  l = 0; 
+
     while (abs(step) > eps) && (l < 10)
         l = l + 1;
-        step = (u*hh)/(up*hh);
-        h = h - step;
-        hh = [M;cumprod(M*h+zeros(m,1))]; % powers of h (This is the fastest way!)
-        hh = hh(end:-1:1); % flip for more accuracy in inner product calculation
+        step = (u*hh)/(up*hh)/M;
+        h = h - step;        
+        Mhzz = (M*h)+zz;
+        hh = [1;cumprod(Mhzz)];     % Powers of h (This is the fastest way!)
+        hh = hh(end:-1:1);          % Flip for more accuracy in inner product 
     end
-    
-    % update
-    roots(j+1) = x + h;
-    ders(j+1) = up*hh;    
+      
+    % Update
+    x = x + h;
+    roots(j+1) = x;
+    ders(j+1) = M*(up*hh);  
 end
 
-% nodes are symmetric
+% Nodes are symmetric.
 roots(1:N+s) = -roots(n:-1:N+1);
 ders(1:N+s) = ders(n:-1:N+1);
 
-
 % -------------------------------------------------------------------------
 
-function [x1 d1] = alg2_Leg(Pn0,n) % find the first root (note P_n'(0)==0)
-
-% % % advance ODE via Runge-Kutta for initial approx (redundant)
-% x1 = rk2_Leg(0,-pi/2,0,n)
-
+function [x1 d1] = alg2_Leg(Pn0,n) % Find the first root (note P_n'(0)==0)
 % Approximate first root via asymptotic formula
-k = ceil(n/2);
-theta = pi*(4*k-1)/(4*n+2);
+k = ceil(n/2); theta = pi*(4*k-1)/(4*n+2);
 x1 = (1-(n-1)/(8*n^3)-1/(384*n^4)*(39-28./sin(theta).^2)).*cos(theta);
 
-m = 30; % number of terms in Taylor expansion
+m = 30; % Number of terms in Taylor expansion.
 
-% scaling
-M = 1/x1;
-    
-% recurrence relation for Legendre polynomials
-u = zeros(1,m+1); up = zeros(1,m+1);
-u(1) = Pn0;
+% Recurrence Taylor coefficients (scaled & incl factorial terms).
+M = 1/x1; % Scaling
+zz = zeros(m,1); u = [Pn0 zeros(1,m)]; up = zeros(1,m+1); % Allocate storage
 for k = 0:2:m-2
     u(k+3) = (k-n*(n+1)/(k+1))*u(k+1)/(M^2*(k+2)); 
     up(k+2) = (k+2)*u(k+3)*M;
 end
 
-% flip for more accuracy in inner product calculation
+% Flip for more accuracy in inner product calculation.
 u = u(m+1:-1:1); up = up(m+1:-1:1);
 
 % Newton iteration
@@ -207,20 +200,9 @@ while (abs(step) > eps) && (l < 10)
     l = l + 1;
     step = (u*x1k)/(up*x1k);
     x1 = x1 - step;
-    x1k = [1;cumprod(M*x1+zeros(m,1))]; % powers of h (This is the fastest way!)
-    x1k = x1k(end:-1:1); % flip for more accuracy in inner product calculation
+    x1k = [1;cumprod(M*x1+zz)]; % Powers of h (This is the fastest way!)
+    x1k = x1k(end:-1:1);        % Flip for more accuracy in inner product
 end
+
+% Get the derivative at this root, i.e. P'(x1).
 d1 = up*x1k;
-
-
-% -------------------------------------------------------------------------
-
-% function x = rk2_Leg(t,tn,x,n) % Runge-Kutta for Legendre Equation (redundant)
-% m = 10; h = (tn-t)/m;
-% for j = 1:m
-%     f1 = (1-x.^2);
-%     k1 = -h*sqrt(f1)./(sqrt(n*(n+1)) - 0.5*x.*sin(2*t));
-%     t = t+h;  f2 = (1-(x+k1).^2);
-%     k2 = -h*sqrt(f2)./(sqrt(n*(n+1)) - 0.5*(x+k1).*sin(2*t));   
-%     x = x+.5*(k1+k2);
-% end
