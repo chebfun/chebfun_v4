d0ac8b5df7f5ebacf241c89c7e924d11654f9f22
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2010-04-10 09:09:31 +0000

    Added support for Chebyshev polynomials of the 2nd kind in chebpoly.

--- chebpoly.m
+++ chebpoly.m
@@ -1,10 +1,14 @@
-function f = chebpoly(n,d)
-% CHEBPOLY   Chebyshev polynomial of degree n.
+function f = chebpoly(n,d,kind)
+%CHEBPOLY   Chebyshev polynomial of degree n.
 % F = CHEBPOLY(N) returns the chebfun corresponding to the Chebyshev
 % polynomials T_N(x) on [-1,1], where N may be a vector of positive integers.
 %
 % F = CHEBPOLY(N,D), where D is an interval or a domain, gives the same 
 % result scaled accordingly.
+%
+% F = CHEBPOLY(N,KIND) or F = CHEBPOLY(N,D,KIND) switches between Chebyshev
+% polynomials of the 1st kind, T_N(x)), when KIND = 1, and Chebyshev polynomials 
+% of the 2nd kind, U_N(x)), when KIND = 2.
 %
 % See also chebfun/chebpoly and legpoly.
 %
@@ -12,15 +16,31 @@ function f = chebpoly(n,d)
 
 % Copyright 2002-2009 by The Chebfun Team. 
 
-if nargin == 1
-    d = domain(chebfunpref('domain'));  
-end
+% Defaults
+defd = domain(chebfunpref('domain')); 
+defkind = 1;
 
-ln = length(n);
+% Parse input
+if nargin == 1
+    d = defd;
+    kind = defkind;
+elseif nargin == 2
+    kind = defkind;
+    if numel(d) == 1
+        kind = d; d = defd;
+    end
+end    
+
+% Loop over n
 f = chebfun;
-for k = 1:ln
+for k = 1:length(n)
+    % Values at 2nd kind points.
     vk = ones(1,n(k)+1); 
     vk(end-1:-2:1) = -1;
+    
+    % Modify for polynomials of the 2nd kind.
+    if kind == 2, vk(1) = (n(k)+1)*vk(1); vk(end) = n(k)+1; end
+    
     f(:,k) = chebfun(vk,d);
 end
 
