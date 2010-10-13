dde2ff12ea08ef7fabf1a9c48fd957732f76f4c8
driscoll <driscoll@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2010-10-13 20:43:50 +0000

    Calls [V,D] = eigs(A) in order to probe eigenfunctions.

--- chebtests/eigsho.m
+++ chebtests/eigsho.m
@@ -1,12 +1,17 @@
 function pass = eigsho
+% Check simple Laplacian eigenvalues.
+%   Toby Driscoll, updated Oct 13, 2010
 
 tol = chebfunpref('eps');
 d = domain(0,pi);
 A = diff(d,2) & 'dirichlet';
-lam = eigs(A,10);
+[V,D] = eigs(A,10);
+lam = diag(D);
 k = sqrt(-lam);
 
-pass = norm( k - (1:10)', Inf ) < 1000*tol; 
+pass(1) = norm( k - (1:10)', Inf ) < 1000*tol; 
+pass(2) = norm( A*V(:,1) - V(:,1)*D(1,1) ) < 1e6*tol;
+pass(3) = abs(norm(V(:,1))-1) < 1000*tol;
 
 
 
