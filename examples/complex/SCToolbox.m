9ad6b062d9fcc36da39c7df7448023d5b957dd83
driscoll <driscoll@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2010-10-14 21:13:07 +0000

    Added links for SC Toolbox and reference [1]. Extensive rewriting for more leisurely pace and smaller code blocks.

--- examples/complex/SCToolbox.m
+++ examples/complex/SCToolbox.m
@@ -6,58 +6,103 @@
 
 %%
 % Chebfun's SCRIBBLE command is good for illustrating conformal maps in the
-% complex plane.  For example, the hyperbolic tangent function maps infinite
-% strips onto lens-shaped regions with vertices at 1 and -1, and this is
-% the basis of numerical methods based on sinc functions, discussed
-% in books and papers by F. Stenger.  Here is an illustration of the tanh map.
-%%
-% <latex> \vspace{-2em}</latex>
-LW = 'linewidth'; lw = 1.2;
-w = .07-.15i+2.2*scribble('INFINITE STRIP');
-bndry = chebfun('1i*pi/8 + t',[-3 3]);
-bndry = [bndry; -bndry];
-figure, subplot(2,1,1)
-plot(w,LW,lw), axis equal
-hold on, plot(bndry,'k',LW,lw), xlim([-3,3])
-subplot(2,1,2)
-g = @(z) tanh(z);
-plot(g(w),LW,lw), axis equal
-hold on, plot(g(bndry),'k',LW,lw), xlim([-2 2])
+% complex plane. These maps can distort distances greatly but preserve all
+% angles, and text demonstrates both of these features well. 
+
+%% Tanh map
+% The hyperbolic tangent function maps infinite
+% strips onto lens-shaped regions with vertices at -1 and 1. We use SCRIBBLE 
+% to define a piecewise smooth parameterized curve
+% whose smooth segments describe letter shapes in the complex plane. The 
+% letters are scaled and translated using complex arithmetic.
 
 %%
-% More generally, suppose we want to work with conformal maps from one
-% polygon to another (possibly with vertices at infinity)
-% but the polygons aren't quite as simple as in the tanh case.
-% Then we can use the Schwarz-Christoffel formula, which is
-% implemented numerically in Driscoll's Schwarz-Christoffel Toolbox.
-% Here is an illustration.
+% <latex> \vspace{-2em}</latex>
+w = .07-.15i + 2.2*scribble('INFINITE STRIP');
+
+%%
+% We also use chebfuns to represent the two sides of the strip, and plot
+% everything together.
+
 %%
 % <latex> \vspace{-2em}</latex>
-w = .07 + 0.3i + 3*scribble('INFINITE STRIP');
+bndry = chebfun('1i*pi/8 + t',[-3 3]);
+bndry = [bndry; -bndry];
+figure('defaultlinelinewidth',1.8)
+plot(w), hold on
+plot(bndry,'k'), axis equal, axis([-3 3 -1.5 1.5])
+
+%%
+% Here, we simply repeat the plots after composing each element with the
+% tanh function.
+
+%%
+% <latex> \vspace{-2em}</latex>
+g = @(z) tanh(z);
+clf, plot(g(w)), hold on
+plot(g(bndry),'k'), axis equal, axis([-1.4 1.4 -1 1]) 
+
+%%
+% The tanh map is the basis of numerical methods based on sinc functions, discussed
+% in books and papers by F. Stenger [3].  
+
+%% Schwarz-Christoffel maps
+% More generally, suppose we want to map the original infinite strip not to 
+% the lens shape but to a polygon. This kind of map is provided by
+% the Schwarz-Christoffel formula [2], which is
+% implemented numerically in Driscoll's 
+% <http://www.math.udel.edu/~driscoll/SC Schwarz-Christoffel Toolbox> [1].
+% Here is an illustration of the map to a rectangle. 
+%%
+% As before, we set up the letters and the boundary of the strip.
+
+%%
+% <latex> \vspace{-2em}</latex>
+w = .07+0.3i + 3*scribble('INFINITE STRIP');
 bndry = [chebfun('1i + t',5*[-1 1]); chebfun('t',5*[-1 1])];
-figure, subplot(2,1,1)
-plot(w,LW,lw), axis equal
-hold on, plot(bndry,'k',LW,lw), xlim(4.5*[-1,1])
-subplot(2,1,2)
-p = polygon([-1 -0.5 0 0.5 1 1+.5i -1+.5i]);
-f = stripmap(p,[1 5]);
-z = prevertex(f);
-g = stripmap(p,z-z(3),[1 5]);
+
+%%
+% Next, define the map to a specified rectangle such that the ends of the
+% strip map to two corners. (The SC Toolbox must be on MATLAB's path for
+% these commands to work.) A few extra manipulations are done to make a
+% map that works for our purposes. 
+
+%%
+% <latex> \vspace{-2em}</latex>
+p = polygon([-1 -0.5 0 0.5 1 1+.5i -1+.5i]);  % rectangle
+f = stripmap(p,[1 5]);                        % map from strip to rectangle
+z = prevertex(f); g = stripmap(p,z-z(3),[1 5]);  % renormalized map
+
+%%
+% Here is where we compose the SC map with the letters.  
+
+%%
+% <latex> \vspace{-2em}</latex>
 gw = chebfun(@(x) g(w(x)),w.ends,'eps',1e-4,'extrapolate','on');
-plot(gw,LW,lw), axis equal
+
+%%
+% Finally, we plot everything.
+
+%%
+% <latex> \vspace{-2em}</latex>
+clf, subplot(2,1,1)
+plot(w), axis equal
+hold on, plot(bndry,'k'), xlim(4.5*[-1,1])
+subplot(2,1,2)
+plot(gw), axis equal
 vp = vertex(p); vp = vp([1:end 1]);
-hold on, plot(vp,'k',LW,lw), xlim(1.5*[-1 1])
+hold on, plot(vp,'k'), xlim(1.5*[-1 1])
 
 %%
 % References:
 %
-% T. A. Driscoll, Algorithm 843: Improvements to the
-% Schwarz-Christoffel Toolbox for Matlab, ACM Transactions on
-% Mathematical Software 31 (2005), 239-251.
+% [1] T. A. Driscoll, 
+% <http://dx.doi.org/10.1145/1067967.1067971 Algorithm 843: Improvements to the Schwarz-Christoffel Toolbox for MATLAB>,
+% ACM Transactions on Mathematical Software 31 (2005), 239-251.
 %
-% T. A. Driscoll and L. N. Trefethen, Schwarz-Christoffel Mapping,
+% [2] T. A. Driscoll and L. N. Trefethen, Schwarz-Christoffel Mapping,
 % Cambridge U. Press, 2002.
 %
-% F. Stenger, Numerical Methods Based on Sinc and
+% [3] F. Stenger, Numerical Methods Based on Sinc and
 % Analytic Functions, Springer, 1993.  
 %
