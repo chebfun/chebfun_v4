f1174785d0b64872238147a1619f2fc9701f09a9
driscoll <driscoll@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2010-10-21 15:35:59 +0000

    Cleaned up text and got a better title.

--- examples/integro/VlasovPoisson.m
+++ examples/integro/VlasovPoisson.m
@@ -1,4 +1,4 @@
-%% TRANSIENT GROWTH IN THE LINEARIZED VLASOV-POISSON OPERATOR
+%% ABSCISSA OF THE LINEARIZED VLASOV-POISSON OPERATOR
 % Toby Driscoll, 20 October 2010
 
 %%
@@ -8,7 +8,7 @@
 %               
 % for a specified kernel function K. As an example, the kernel function
 %
-%   K(s,t) = [a^2 (s-t)^2 - 1] exp [ -a^2 (s-t)^2/2 ]
+%   K(s,t) = [1-a^2 (s-t)^2] exp [ -a^2 (s-t)^2/2 ]
 %
 % arises in the linearization of the Vlasov-Poisson equation of plasma physics [1]. 
 % An application is to find the numerical
@@ -19,28 +19,28 @@
 % For a piecewise smooth kernel, the VOLT function can produce the original
 % operator A directly:
 [d,t] = domain(0,6);  a = 1;
-k = @(s,t) (1-(a*(t-s)).^2) .* exp(-0.5*a^2*(t-s).^2 );
-A = volt(k,d);
+K = @(s,t) (1-(a*(t-s)).^2) .* exp(-0.5*a^2*(t-s).^2 );
+A = volt(K,d);
 
 %%
 % Chebfun does not currently have a facility for finding the
 % adjoint of a linear operator. For a Volterra operator, however, the
 % adjoint can again be written in terms of integral operators using the
-% kernel K. In this case, the symmetry K(s,t)=K(t,s) simplifies things even
+% kernel K. In this case, the symmetry property K(s,t)=K(t,s) simplifies things even
 % further, and B is just the Fredholm variant of A. So we can use 
 % FRED:
-B = 0.5*fred(k,d);
+B = 0.5*fred(K,d);
 
 %%
 % The following lines create a function to return the numerical abscissa
 % given a value for a.
 [d,t] = domain(0,6);
-k = @(s,t,a) (1-(a*(t-s)).^2) .* exp(-0.5*a^2*(t-s).^2 );
-B = @(a) 0.5*fred(@(s,t) k(s,t,a),d);  % (A+A')/2 for any A
+K = @(s,t,a) (1-(a*(t-s)).^2) .* exp(-0.5*a^2*(t-s).^2 );
+B = @(a) 0.5*fred(@(s,t) K(s,t,a),d);  % (A+A')/2 for any A
 numabs = @(a) eigs(B(a),1,'lr');       % e.v. with max real part
 
 %%
-% It's easy to show that as a->0, the numerical absicssa is T/2 on the
+% It's easy to show that for a=0, the numerical absicssa is T/2 on the
 % domain [0,T].
 numabs(0)
 
@@ -61,8 +61,7 @@ xlabel('a'), ylabel('\omega')
 astar = omega.ends(2)
 
 %%
-% Presumably the breakpoint represents eigenvalues for two different
-% eigenfunctions crossing one another. We can check this assertion by
+% Presumably the breakpoint represents an eigenvalue crossing. We can check this conjecture by
 % looking at the eigenfunctions on either side of the breakpoint.
 [Vm,Dm] = eigs(B(astar-1e-3),2,'lr');
 [Vp,Dp] = eigs(B(astar+1e-3),2,'lr');
@@ -71,7 +70,7 @@ subplot(2,1,2), plot(Vp/Vp(1,2)), title('Eigenfunctions at (a^* + 1e-3)')
 
 %%
 % Note that while the eigenfunctions themselves vary
-% continuously with the parameter, the color scheme changes in the two
+% smoothly with the parameter, the color scheme changes in the two
 % plots! This indicates that the role of leading eigenfunction (blue) has
 % passed from one to the other. 
 
