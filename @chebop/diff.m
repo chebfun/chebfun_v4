9f1737ea39f231e54f587b88ac9f3158a7bc6707
birkisson <birkisson@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2010-10-22 10:35:51 +0000

    Bug fix to chebop.diff.

--- @chebop/diff.m
+++ @chebop/diff.m
@@ -2,7 +2,7 @@ function J = diff(N,u)
 %DIFF    Jacobian (Frechet derivative) of nonlinear operator.
 % J = DIFF(N,U) for a chebop N and chebfun U, returns a chebop
 % represening the Jacobian (i.e., the Frechet derivative) of N evaluated at
-% U. More specifically, J is the operator such that 
+% U. More specifically, J is the operator such that
 %
 %   || N(u+delta) - N(u) - J*delta || / ||delta|| -> 0
 %
@@ -29,8 +29,8 @@ function J = diff(N,u)
 %     delta = -J\r;  u = u+delta;
 %   end
 %   plot(u), title(sprintf('|| residual || = %.3e',norm(N(u))))
-%  
-% See also chebfun/diff. 
+%
+% See also chebfun/diff.
 
 % Copyright 2009 by Toby Driscoll.
 % See http://www2.maths.ox.ac.uk/chebfun/ for more on chebfun.
@@ -38,30 +38,39 @@ function J = diff(N,u)
 %  Last commit: $Author$: $Rev$:
 %  $Date$:
 
-% Functional part
-Nu = N.op(u);
-J = diff(Nu,u);
+
+
+% Boundary conditions part
+ab = N.dom.ends;
+a = ab(1); b = ab(end);
 
 % Left BC
 if ~isempty(N.lbc)
-  if ~iscell(N.lbc), lbc = {N.lbc};   % wrap singleton in cell
-  else lbc = N.lbc;
-  end
-  for j = 1:length(lbc)
-    gu = lbc{j}(u);
-    J.lbc(j) = diff(gu,u);
-  end
+    if ~iscell(N.lbc), lbc = {N.lbc};   % wrap singleton in cell
+    else lbc = N.lbc;
+    end
+    for j = 1:length(lbc)
+        gu = lbc{j}(u);
+        linBC.left(j) = struct('op',diff(gu,u),'val',gu(a,j));
+    end
 end
 
 % Right BC
 if ~isempty(N.rbc)
-  if ~iscell(N.rbc), rbc = {N.rbc};   % wrap singleton in cell
-  else rbc = N.rbc;
-  end
-  for j = 1:length(rbc)
-    gu = rbc{j}(u);
-    J.rbc(j) = diff(gu,u);
-  end
+    if ~iscell(N.rbc), rbc = {N.rbc};   % wrap singleton in cell
+    else rbc = N.rbc;
+    end
+    for j = 1:length(rbc)
+        gu = rbc{j}(u);
+        linBC.right(j) = struct('op',diff(gu,u),'val',gu(b,j));
+    end
 end
 
+% Functional part + add BCs
+Nu = N.op(u);
+if exist('linBC','var')
+    J = diff(Nu,u) & linBC;
+else
+    J = diff(Nu,u);
+end
 end
