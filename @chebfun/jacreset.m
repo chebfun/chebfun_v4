2739840298e8d00a58ed1737a731fa86d9cab4c9
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2010-11-16 13:37:05 +0000

    Minor change to jacreset.m

--- @chebfun/jacreset.m
+++ @chebfun/jacreset.m
@@ -1,13 +1,13 @@
 function fout = jacreset(fin,newjac)
 %JACRESET Reset Jacobian of a chebfun.
-% Chebfuns keep track of their construction in such a way that they can be 
+% Chebfuns keep track of their construction in such a way that they can be
 % queried for their Frechet derivative (Jacobian) with respect to any other
-% chebfun using DIFF. FOUT = JACRESET(FIN) resets the Jacobian field of a 
+% chebfun using DIFF. FOUT = JACRESET(FIN) resets the Jacobian field of a
 % chebfun to be empty, which will be interpreted as zero in all cases.
 %
 % FOUT = JACRESET(FIN,NEWJAC) sets the Jacobian to be NEWJAC, which must be
 % an ANON object.
-% 
+%
 % FIN may be a quasimatrix.
 %
 % See also DIFF, ANON.
