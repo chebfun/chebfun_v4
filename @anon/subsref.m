f10dfbae199289a382f66caa790377e072d48084
birkisson <birkisson@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2010-11-21 13:44:56 +0000

    Improvement to anon/subsref, avoiding calls to anon/get to decrease overhead. This speeds my chebtest by 0.3 seconds!

--- @anon/subsref.m
+++ @anon/subsref.m
@@ -1,13 +1,24 @@
-function varargout = subsref(f,index)
+function varargout = subsref(an,index)
 % SUBSREF for the anon class. At the moment, the only allowed subsref type
 % is . and ().
 
 idx = index(1).subs;
 switch index(1).type
     case '.'
-        varargout = { get(f,idx) };
+        switch(idx)
+            case 'function'
+                varargout = {an.function};
+            case 'variablesName'
+                varargout = {an.variablesName};
+            case 'workspace'
+                varargout = {an.workspace};
+            case 'depth'
+                varargout = {an.depth};
+            otherwise
+                error('ANON:get:propnam',[propName,' is not a varargoutid anon property.'])
+        end
     case '()'
-        varargout = {feval(f,idx{1})};
+        varargout = {feval(an,idx{1})};
     otherwise
         error('CHEBFUN:anon:subsref',['??? Unexpected index.type of ' index(1).type]);
 end
