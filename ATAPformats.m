f4f488e5c8e75632435b68f6d2d94c92533136bb
trefethen <trefethen@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2010-11-27 12:42:58 +0000

    New code fov.m for computing field of values of a matrix A (as in the Example introduced a few weeks ago), together with test fovtest.m.  I haven't committed anything in ages, so please tell me somebody if my formats are wrong somewhere.

--- ATAPformats.m
+++ ATAPformats.m
@@ -5,8 +5,8 @@
 % This code sets certain default properties and is called at
 % the beginning of each section of the book so that the output
 % from "publish" has a pleasing appearance.  It is included in
-% this directory as part of the chebfun distribution so that
-% readers of the book need only download chebfun to be able
+% this directory as part of the Chebfun distribution so that
+% readers of the book need only download Chebfun to be able
 % to execute each section.
 
 % These formats are a work in progress -- they will go through
@@ -16,4 +16,4 @@ set(0,'defaultfigureposition',[380 320 540 200],...
 'defaultaxeslinewidth',0.9,'defaultaxesfontsize',8,...
 'defaultlinelinewidth',1.1,'defaultpatchlinewidth',1.1,...
 'defaultlinemarkersize',15), format compact, format long
-chebfunpref('factory'); clear all, x = chebfun('x',[-1 1]);
+chebfunpref('factory')
