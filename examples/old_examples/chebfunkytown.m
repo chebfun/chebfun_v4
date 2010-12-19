ff8f2b833e4174d963dd152fd5c4a5ef7b8c9d13
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2010-12-19 10:42:35 +0000

    Removing examples directory from trunk to avoid further confusion. Examples live in etc/examples branch.

--- examples/old_examples/chebfunkytown.m
+++
@@ -1,6 +0,0 @@
-% Chebfunkytown - Please the beginning of Funkytown by Lipps Inc.
-
-% Asgeir Birkisson, 2010
-
-e = exp(sing('C+ C+ A+# C+ r r G r r r G C+ F+ E+ C+'));
-sound(e), pause(0.8), sound(e)
