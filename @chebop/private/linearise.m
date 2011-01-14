5a94985a84345ca55e39579d84af199a458fe8e8
birkisson <birkisson@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-01-14 15:00:03 +0000

    Added support for periodic BCs for linearising chebops (lack of which was pointed out by Mark).

--- @chebop/private/linearise.m
+++ @chebop/private/linearise.m
@@ -14,10 +14,10 @@ function [L linBC isLin] = linearise(N,u,flag)
 if nargin == 1 || isempty(u)
     if isempty(N.dom)
         error('CHEBFUN:chebop:linop:emptydomain', ...
-            'Cannot linearise a chebop defined on an empty domain.'); 
+            'Cannot linearise a chebop defined on an empty domain.');
     end
-%   Create a chebfun to let the operator operate on. Using the findguess
-%   method ensures that the guess is of the right (quasimatrix) dimension.
+    %   Create a chebfun to let the operator operate on. Using the findguess
+    %   method ensures that the guess is of the right (quasimatrix) dimension.
     u = findguess(N);
 end
 
@@ -30,44 +30,54 @@ linBC = [];
 ab = N.dom.ends;
 a = ab(1); b = ab(end);
 
-% Left BC
-if ~isempty(N.lbc)
-    if ~iscell(N.lbc), lbc = {N.lbc};   % wrap singleton in cell
-    else lbc = N.lbc;
-    end
-    l = 1;
-    for j = 1:length(lbc)
-        guj = lbc{j}(u);
-        for k = 1:numel(guj);
-            [Dgujk nonConst] = diff(guj(:,k),u);
-            if any(nonConst),  isLin = 0;   end
-            if ~isLin && flag == 1, return, end
-            linBC.left(l) = struct('op',Dgujk,'val',-guj(a,k));
-            l = l+1;
-        end
-    end
-else
-    linBC.left = struct([]);
+% Check whether we have a mismatch between periodic BCs
+if xor(strcmpi(N.lbc,'periodic'),strcmpi(N.rbc,'periodic'))
+    error('CHEBOP:linearise:periodic', 'BC is periodic at one end but not at the other.');
 end
 
-% Right BC
-if ~isempty(N.rbc)
-    if ~iscell(N.rbc), rbc = {N.rbc};   % wrap singleton in cell
-    else rbc = N.rbc;
-    end
-    l = 1;
-    for j = 1:length(rbc)
-        guj = rbc{j}(u);
-        for k = 1:numel(guj);
-            [Dgujk nonConst] = diff(guj(:,k),u);
-            if any(nonConst),  isLin = 0;   end
-            if ~isLin && flag, return,      end
-            linBC.right(l) = struct('op',Dgujk,'val',-guj(b,k));
-            l = l+1;
-        end
-    end
+% Need to treat periodic BCs specially
+if strcmpi(N.lbc,'periodic')
+    linBC = 'periodic';
 else
-    linBC.right = struct([]);
+    % Left BC
+    if ~isempty(N.lbc)
+        if ~iscell(N.lbc), lbc = {N.lbc};   % wrap singleton in cell
+        else lbc = N.lbc;
+        end
+        l = 1;
+        for j = 1:length(lbc)
+            guj = lbc{j}(u);
+            for k = 1:numel(guj);
+                [Dgujk nonConst] = diff(guj(:,k),u);
+                if any(nonConst),  isLin = 0;   end
+                if ~isLin && flag == 1, return, end
+                linBC.left(l) = struct('op',Dgujk,'val',-guj(a,k));
+                l = l+1;
+            end
+        end
+    else
+        linBC.left = struct([]);
+    end
+    
+    % Right BC
+    if ~isempty(N.rbc)
+        if ~iscell(N.rbc), rbc = {N.rbc};   % wrap singleton in cell
+        else rbc = N.rbc;
+        end
+        l = 1;
+        for j = 1:length(rbc)
+            guj = rbc{j}(u);
+            for k = 1:numel(guj);
+                [Dgujk nonConst] = diff(guj(:,k),u);
+                if any(nonConst),  isLin = 0;   end
+                if ~isLin && flag, return,      end
+                linBC.right(l) = struct('op',Dgujk,'val',-guj(b,k));
+                l = l+1;
+            end
+        end
+    else
+        linBC.right = struct([]);
+    end
 end
 
 % Functional part
