4167d3b782346cabdea65bf25f7560db19f5eae7
trefethen <trefethen@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-02-06 12:31:47 +0000

    Various new scalar BVP demos, plus a general reorganization. I'm working with these principles:
    - demos have names, not equations
    - the names are alphabetical (Airy, Bessel, Carrier,...)
    - we try to include familiar problems that users may relate to
    
    I hope we will eventually separate the linear and nonlinear
    BVP examples into two separate entries on the Demos menu.

--- @chebgui/private/bvpdemos.m
+++ @chebgui/private/bvpdemos.m
@@ -3,7 +3,7 @@ if nargin < 3
     mode = 'start';
 end
 
-numberOfExamples = 11;
+numberOfExamples = 14;
 
 % If called with a 0, open with an empty gui. If called with a number less
 % than 0, bigger than the number of available examples, or no argument,
@@ -24,111 +24,123 @@ switch exampleNumber
         DE = '';
         LBC = ''; RBC = '';
         init = ''; tol = '';
-        damping = '';
-        plotting = '0.1';
+        damping = ''; plotting = '0.1';
         name = '';
         demotype = '';
     case 1
-        a = '-2'; b = '2';
-        DE = 'u" + x*sin(u) = 1';
-        LBC = 'u = 0'; RBC = 'u = 0';
-        init = ''; tol = '1e-10';
-        damping = '1';
-        plotting = '0.1';
-        name = DE;
-        demotype = 'bvp';
-    case 2
-        a = '1'; b = '3';
-        DE = 'u" + cos(sin(u)) = -1';
-        LBC = 'u'' = 2'; RBC = 'u = 0';
-        init = ''; tol = '1e-10';
-        damping = '1';
-        plotting = '0.1';
-        name = DE;
-        demotype = 'bvp';
-    case 3
         a = '0'; b = '1';
         DE = '0.02*u'''' + u'' = 0';
         LBC = 'u = 0'; RBC = 'u = 1';
         init = ''; tol = '1e-10';
-        damping = '0';
-        plotting = '0.1';
-        name = 'Advection-diffusion';
+        damping = '0'; plotting = '0.1';
+        name = 'Advection-diffusion equation';
         demotype = 'bvp';
-    case 4
+    case 2
         a = '-5'; b = '5';
         DE = '0.01*u'''' - x*u = 1';
         LBC = 'u = 0'; RBC = 'u = 0';
         init = ''; tol = '1e-10';
-        damping = '0';
-        plotting = '0.1';
+        damping = '0'; plotting = '0.1';
         name = 'Airy equation';
         demotype = 'bvp';
-   case 5
+   case 3
         a = '0'; b = '30';
         DE = 'x^2*u'''' + x*u'' + (x^2-3^2)*u = 0';
         LBC = 'u = 0'; RBC = 'u = 1';
         init = ''; tol = '1e-10';
-        damping = '0';
-        plotting = '0.1';
+        damping = '0'; plotting = '0.1';
         name = 'Bessel equation';
         demotype = 'bvp';
-    case 6
+    case 4
         a = '-1'; b = '1';
         DE = {'u" - sin(v) = 0';'cos(u) + v" = 0'};
         LBC = {'u = 1';'v'' = 0'};
         RBC = {'u'' = 0';'v = 0'};
         init = ''; tol = '1e-10';
-        damping = '0';
-        plotting = '0.4';
+        damping = '0'; plotting = '0.4';
         name = 'Coupled system';
         demotype = 'system';
-    case 7
+    case 5
         a = '-2'; b = '2';
         DE = 'u" - x*sin(u) = 1';
         LBC = {'u = 1';'u'' = 0'}; RBC = '';
         init = ''; tol = '1e-10';
-        damping = '1';
-        plotting = '0.1';
+        damping = '1'; plotting = '0.1';
         name = 'IVP';
         demotype = 'ivp';
-    case 8
+    case 6
         a = '0'; b = '15';
         DE = 'u" - (1-u^2)*u'' + u = 0';
         DErhs = '0';
         LBC = {'u = 2';'u'' = 0'}; RBC = '';
         init = ''; tol = '1e-10';
-        damping = '1';
-        plotting = '0.1';
+        damping = '1'; plotting = '0.1';
         name = 'Van der Pol';
         demotype = 'ivp';
-    case 9
+    case 7
         a = '-1'; b = '1';
         DE = '0.01*u" + 2*(1-x^2)*u + u^2 = 1';
         LBC = 'u = 0'; RBC = 'u = 0';
         init = '2*(x^2-1).*(1-2/(1+20*x^2))';
         tol = '1e-10';
-        damping = '1';
-        plotting = '0.1';
+        damping = '1'; plotting = '0.1';
         name = 'Carrier equation';
         demotype = 'bvp';
-    case 10
+    case 8
         a = '-100'; b = '100';
         DE = 'u" + (1.2+sign(10-abs(x)))*u = 1';
         LBC = 'u = 0'; RBC = 'u = 0';
         init = ''; tol = '1e-10';
-        damping = '1';
-        plotting = '0.1';
-        name = 'Discontinuous coefficient';
+        damping = '1'; plotting = '0.1';
+        name = 'Discontinuous coefficient wave equation';
         demotype = 'bvp';
-    case 11
+    case 9
+        a = '-4'; b = '4';
+        DE = 'u" + u - u^2 = 0';
+        LBC = 'u = 1'; RBC = 'u = 0';
+        init = ''; tol = '1e-10';
+        damping = '1'; plotting = '0.1';
+        name = 'Fisher-KPP equation';
+        demotype = 'bvp';
+    case 10
         a = '-1'; b = '1';
         DE= 'u" + .87*exp(u) = 0';
         LBC = 'u = 0'; RBC = 'u = 0';
         init = ''; tol = '1e-10';
-        damping = '1';
-        plotting = '0.1';
+        damping = '1'; plotting = '0.1';
         name = 'Frank-Kamenetskii blowup equation';
+        demotype = 'bvp';
+    case 11
+        a = '-1'; b = '1';
+        DE = '.0001*u'''' + x*(x^2-0.5)*u'' + 3*(x^2-0.5)*u = 0';
+        LBC = 'u = -2'; RBC = 'u = 4';
+        init = ''; tol = '1e-10';
+        damping = '1'; plotting = '0.1';
+        name = 'Interior layers linear equation';
+        demotype = 'bvp';
+    case 12
+        a = '0'; b = '10';
+        DE = 'u'''' = - sin(u)';
+        LBC = 'u = 2'; RBC = 'u = 2';
+        init = '2*cos(2*pi*x/10)'; tol = '1e-10';
+        damping = '1'; plotting = '0.1';
+        name = 'Nonlinear pendulum';
+        demotype = 'bvp';
+    case 13
+        a = '0'; b = '10';
+        DE = 'u'''' = - sin(u)';
+        LBC = 'u = 2'; RBC = 'u = 2';
+        init = '2 + sin(0.5*pi*x/10)'; tol = '1e-10';
+        damping = '1'; plotting = '0.1';
+        name = 'Nonlinear pendulum (a different solution)';
+        demotype = 'bvp';
+    case 14
+        a = '0'; b = '1';
+        DE = 'u" = 8*sinh(8*u)';
+        LBC = 'u = 0'; RBC = 'u = 1';
+        init = ''; tol = '1e-8';
+        damping = '1'; plotting = '0.1';
+        name = 'Troesch equation';
         demotype = 'bvp';
 end
 
