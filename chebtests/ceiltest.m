a66f6bac7dbecbe623681051ad048ab69282322c
gonnet <gonnet@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-02-15 15:05:26 +0000

    merged into level0/breakpoints.m

--- chebtests/ceiltest.m
+++
@@ -1,9 +0,0 @@
-function pass = ceiltest
-
-% Rodrigo Platte
-
-splitting on
-
-f1 = chebfun(@(x) ceil(x), -10:2); % Prescribed brkpoints
-f2 = chebfun(@(x) ceil(x), [-10 2]); 
-pass = isequal(f1,f2);
