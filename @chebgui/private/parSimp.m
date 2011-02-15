91a6366353a1ca479148dbee3fe34436caa0e7d2
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-02-15 00:25:48 +0000

    Brand new and improve parSimp. Guarenteed not to fail. Ever. Remove depricated parent_remove.m

--- @chebgui/private/parSimp.m
+++ @chebgui/private/parSimp.m
@@ -1,7 +1,7 @@
 function str = parSimp(guifile,str)
 % PARSIMP  Remove unnecessary parentheses from string inputs.
 %  parSimp does some basic parsing of the input STR to attempt to remove
-%  unnecessary parenthesis and consecutive +/- pairs.
+%  unnecessary parenthesis, zeros, and consecutive +/- pairs.
 
 if length(str) < 2, return, end
 
@@ -18,18 +18,12 @@ function str = parSimpMain(str)
 % Find all locations of ( and )
 leftParLoc = strfind(str,'(');
 rightParLoc = strfind(str,')');
-% Find true-false vector of operators
-minusVec = str == '-';
-plusVec = str == '+';
-timDivVec = 2*((str == '*') + (str == '/'));
-powerVec = 3*(str == '^');
-opVec = plusVec + minusVec + timDivVec + powerVec;
+% Find weighted vector of operators
+opVec = (str=='-')+(str=='+')+2*((str=='*')+(str=='/'))+3*(str=='^');
 % operator locations
 opLoc = find(opVec);
-opVec(opVec == 0) = inf;
 % Location of characters and numbers
 charLoc = regexp(str,'[A-Za-z0-9]');
-
 
 % Error if the number of ( and ) are not equal
 if length(leftParLoc) ~= length(rightParLoc)
@@ -59,8 +53,87 @@ end
 leftParsLoc = pairsLoc(:,1);
 rightParsLoc = pairsLoc(:,2);
 
+    function removeExtras(flag)
+        % Remove things other than parentheses, like consecutive +/- pairs
+        % and hanging zeros. If there's an input argument, then we don't
+        % attempt to remove zeros.
+        
+        % Remove leading + signs
+        if strcmp(str(1),'+')
+            str(1) = []; 
+            opVec(1) = [];
+            leftParsLoc = leftParsLoc - 1;
+            rightParsLoc = rightParsLoc - 1;
+            opLoc = opLoc - 1;
+            charLoc = charLoc - 1;
+        end
+
+        % Remove consecutive +/- pairs
+        opVec1 = opVec == 1;
+        dOV = ~[abs(diff(opVec1)) 1] & opVec1; % Find adjacent opVec == 1's
+        pm = find(dOV,1); % Get the first
+        if ~isempty(pm) % If +/- pair exists, remove the first occurence
+            if str(pm) == str(pm+1)
+                str(pm) = '+';
+            else
+                str(pm) = '-';
+            end
+            str(pm+1) = [];
+            % Need to update indices and operators
+            opVec(pm+1) = [];
+            leftParsLoc(leftParsLoc > pm) = leftParsLoc(leftParsLoc > pm)-1;
+            rightParsLoc(rightParsLoc > pm) = rightParsLoc(rightParsLoc > pm)-1;
+            opLoc(opLoc > pm) = opLoc(opLoc > pm)-1;
+            charLoc(charLoc > pm) = charLoc(charLoc > pm)-1;
+            removeExtras(1) % recurse
+        end
+
+        if nargin > 0, return, end % +/- pairs recurse locally
+
+        % Remove +0, -0, 0+, 0-, etc
+        zs = strfind(str,'0');
+        for kk = 1:numel(zs)
+            zsk = zs(kk); ls = length(str);
+            if (zsk == 1 && ls > 2 && opVec(zsk+1) == 1) % 0+, 0-
+                shift = 1;
+            elseif ls > 2 &&  zsk==ls && opVec(zsk-1) == 1 %-0, +0
+                shift = 2;
+            elseif (zsk == 1 || zsk == ls) 
+                continue
+            elseif any(zsk-1 == leftParsLoc) && opVec(zsk+1)==1 %(0+, (0-
+                shift = 1;
+            elseif (any(zsk+1 == rightParsLoc) && opVec(zsk-1)==1) ... %+0), -0)
+                || (opVec(zsk-1)==1 && opVec(zsk+1)==1) % +/-0+/-
+                shift = 2;
+            else
+                continue
+            end
+            if shift == 1 % One entry have been removed
+                str(zsk) = [];
+                opVec(zsk) = [];
+            elseif shift == 2 % Two entries have been removed
+                str(zsk-1:zsk) = [];
+                % Need to update indices and operators
+                opVec(zsk-1:zsk) = [];
+                opLoc(opLoc == zsk-1) = [];
+            end
+            % Need to update indices and operators
+            leftParsLoc(leftParsLoc > zsk) = leftParsLoc(leftParsLoc > zsk)-shift;
+            rightParsLoc(rightParsLoc > zsk) = rightParsLoc(rightParsLoc > zsk)-shift;
+            opLoc(opLoc > zsk) = opLoc(opLoc > zsk)-shift;
+            charLoc(charLoc == zsk) = [];
+            charLoc(charLoc > zsk) = charLoc(charLoc > zsk) - shift;
+            zs = zs - shift;
+        end
+    end
+
+removeExtras
+
 % Loop over the pairs
 for pIndex = 1:numOfPars
+    
+    removeExtras
+    
     pLeft = leftParsLoc(pIndex);
     pRight = rightParsLoc(pIndex);
     nextOpLeft = max(opLoc(opLoc < pLeft));
@@ -82,9 +155,8 @@ for pIndex = 1:numOfPars
     for k = 1:numel(idx)
         mask(leftParsLoc(idx(k)):rightParsLoc(idx(k))) = false;
     end
-    mask = isfinite(opVec.*mask) & opVec.*mask > 0;
-    interiorOpVec = opVec.*mask; 
-    interiorOpVec(interiorOpVec==0 | isnan(interiorOpVec)) = inf;
+    interiorOpVec = opVec.*mask;
+    interiorOpVec(interiorOpVec==0) = inf;
     minOpInside = min(interiorOpVec(pLeft:pRight));
     
     % If not, we perform a check to see whether we can remove them.
@@ -100,7 +172,7 @@ for pIndex = 1:numOfPars
             p = str=='+' & interiorPM;
             str(m) = '+'; str(p) = '-';
         end
-        
+
         % Remove parenthesis pairs
         str(pLeft) = [];
         str(pRight-1) = [];
@@ -113,49 +185,6 @@ for pIndex = 1:numOfPars
         rightParsLoc = updateLoc(rightParsLoc,pLeft,pRight);
         opLoc = updateLoc(opLoc,pLeft,pRight);
         charLoc = updateLoc(charLoc,pLeft,pRight);
-    end
-        
-    % Remove leading + signs
-    if strcmp(str(1),'+')
-        str(1) = []; 
-        opVec(1) = [];
-        leftParsLoc = leftParsLoc - 1;
-        rightParsLoc = rightParsLoc - 1;
-        opLoc = opLoc - 1;
-        charLoc = charLoc - 1;
-    end
-    
-    % remove consecutive +/- pairs
-    dOV = diff(opVec);
-    dOV(isnan(dOV)) = inf;
-    pm = find(~dOV,1);
-    if ~isempty(pm)
-        if str(pm) == str(pm+1)
-            str(pm) = '+';
-        else
-            str(pm) = '-';
-        end
-        str(pm+1) = [];
-        % Need to update indices and operators
-        opVec(pm+1) = [];
-        leftParsLoc(leftParsLoc > pm) = leftParsLoc(leftParsLoc > pm)-1;
-        rightParsLoc(rightParsLoc > pm) = rightParsLoc(rightParsLoc > pm)-1;
-        opLoc(opLoc > pm) = opLoc(opLoc > pm)-1;
-        charLoc(charLoc > pm) = charLoc(charLoc > pm)-1;
-    end
-    
-    % remove +0, -0, 0+, 0-
-    zs = strfind(str,'0');
-    for k = 1:numel(zs)
-        zsk = zs(k);
-        if any(zsk-1 == leftParsLoc) && opVec(zsk+1)==1
-            str(zsk) = [];
-            % Need to update indices and operators
-            opVec(zsk) = [];
-            leftParsLoc(leftParsLoc > zsk) = leftParsLoc(leftParsLoc > zsk)-1;
-            rightParsLoc(rightParsLoc > zsk) = rightParsLoc(rightParsLoc > zsk)-1;
-            opLoc(opLoc > zsk) = opLoc(opLoc > zsk)-1;
-        end
     end
     
 end
