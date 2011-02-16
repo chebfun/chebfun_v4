1a8ccf4c2b00639efffba4d4da661023c60f64c2
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-02-16 18:18:23 +0000

    Minor bug fix for linop and chebop EIG. If more than one input arg was given, an error would be returned about the nargin rather than the pointer to use EIGS.

--- @chebop/eig.m
+++ @chebop/eig.m
@@ -1,4 +1,6 @@
-function eig(A)
+function eig(varargin)
+% Use EIGS to find selected eigenvalues of a linear operator.
+% See also chebop/eigs
 
 error('chebfun:chebop:eig','Use EIGS to find selected eigenvalues of a linear operator.')
 
