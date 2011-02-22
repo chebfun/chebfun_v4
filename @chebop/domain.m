794aa5d17658eaeccb977c6ec0f0983ea72897cf
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-02-22 17:38:38 +0000

    Adding missing chebop/domain from previous commit.

---
+++ @chebop/domain.m
@@ -0,0 +1,12 @@
+function dom = domain(A)
+% DOMAIN   Domain of function defintion.
+
+% Copyright 2008 by Toby Driscoll.
+% See http://www.maths.ox.ac.uk/chebfun.
+
+%  Last commit: $Author: driscoll $: $Rev: 907 $:
+%  $Date: 2009-12-04 15:49:08 +0000 (Fri, 04 Dec 2009) $:
+
+dom = A.dom;
+
+end
