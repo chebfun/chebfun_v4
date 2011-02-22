f2b13a20e71789d610da85779710a23d1840f415
gonnet <gonnet@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-02-22 13:19:09 +0000

    implemented missing boolean operators on quasimatrices, added chebtest for boolean ops.

--- @chebfun/ge.m
+++ @chebfun/ge.m
@@ -2,16 +2,38 @@ function h = ge(f,g)
 % GE (>=) for chebfuns
 %   H = F >= G where F and/or G are chebfuns constructs a logical chebfun H
 %   which is true (i.e. takes the value 1) where F >= G, and false (0) elsewhere.
-%
-%   Quasimatrices are not yet supported.
 
-if numel(f) > 1 || numel(g) > 1
-    error('CHEBFUN:ge:quasi','ge is not yet implemented for quasimatrices.');
+
+% Quasimatrices?
+nf = numel(f); ng = numel(g);
+if nf > 1 || ng > 1
+    if nf == 1
+        h = g;
+        for k=1:ng
+            h(k) = ge(f,g(k));
+        end;
+    elseif ng == 1
+        h = f;
+        for k=1:nf
+            h(k) = ge(f(k),g);
+        end;
+    elseif nf == ng
+        h = f;
+        for k=1:nf
+            h(k) = ge(f(k),g(k));
+        end;
+    else
+        error('CHEBFUN:ge:quasi','Quasi-matrix dimensions must agree');
+    end
+
+% Nope, no quasimatrices.
+else
+
+    h = heaviside(f-g);
+    h.imps(h.imps == .5) = 1;
+    for k = 1:h.nfuns
+        if h.funs(k).vals == .5, h.funs(k).vals = 1; end
+    end    
+    h = merge(h);
+    
 end
-
-h = heaviside(f-g);
-h.imps(h.imps == .5) = 1;
-for k = 1:h.nfuns
-    if h.funs(k).vals == .5, h.funs(k).vals = 1; end
-end    
-h = merge(h);
--- @chebfun/le.m
+++ @chebfun/le.m
@@ -2,16 +2,38 @@ function h = le(f,g)
 % LE (<=) for chebfuns
 %   H = F <= G where F and/or G are chebfuns constructs a logical chebfun H
 %   which is true (i.e. takes the value 1) where F <= G, and false (0) elsewhere.
-%
-%   Quasimatrices are not yet supported.
 
-if numel(f) > 1 || numel(g) > 1
-    error('CHEBFUN:le:quasi','le is not yet implemented for quasimatrices.');
+
+% Quasimatrices?
+nf = numel(f); ng = numel(g);
+if nf > 1 || ng > 1
+    if nf == 1
+        h = g;
+        for k=1:ng
+            h(k) = le(f,g(k));
+        end;
+    elseif ng == 1
+        h = f;
+        for k=1:nf
+            h(k) = le(f(k),g);
+        end;
+    elseif nf == ng
+        h = f;
+        for k=1:nf
+            h(k) = le(f(k),g(k));
+        end;
+    else
+        error('CHEBFUN:le:quasi','Quasi-matrix dimensions must agree');
+    end
+
+% Nope, no quasimatrices.
+else
+
+    h = heaviside(g-f);
+    h.imps(h.imps == .5) = 1;
+    for k = 1:h.nfuns
+        if h.funs(k).vals == .5, h.funs(k).vals = 1; end
+    end    
+    h = merge(h);
+    
 end
-
-h = heaviside(g-f);
-h.imps(h.imps == .5) = 1;
-for k = 1:h.nfuns
-    if h.funs(k).vals == .5, h.funs(k).vals = 1; end
-end    
-h = merge(h);
