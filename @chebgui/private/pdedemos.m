0805d42c584b8be64defa258e80ce76c1f5a9641
trefethen <trefethen@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-02-23 10:28:42 +0000

    minor change to pdedemos.m

--- @chebgui/private/pdedemos.m
+++ @chebgui/private/pdedemos.m
@@ -3,7 +3,7 @@ if nargin < 3
     mode = 'start';
 end
 
-numberOfExamples = 10;
+numberOfExamples = 11;
 
 % If called with a 0, open with an empty gui. If called with a number less
 % than 0, bigger than the number of available examples, or no argument,
@@ -40,9 +40,18 @@ switch exampleNumber
         init = 'u = sin(2*x)';
         tol = '1e-6'; plotting = 'on';
         fixYaxisLower = '-1'; fixYaxisUpper = '1';
-        name = 'Advection-diffusion equation';
+        name = 'Advection-diffusion equation 1';
         demotype = 'scalar';
     case 2
+        a = '-1'; b = '1'; t = '0:.0125:.25';
+        DE = 'u_t = 0.3*u" + 10*u''';
+        LBC = '0'; RBC = '0';
+        init = 'u = exp(-10*x^4/(1-x^2))';
+        tol = '1e-6'; plotting = 'on';
+        fixYaxisLower = '0'; fixYaxisUpper = '1';
+        name = 'Advection-diffusion equation 2';
+        demotype = 'scalar';
+    case 3
         a = '-3'; b = '3'; t = '0:0.2:10';
         DE = 'u_t = .01*u'''' + u - u^3';
         LBC = 'periodic';
@@ -52,7 +61,7 @@ switch exampleNumber
         name = 'Allen-Cahn equation';
         fixYaxisLower = '-1'; fixYaxisUpper = '1';
         demotype = 'scalar';
-    case 3
+    case 4
         a = '-1'; b = '1'; t = '0:.04:5';
         DE = 'u_t = -(u^2)'' + .02*u"';
         LBC = 'dirichlet'; RBC = 'dirichlet';
@@ -61,7 +70,7 @@ switch exampleNumber
         name = 'Burgers equation';
         fixYaxisLower = '0'; fixYaxisUpper = '0.8';
         demotype = 'scalar';
-    case 4
+    case 5
         a = '-3'; b = '3'; t = '0:.025:1.6';
         DE = 'u_t = -.003*u'''''''' + (u^3-u)''''';
         LBC = {'u = -1','u''=0'}; RBC = {'u = -1','u''=0'};
@@ -70,7 +79,7 @@ switch exampleNumber
         fixYaxisLower = '-1'; fixYaxisUpper = '1.2';
         name = 'Cahn-Hilliard equation';
         demotype = 'scalar';
-    case 5
+    case 6
         a = '-4'; b = '4'; t = '0:.01:1.5';
         DE = 'u_t = 0.1*u''''';
         LBC = 'dirichlet'; RBC = 'dirichlet';
@@ -79,7 +88,7 @@ switch exampleNumber
         fixYaxisLower = '-3'; fixYaxisUpper = '3';
         name = 'Heat equation';
         demotype = 'scalar';
-    case 6
+    case 7
         a = '-1'; b = '1'; t = '0:.1:4';
         DE = {'u_t = .02*u" + cumsum(u)*sum(u)'};
         LBC = {'dirichlet'};
@@ -89,7 +98,7 @@ switch exampleNumber
         name = 'Integro-differential equation';
         fixYaxisLower = '0'; fixYaxisUpper = '1.4';
         demotype = 'scalar';
-    case 7
+    case 8
         a = '-1'; b = '1'; t = '0:.01:2';
         DE = 'u_t = u*u'' - u" - 0.006*u""';
         LBC = {'u = 1','u'' = 2'}; RBC = {'u = 1','u'' = 2'};
@@ -98,7 +107,7 @@ switch exampleNumber
         fixYaxisLower = '-30'; fixYaxisUpper = '30';
         name = 'Kuramoto-Sivashinsky equation';
         demotype = 'scalar';
-    case 8
+    case 9
         a = '-1'; b = '1'; t = '0:.1:3';
         DE = {'u_t = 0.1*u" - 100*u*v';'v_t = .2*v" - 100*u*v';'w_t = 0.001*w" + 200*u*v'};
         LBC = {'neumann'}; RBC = {'neumann'};
@@ -107,7 +116,7 @@ switch exampleNumber
         fixYaxisLower = '0'; fixYaxisUpper = '2.2';
         name = 'Diffusion and reaction of three chemicals';
         demotype = 'system';
-    case 9
+    case 10
         a = '-1'; b = '1';
         t = '0:.1:2';
         DE = {'u_t = u" - v' ; 'v" - u = 0'};
@@ -118,7 +127,7 @@ switch exampleNumber
         fixYaxisLower = '0.5'; fixYaxisUpper = '1';
         name = 'Coupled PDE-BVP';
         demotype = 'system';
-    case 10
+    case 11
         a = '-2'; b = '2';
         t = '0:.1:10';
         DE = {'u_t = v''','v_t = u'''};
