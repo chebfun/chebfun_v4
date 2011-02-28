dac328a4128704102357716addae701b858aa9ea
birkisson <birkisson@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-02-28 13:11:44 +0000

    * Last minute fix to chebop/diff. The function will for now still return a   linop, rather than a linear chebop. Since this is a fairly advanced method,   and when a user calls it he/she is probably really interested in the linear   operator, we should be OK with letting this be the only function where the   output is a linop. The treatment of BCs need to be dealt with before   next release.

--- @chebop/diff.m
+++ @chebop/diff.m
@@ -1,7 +1,10 @@
 function J = diff(N,u,flag)
 %DIFF    Jacobian (Frechet derivative) of nonlinear operator.
-% J = DIFF(N,U) for a chebop N and chebfun U, returns a chebop
-% represening the Jacobian (i.e., the Frechet derivative) of N evaluated at
+%
+% Please note this method is experimental in Chebfun version 4.0.
+%
+% J = DIFF(N,U) for a chebop N and chebfun U, returns a linop
+% representing the Jacobian (i.e., the Frechet derivative) of N evaluated at
 % U. More specifically, J is the operator such that
 %
 %   || N(u+delta) - N(u) - J*delta || / ||delta|| -> 0
@@ -13,7 +16,7 @@ function J = diff(N,u,flag)
 % Note that J includes boundary conditions, if any are specified for N.
 % Each condition, given in the form g(u)=0, produces a linear boundary
 % condition in J in the form g'(u)*delta. The operator g'(u) is assigned
-% as a corresponding boundary condition of J (with homogeneous value).
+% as a corresponding boundary condition of J.
 %
 % Example: A basic Newton iteration to solve u''-exp(u)=0, subject to
 %          u(0)=1, u(1)*u'(1)=1
@@ -24,62 +27,39 @@ function J = diff(N,u,flag)
 %   N = chebop(d,@(u)diff(u,2)-exp(u),lbc,rbc);
 %   for k = 1:6
 %     r = N(u);  J = diff(N,u);
-%     J.lbc(1) = {J.lbc(1).op, -feval(lbc(u),0)};
-%     J.rbc(1) = {J.rbc(1).op, -feval(rbc(u),1)};
 %     delta = -J\r;  u = u+delta;
 %   end
 %   plot(u), title(sprintf('|| residual || = %.3e',norm(N(u))))
 %
-% See also chebfun/diff.
+% See also chebfun/diff, chebop/linop.
 
 % Copyright 2011 by The University of Oxford and The Chebfun Developers. 
 % See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
 
-% Boundary conditions part
-ab = N.dom.ends;
-a = ab(1); b = ab(end);
+% Initialise
+if nargin < 3, flag = 0; end
 
-% Left BC
-if ~isempty(N.lbc)
-    if ~iscell(N.lbc), lbc = {N.lbc};   % wrap singleton in cell
-    else lbc = N.lbc;
-    end
-    for j = 1:length(lbc)
-        gu = lbc{j}(u);
-        linBC.left(j) = struct('op',diff(gu,u,'linop'),'val',gu(a,j));
-    end
-end
+[L linBC isLin affine] = linearise(N,u,flag);
 
-% Right BC
-if ~isempty(N.rbc)
-    if ~iscell(N.rbc), rbc = {N.rbc};   % wrap singleton in cell
-    else rbc = N.rbc;
-    end
-    for j = 1:length(rbc)
-        gu = rbc{j}(u);
-        linBC.right(j) = struct('op',diff(gu,u,'linop'),'val',gu(b,j));
-    end
-end
-
-% Functional part + add BCs
-Nu = N.op(u);
-if exist('linBC','var')
-    J = diff(Nu,u,'linop') & linBC;
-else
-    J = diff(Nu,u,'linop');
-end
-
-if nargin == 3 && strcmp(flag,'linop')
-    return
-end
+J = L & linBC;
 
 % No flag, so return a chebop
-F = J;
-J = chebop(get(F,'fundomain'));
-J.op = F; 
-if ~isempty(inputname(1)) && ~isempty(inputname(2))
-    s = ['diff(' inputname(1) ',' inputname(2) ')'];
-else
-    s = '';
-end
-J = set(J,'opshow',s);
+% F = J;
+% J = chebop(get(L,'fundomain'));
+% J.op = L;
+% 
+% if ~isempty(inputname(1)) && ~isempty(inputname(2))
+%     s = ['diff(' inputname(1) ',' inputname(2) ')'];
+% else
+%     s = '';
+% end
+% J = set(J,'opshow',s);
+% 
+% if ~isempty(linBC.left)
+%     J.lbc = linBC.left;
+%     J.lbcshow = 'Linear operator';
+% end
+% if ~isempty(linBC.right)
+%     J.rbc = linBC.right;
+%     J.rbcshow = 'Linear operator';
+% end
