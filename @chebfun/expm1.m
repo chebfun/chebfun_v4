00cd6782a8fae8831ad9498c7db853ac605fc103
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-06-08 13:31:23 +0000

    Added EXPM1 and LOGP1 chebfun methods and support in the CHEBGUI lexer.

---
+++ @chebfun/expm1.m
@@ -0,0 +1,7 @@
+function Fout = expm1(F)
+% EXPM1 computes EXP(F)-1 of a chebfun F.
+
+% Copyright 2011 by The University of Oxford and The Chebfun Developers. 
+% See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
+
+Fout = exp(F)-1;
---
+++ @chebfun/log1p.m
@@ -0,0 +1,7 @@
+function Fout = log1p(F)
+% LOG1P computes LOG(1+F) of a chebfun F.
+
+% Copyright 2011 by The University of Oxford and The Chebfun Developers. 
+% See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
+
+Fout = log(1+F);
