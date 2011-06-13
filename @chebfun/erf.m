c1a6f8154c26e4d518cc66566956dd3deed2cadb
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-06-13 14:51:43 +0000

    Removing redundant composition in CHEBFUN/ERF.M.

--- @chebfun/erf.m
+++ @chebfun/erf.m
@@ -5,8 +5,6 @@ function Fout = erf(F)
 % See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
 
 Fout = comp(F, @(x) erf(x));
-
-Fout = comp(F, @(x) erf(x));
 for k = 1:numel(F)
   Fout(k).jacobian = anon('diag1 = diag(2*exp(-F.^2)/sqrt(pi)); der2 = diff(F,u,''linop''); der = diag1*der2; nonConst = ~der2.iszero;',{'F'},{F(k)},1);
   Fout(k).ID = newIDnum();
