4a7de0b9f50cc1a9f456822a904c354780d36f67
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-06-23 08:53:37 +0000

    Adding new trig-d overloads. These require chebtests and inclusion into the chebgui parser.

--- @chebfun/cscd.m
+++ @chebfun/cscd.m
@@ -1,5 +1,5 @@
 function Fout = cscd(F)
-% CSCD   Cosecant of a chebfun with argument in degrees.
+% CSCD   Cosecant of a chebfun in degrees.
 
 % Copyright 2011 by The University of Oxford and The Chebfun Developers. 
 % See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
@@ -11,6 +11,6 @@ end
 
 Fout = comp(F, @(x) cscd(x));
 for k = 1:numel(F)
-    Fout(k).jacobian = anon('diag1 = diag(-(pi/180)*cot((pi/180)*F).*cscd(F)); der2 = diff(F,u,''linop''); der = diag1*der2; nonConst = ~der2.iszero;',{'F'},{F(k)},1);
+    Fout(k).jacobian = anon('diag1 = diag((-pi/180)*cotd(F).*cscd(F)); der2 = diff(F,u,''linop''); der = diag1*der2; nonConst = ~der2.iszero;',{'F'},{F(k)},1);
     Fout(k).ID = newIDnum();
 end
