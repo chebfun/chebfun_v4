ee44628c24562e1322124261712ff9ec404276ba
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-07-25 12:18:44 +0000

    Added initial implementation of POLYEIGS for chebops/linops (incl a chebtest). More work needed on code, documentation, and examples.

---
+++ chebtests/level3/chebop_polyeigs.m
@@ -0,0 +1,33 @@
+function pass = chebop_polyeigs
+% Test the chebop polyeigs method.
+% Nick Hale, july 2011
+
+%Tolerance
+tol = 1000*chebfunpref('eps');
+
+% Precomputed.
+precomp = [   
+   1.359355061649265
+  -1.876033230374035
+   3.000000000000368
+  -3.537162428575435
+   4.643868772821693
+  -5.186237295747643];
+
+%% With linops
+[d x] = domain(-1,1);
+A = diff(d,2); A.lbc = 0; A.rbc = 0;
+B = -diag(x)*diff(d);
+C = eye(d);
+[V D] = polyeigs(A,B,C,6,0);
+
+pass(1) = norm(D-precomp,inf) < tol;
+
+%% With chebops
+A = chebop(@(x,u) diff(u,2),[-1 1],'dirichlet');
+B = chebop(@(x,u) -x.*diff(u));
+C = chebop(@(x,u) u);
+[V D] = polyeigs(A,B,C,6,0);
+
+pass(2) = norm(D-precomp,inf) < tol
+
