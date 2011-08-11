dce187aa30d51a7ecf7eb5d8d98a58ba2eecb21e
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-08-11 13:11:10 +0000

    Updating documentation for CHEBFUN/SUBSREF.M GET.M and SET.M. Closes #162.

--- @chebfun/get.m
+++ @chebfun/get.m
@@ -1,11 +1,20 @@
 function val = get(f, propName)
 % GET   Get chebfun properties.
 % P = GET(F,PROP) returns the property P specified in the string PROP from
-% the chebfun F. The string PROP can be 'funs', 'ends' or 'imps', to
-% retrieve the cell array of funs, the vector with endpoints or the matrix
-% with Dirac impulses respectively. Or 'nfuns', 'points', 'scl', 'vals',
-% 'exps', or 'trans'. If F is a column (row) quasimatrix, GET will only 
-% return a list of the requested properties for each columns (rows).
+% the chebfun F. Valid entries for the string PROP are:
+%   'FUNS'   - Smooth components ('funs') of F.
+%   'NFUNS'  - Number of piecewise components in F (i.e., numel(F.FUNS)).
+%   'DOMAIN' - Endpoints of F.
+%   'ENDS'   - Endpoints and breakpoints of F.
+%   'IMPS'   - Values F takes at the F.ENDS.
+%   'VALS'   - Values of F at Chebyshev points (see F.POINTS).
+%   'POINTS' - Points at which F.VALS are stored.
+%   'SCL'    - Vertical scale of F.
+%   'TRANS'  - Orientation of F (i.e., column or row chebfun).
+%   'EXPS'   - Exponents used for unbounded chebfuns.
+%   'MAP'    - Map(s) used by F. (See "help maps")
+% If F is a column (row) quasimatrix, GET will return a list of the
+% requested properties for each column (row).
 
 % Copyright 2011 by The University of Oxford and The Chebfun Developers. 
 % See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
@@ -13,11 +22,6 @@ function val = get(f, propName)
 val = [];
 
 if numel(f) > 1
-%     if strcmpi(propName,'trans'), val = f(1).trans; return
-%     elseif strcmpi(propName,'domain'), val = f(1).ends([1 end]); return
-%     else
-%         warning('CHEBFUN:get:quasi','''Get'' should not be called with quasimatrices');
-%     end
     val = cell(numel(f));
     for k = 1:numel(f)
         val{k} = get(f(k), propName);
