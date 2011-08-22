95bcc95be222f091f34fe182a4d8f6cc7bfe5365
birkisson <birkisson@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-08-22 13:57:41 +0000

    Avoiding use of randi (which apparently wasn't introduced until version 2008b) in chebgui.

--- @chebgui/loadexample.m
+++ @chebgui/loadexample.m
@@ -17,7 +17,13 @@ D = dir(bvppath);
 % exampleNumber == -1 denotes a random example is required
 if exampleNumber == -1
     numberOfDemos = length(D)-2; % First two entries are . and ..
-    selected = randi(numberOfDemos)+2;
+    % This can be used in V5.0
+%     selected = randi(numberOfDemos)+2;
+    
+    % Avoid use of randi which was introduced in v2008b
+    % Create a random integer in 1:numberOfDemos
+    randnum = floor(numberOfDemos*rand)+1;
+    selected = randnum + 2; % Need to shift the random integer by 2
 else
     selected = exampleNumber + 2;
 end
