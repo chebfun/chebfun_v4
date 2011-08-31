2930ad5a4e9f01efbe069b0158a7547421238dfc
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-08-31 14:22:36 +0000

    Fixed the way varmat displays matrices with large entries (by dividing out the large exponent and dislaying in the same way matlab would in the command wnidow).
    >> D = diff(domain(-1,1))
    D = linop
       operating on chebfuns defined on:                          
         interval [-1,1]                                          
       with n = 6 realization:                                    
         -8.5000   10.4721   -2.8944    1.5279   -1.1056    0.5000
         -2.6180    1.1708    2.0000   -0.8944    0.6180   -0.2764
          0.7236   -2.0000    0.1708    1.6180   -0.8944    0.3820
         -0.3820    0.8944   -1.6180   -0.1708    2.0000   -0.7236
          0.2764   -0.6180    0.8944   -2.0000   -1.1708    2.6180
         -0.5000    1.1056   -1.5279    2.8944  -10.4721    8.5000
       with functional representation:                            
         @(u)diff(u,m)                                            
       and differential order 1 
                                      
    >> 1e300*D
    ans = linop
       operating on chebfuns defined on:                          
         interval [-1,1]                                          
       with n = 6 realization:
        1.0e+300 *                     
         -8.5000   10.4721   -2.8944    1.5279   -1.1056    0.5000
         -2.6180    1.1708    2.0000   -0.8944    0.6180   -0.2764
          0.7236   -2.0000    0.1708    1.6180   -0.8944    0.3820
         -0.3820    0.8944   -1.6180   -0.1708    2.0000   -0.7236
          0.2764   -0.6180    0.8944   -2.0000   -1.1708    2.6180
         -0.5000    1.1056   -1.5279    2.8944  -10.4721    8.5000
       with functional representation:                            
         @(u) B*feval(op,u)                                       
       and differential order 1 
    
    Notice the scale invariance! ;)
    
    This closes #195.

--- @varmat/char.m
+++ @varmat/char.m
@@ -12,7 +12,14 @@ else
   if nargin == 1
       s1 = ['   with n = ',int2str(defreal),' realization:'];
       try
-          s2 = num2str(feval(V,defreal),'  %8.4f');
+          Vmat = feval(V,defreal);
+          M = max(max(abs(Vmat)));
+          if M > 1e3;
+              M = 10^round(log10(M)-1);
+              Vmat = Vmat/M;
+              s1 = [s1 sprintf('\n    %2.1e * ',M)];
+          end
+          s2 = num2str(Vmat,'  %8.4f');
       catch
           s2 = 'WARNING - varmat cannot be displayed. Possibly piecewise.';
       end
@@ -20,7 +27,14 @@ else
       numints = numel(dom.endsandbreaks);
       defreal = max(2,ceil(defreal/(numints-1)));
       s1 = ['   with n = ',int2str(defreal),' realization:'];
-      s2 = num2str(feval(V,{defreal,[],dom}),'  %8.4f');
+      Vmat = feval(V,{defreal,[],dom});
+      M = max(max(abs(Vmat)));
+      if M > 1e3;
+          M = 10^round(log10(M)-1);
+          Vmat = Vmat/M;
+          s1 = [s1 sprintf('\n    %2.1e * ',M)];
+      end
+      s2 = num2str(Vmat,'  %8.4f');
   end
   space = ' ';
   s2 = [ repmat(space,size(s2,1),5) s2 ];
