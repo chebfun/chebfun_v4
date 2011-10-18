1d00d31211698c988d5f1e55f4aed81964ffa941
guettel <guettel@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-10-18 08:54:54 +0000

    Floater-Hormann interpolation by Georges Klein added

--- chebsnake.m
+++ chebsnake.m
@@ -7,10 +7,14 @@ function chebsnake(nodes,alpha)
 %   Any other key will quit the game.
 %
 %   CHEBSNAKE(NODES) allows one to change the
-%   interpolation nodes from Chebyshev nodes
-%   (NODES = 'cheby', default) to equispaced 
-%   nodes ('equi'). The blue dots on the snake 
-%   indicate the interpolated function values.
+%   interpolation type. The default type 'cheby' 
+%   is polynomial interpolation in Chebyshev points.
+%   Other types are polynomial interpolation in 
+%   equispaced points ('equi') and 
+%   Floater-Hormann rational interpolation in 
+%   equispaced points ('fh'). 
+%   The blue dots on the snake indicate the 
+%   interpolated function values.
 %
 %   CHEBSNAKE(NODES,ALPHA) allows to change the
 %   initial game speed by a factor ALPHA > 0,
@@ -22,13 +26,14 @@ function chebsnake(nodes,alpha)
 %   number of achieved points...
 
     % get some constants right
-    if nargin < 2, alpha = 1; end;
-    if nargin > 0 && strcmp(nodes,'equi'), nodes = 0; else nodes = 1; end;
+    if nargin < 2, alpha = 1; end
+    if nargin > 0 && strcmp(nodes,'equi'), nodes = 0; 
+    elseif nargin > 0 && strcmp(nodes,'fh'), nodes = 2;  else nodes = 1; end
     LW = 'LineWidth'; lw = 2;
     res = 0.15; len = 5; dom = domain(-1,1); d = 1;
     food = @() res*(round((1.8*rand-.9)/res)+1i*round((1.8*rand-.9)/res));
     pause on
-
+    
     % keyboard interaction
     figure('KeyPressFcn',@keypress);
     function keypress(~,evnt)
@@ -53,7 +58,7 @@ function chebsnake(nodes,alpha)
         hf = plot(real(f),imag(f),'md','MarkerSize',10,'MarkerFaceColor','m');
         ht = plot(8,0);                     % dummy handle
         title('Control the snake with arrow keys. Quit with any other key.');
-        axis([-1,1,-1,1]); shg; pause(.3);
+        axis([-1,1,-1,1]); shg; pause(0.3);
         pts = 0;                            % points counter
         t = 1;                              % convex factor for nodes
         tic;
@@ -62,14 +67,18 @@ function chebsnake(nodes,alpha)
             if t > 1,
                 t = 0; dr = res*d;
                 s = [ s(2:end),s(end)+dr ];
-                if length(s) < len+pts, s = [ s(2),s ]; end;
-            end;
+                if length(s) < len+pts, s = [ s(2),s ]; end
+            end
             y = (1-t)*s(1:end-1)+t*s(2:end);
-            if nodes,
+            if nodes==1,
                 c = chebfun(y);
-            else
+            elseif nodes==2,
+                fhd = min(ceil(0.4*sqrt(length(y))),4);
+                c = bary(linspace(-1,1,5*length(y)),y,linspace(-1,1,...
+                    length(y)),weights(length(y)-1,fhd));
+            elseif nodes==0
                 c = polyfit(linspace(-1,1,length(y)),y,length(y)-1,dom);
-            end;
+            end
             for k = 1:numel(hs1)
                 if isnan(hs1(k)), continue, end
                 delete(hs1(k));
@@ -83,15 +92,15 @@ function chebsnake(nodes,alpha)
             if max(abs([real(y(end)),imag(y(end))])) > 1 || ...
                    min(abs(y(end)-y(1:end-1))) < res/2,
                 ht = plot(.8*scribble('game over'),'r',LW,lw); 
-                shg; pause(1); break;
-            end;
+                shg; pause(1); break
+            end
             if abs(y(end)-f) < res/2, % snake eats food ?
                 pts = pts + 1; alpha = alpha * 1.003;
                 title(['Points : ' num2str(pts)]);
                 f = food();
                 set(hf,'XData',real(f),'YData',imag(f));
-            end;
-        end;
+            end
+        end
         for k = 1:numel(ht)
             if isnan(ht(k)), continue, end
             delete(ht(k));
@@ -99,4 +108,18 @@ function chebsnake(nodes,alpha)
     end;
     plot(.8*scribble('goodbye'),'r',LW,lw);
     shg; pause(1); close(gcf);
+
+    function w = weights(n,fhd) % weights for Floater-Hormann interpolation
+        w = zeros(1,n+1);
+        for l = 0:n
+            ji = max(l-fhd,0);
+            jf = min(l,n-fhd);
+            sumcoeff = zeros(jf-ji+1,1);
+            for i=ji:jf
+                sumcoeff(i-ji+1) = nchoosek(fhd,l-i);
+            end
+            w(l+1) = (-1)^(l-fhd)*sum(sumcoeff);
+        end
+    end
+
 end % chebsnake()
