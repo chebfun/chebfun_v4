895b7525b76798d843de8738ee1434a5442d4246
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-10-20 11:10:01 +0000

    Adding an example to jump.m help text, as suggested my LNT

--- @chebfun/jump.m
+++ @chebfun/jump.m
@@ -1,6 +1,11 @@
 function j = jump(u,x,c)
 % JUMP   Compute the jump in a chebfun over a breakpoint.
 % J = JUMP(F,X) is simply a wrapper for F(X,'right')-U(F,'left')
+%
+% Example
+%   x = chebfun('x');
+%   jump(sign(x),0)      % returns 2
+% 
 % See also chebfun/feval.
 
 % Copyright 2011 by The University of Oxford and The Chebfun Developers. 
