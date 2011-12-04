53e99c9e6ff20cf572669484afb2b2bd5a5e382c
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2011-12-04 18:54:38 +0000

    Minor change to linop char for complex operators and fix to OrrSom Example.

--- @linop/char.m
+++ @linop/char.m
@@ -13,7 +13,8 @@ else
 
   if all(A.blocksize==[1 1])
     if ~isempty(A.varmat)
-      s = char(s,char(A.varmat,A.fundomain),' ');
+%       s = char(s,char(A.varmat,A.fundomain),' ');
+      s = char(s,mat2char(A),' ');
     end
     if ~isempty(A.oparray)
       s = char(s, '   with functional representation:',' ',...
@@ -47,4 +48,49 @@ else
       s = char(s,['   and ' int2str(A.numbc) ' boundary conditions'],' ' );
     end
   end
+end
+
+end
+
+
+function s = mat2char(V)
+% MAT2CHAR  Convert the varmat to pretty-print string (incl. BCs)
+
+print_bc = true; % Print BCs or not
+defreal = 6;  % Size of matrix to display
+
+dom = V.fundomain;
+numints = numel(dom.endsandbreaks);
+if numints == 1 && isempty(V,'bcs')
+    print_bc = false;
+end
+defreal = max(2,ceil(defreal/(numints-1)));
+s1 = ['   with n = ',int2str(defreal),' realization:'];
+% Evaluate the linop
+if print_bc
+    Vmat = feval(V,defreal,'bc',dom);
+else
+    Vmat = feval(V,defreal,'nobc',dom);
+end
+% Some pretty scaling for large/small numbers
+M = max(max(abs(Vmat)));
+if (M > 1e2 || M < 1) && M ~= 0
+  M = 10^round(log10(M)-1);
+  Vmat = Vmat/M;
+  s1 = [s1 sprintf('\n    %2.1e * ',M)];
+end
+if isreal(Vmat)
+    s2 = num2str(Vmat,'  %8.4f');
+    for k = 1:size(s2,1)
+        s2(k,:) = strrep(s2(k,:),'0.0000','     0');
+    end
+else
+    s2 = num2str(Vmat,'%8.2f');
+    for k = 1:size(s2,1)
+        s2(k,:) = strrep(s2(k,:),'0.00','   0');
+    end
+end
+% Pad with appropriate spaces
+s2 = [ repmat(' ',size(s2,1),5) s2 ];
+s = char(s1,'',s2);
 end
