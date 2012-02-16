f6c98eb5f19821ea0c7bb576c13242da0519424f
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2012-02-16 10:54:09 +0000

    Adding new Examples and updating some old ones.

--- examples/ode/ParamODEs.m
+++ examples/ode/ParamODEs.m
@@ -102,4 +102,5 @@ norm(uv(1,2)-31.836463244694285264)
 
 %% References
 % [1] Chebyshev Spectral Methods and the Lane-Emden Problem by John Boyd.
+%
 % [2] http://www2.maths.ox.ac.uk/chebfun/examples/ode/html/LaneEmden.shtml
