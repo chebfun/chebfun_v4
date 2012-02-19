2b2ee47b0d140e51c05270170253670120567ae6
trefethen <trefethen@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2012-02-19 08:49:07 +0000

    This test currently fails.  I am about to file a bug report.

---
+++ chebtests/level0/lebesguetest.m
@@ -0,0 +1,13 @@
+function pass = lebesguetest
+% Check behavior of LEBESGE
+% Nick Trefethen, February 2012
+% A level 0 Chebtest.
+
+% An arbitrary small test:
+[L,Lconst] = lebesgue([-1 0 1],[-1,2]);
+pass(1) = norm([7 7]-[Lconst max(L)],inf) < 100*chebfunpref('eps');
+
+% This failed because of a bug in Feb 2012:
+s = linspace(.25,1,17); s = [-s s];
+[L,Lconst] = lebesgue(s);
+pass(2) = min(L.vals)>.999;
