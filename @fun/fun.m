e5039b33c2b96027d4abd45e5afeaa308f34977c
gonnet <gonnet@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2012-02-21 16:42:16 +0000

    allow the construction of an array of funs by passing the parameters for each fun packed in a cell array.

--- @fun/fun.m
+++ @fun/fun.m
@@ -12,9 +12,16 @@
 % Here SCL is a structure with fields SCL.H (horizontal scale) and SCL.V 
 % (vertical scale).
 %
+% FUN(C) for a cell array C creates a vector of funs using the entries of
+% C as its arguments, i.e. [ fun( C{1}{:} ) , fun( C(2){:} ) , ... ].
+%
 % Additionally, exponents can be pass within PREF by attaching them in a cell
 % array to PREF.EXPS, and a non-adaptive call can be forced by setting
 % PREF.N to be a positive integer.
+%
+% [G,ISH] = FUN(...) returns the constructed fun G and the boolean ISH,
+% which is true if the construction is believed to have converged and false
+% otherwise.
 %
 % FUN creates an empty fun.
 
@@ -24,24 +31,32 @@
 classdef fun
     
     properties ( GetAccess = 'public' , SetAccess = 'public' )
-        n = 0;         % The length of the fun
-        vals = [];     % Fun values at Chebyshev points
-        coeffs = [];   % Chebyshev coeffs of fun
-        ends = [-1 1]; % Domain of the fun
-        exps = [0 0];  % Exponents: f(x)=(x-a)^exps(1)*(b-x)*exps(b)*p(x)
-        scl = struct('h',[],'v',[]); % Scale info (horizontal and vertical)
-        map = struct('for',[],'inv',[],'der',[],'name',[],'par',[]); % map 
+        n = 0;
+        vals = [];
+        coeffs = [];
+        points = [];
+        ends = [-1 1];
+        exps = [0 0];
+        scl = struct('h',[],'v',[]);
+        map = struct('for',[],'inv',[],'der',[],'name',[],'par',[]);
     end
     
     properties ( GetAccess = 'private' , SetAccess = 'private' )
-        ish = true;    % Happiness indicator. Did fun converege happily?
+        ish = true;
     end
     
     methods
         
         function g = fun(varargin)
             if nargin == 0, return, end
-            g = ctor(g,varargin{:});
+            if nargin == 1 && iscell( varargin{1} ),
+                data = varargin{1}; f0 = fun;
+                for k=1:length(data)
+                    g(k) = ctor( f0 , data{k}{:} );
+                end
+            else
+                g = ctor(g,varargin{:});
+            end
         end
         
     end
