a4c2c2f8d110469b7765adc7e04453d7a6c682a8
gonnet <gonnet@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2012-02-22 14:59:11 +0000

    discretize in the absence of exponents and non-linear maps.

--- @chebfun/mtimes.m
+++ @chebfun/mtimes.m
@@ -47,21 +47,122 @@ if (isa(F1,'chebfun') && isa(F2,'chebfun'))
       chebfunpref('splitting',splitstate)
       chebfunpref('resampling',sampstate)
     else      % inner product
+    
       funreturn = F1(1).funreturn || F2(1).funreturn;
       if funreturn
           Fout = chebconst;
       else
           Fout = zeros(size(F1,1),size(F2,2));
       end
-      for k = 1:size(F1,1)
-        for j = 1:size(F2,2)
-          if F1(k).trans && ~F2(j).trans
-            Fout(k,j) = sum((F1(k).').*F2(j));
-          else
-            error('CHEBFUN:mtimes:dim','Chebfun dimensions must agree.')
-          end
+      
+      % Check for exponents
+      hasexps = false;
+      for k=1:numel(F1), f = F1(k,:); for j=1:f.nfuns
+        hasexps = hasexps || any( f.funs(j).exps ~= 0 );
+      end; end
+      for k=1:numel(F2), f = F2(:,k); for j=1:f.nfuns
+          hasexps = hasexps || any( f.funs(j).exps ~= 0 );
+      end; end
+        
+      % Check for non-linear maps
+      nonlinmap = false;
+      for k=1:numel(F1), f = F1(k,:); for j=1:f.nfuns
+        nonlinmap = nonlinmap || ~strcmp( f.funs(j).map.name , 'linear' );
+      end; end
+      for k=1:numel(F2), f = F2(:,k); for j=1:f.nfuns
+        nonlinmap = nonlinmap || ~strcmp( f.funs(j).map.name , 'linear' );
+      end; end
+
+      % If the quasimatrices F1 and F2 have neither exponents nor non-linear
+      % maps, then we can compute a discretized inner product. This is done
+      % by discretizing both F1 and F2 on a chebyshev grid of the size of
+      % the sum of their lengths and computing the inner products relative
+      % to the Clensaw-Curtis quadrature weights.
+      if ~hasexps && ~nonlinmap
+      
+        % Get the outer dimensions
+        m = size(F1,1); n = size(F2,2);
+      
+        % Get the set of breakpoints for all funs in F1 and F2
+        ends = [];
+        for k=m, ends = union( ends , F1(k,:).ends ); end
+        for k=n, ends = union( ends , F2(:,k).ends ); end
+        iends = ends(2:end-1)';
+        nfuns = length(ends) - 1;
+        
+        % Get the sizes in F1 and F2
+        sizes1 = zeros( nfuns , m );
+        for k=1:m
+            f = F1(k,:); ef = f.ends;
+            for j=1:nfuns
+                sizes1(j,k) = f.funs( find( ef > (ends(j)+ends(j+1))/2 , 1 ) - 1 ).n;
+            end
         end
-      end
+        sizes1 = max( sizes1 , [] , 2 );
+        sizes2 = zeros( nfuns , n );
+        for k=1:n
+            f = F2(:,k); ef = f.ends;
+            for j=1:nfuns
+                sizes2(j,k) = f.funs( find( ef > (ends(j)+ends(j+1))/2 , 1 ) - 1 ).n;
+            end
+        end
+        sizes2 = max( sizes2 , [] , 2 );
+        
+        % Get the product sizes.
+        sizes = sizes1 + sizes2;
+        inds = [ 0 ; cumsum(sizes) ];
+        
+        % Create the chebyshev nodes and quadrature weights
+        [ pts , w ] = chebpts( sizes , ends );
+
+        % Discretize F1 and F2. Do this fun-wise to get the right values
+        % at the edges of the funs if there are jumps.
+        dF1 = zeros( sum(sizes) , m );
+        if nfuns > 1
+            for k=1:m
+                f = F1(k,:); ef = f.ends;
+                for j=1:nfuns
+                    ind = find( ef > (ends(j)+ends(j+1))/2 , 1 ) - 1;
+                    dF1( inds(j)+1:inds(j+1) , k ) = feval( f.funs(ind) , pts( inds(j)+1:inds(j+1) ) );
+                end
+            end
+        else
+            for k=1:m
+                dF1( : , k ) = chebpolyval( [ zeros( sizes(j) - F1(k,:).funs.n , 1 ) ; F1(k,:).funs(1).coeffs ] );
+            end
+        end
+        dF2 = zeros( sum(sizes) , n );
+        if nfuns > 1
+            for k=1:n
+                f = F2(:,k); ef = f.ends;
+                for j=1:nfuns
+                    ind = find( ef > (ends(j)+ends(j+1))/2 , 1 ) - 1;
+                    dF2( inds(j)+1:inds(j+1) , k ) = feval( f.funs(ind) , pts( inds(j)+1:inds(j+1) ) );
+                end
+            end
+        else
+            for k=1:n
+                dF2( : , k ) = chebpolyval( [ zeros( sizes(j) - F2(:,k).funs.n , 1 ) ; F2(:,k).funs.coeffs ] );
+            end
+        end
+        
+        % Form the inner products
+        Fout = dF1.' * spdiags(w',0,inds(end),inds(end)) * dF2;
+        
+      % Otherwise, same old...
+      else
+      
+          for k = 1:size(F1,1)
+            for j = 1:size(F2,2)
+              if F1(k).trans && ~F2(j).trans
+                Fout(k,j) = sum((F1(k).').*F2(j));
+              else
+                error('CHEBFUN:mtimes:dim','Chebfun dimensions must agree.')
+              end
+            end
+          end
+          
+      end % new stuff?
     end
 
 % Chebfun times double
@@ -74,12 +175,96 @@ elseif isa(F1,'chebfun')
         end
      % quasimatrix times matrix of doubles
     else
+    
+        % Check the inner size.
         if size(F1,2)~=size(F2,1), error('CHEBFUN:mtimes:dim','Dimensions must agree'), end
-        for j = 1:size(F2,2)
-            Fout(j) =  mtimescol(F2(1,j),F1(1));
-            for i = 2:size(F2,1)
-                Fout(j) = Fout(j) + mtimescol(F2(i,j),F1(i));
+        
+        % Check for exponents
+        hasexps = false;
+        for k=1:numel(F1), f = F1(:,k); for j=1:f.nfuns
+          hasexps = hasexps || any( f.funs(j).exps ~= 0 );
+        end; end
+
+        % Check for non-linear maps
+        nonlinmap = false;
+        for k=1:numel(F1), f = F1(:,k); for j=1:f.nfuns
+          nonlinmap = nonlinmap || ~strcmp( f.funs(j).map.name , 'linear' );
+        end; end
+
+        % If the quasimatrix F1 has neither exponents nor non-linear maps,
+        % it is discretized over a grid of Chebyshev nodes of the length
+        % of the largest column. The column sums are then formed over the
+        % discretized columns and reassembled as Chebfuns.
+        if ~hasexps && ~nonlinmap
+        
+            % Get a hand on some useful values
+            n = size(F1,2);
+        
+            % Get the set of breakpoints for all funs in F1
+            ends = [];
+            for k=1:n, ends = union( ends , F1(:,k).ends ); end
+            iends = ends(2:end-1)';
+            m = length(ends)-1;
+            
+            % Discretize all columns of F1
+            sizes = zeros( m , n );
+            for k=1:n
+                f = F1(:,k); ef = f.ends;
+                for j=1:m
+                    sizes(j,k) = f.funs( find( ef > (ends(j)+ends(j+1))/2 , 1 ) - 1 ).n;
+                end
             end
+            sizes = max( sizes , [] , 2 ); inds = [ 0 ; cumsum(sizes) ];
+            dF = zeros( sum(sizes) , n );
+            if m > 1
+                for k=1:n
+                    f = F1(:,k); ef = f.ends;
+                    for j=1:m
+                        ind = find( ef > (ends(j)+ends(j+1))/2 , 1 ) - 1;
+                        dF( inds(j)+1:inds(j+1) , k ) = feval( f.funs(ind) , chebpts( sizes(j) , ends(j:j+1) ) );
+                    end
+                end
+            else
+                for k=1:n
+                    if F1(:,k).funs.n == sizes(1)
+                        dF( : , k ) = F1(:,k).funs.vals;
+                    else
+                        dF( : , k ) = chebpolyval( [ zeros( sizes(1) - F1(:,k).funs.n , 1 ) ; F1(:,k).funs.coeffs ] );
+                    end
+                end
+            end
+            
+            % Sum the columns
+            res = dF * F2;
+            
+            % Re-assemble the chebfun(s)
+            for k=1:size(res,2)
+                g = chebfun;
+                g.nfuns = m;
+                g.ends = ends;
+                g.scl = norm( res( : , k ) , inf );
+                g.imps = res( [ 1 ; inds(2:end) ] , k )';
+                f = {};
+                for j=1:m
+                    f{end+1} = { res( inds(j)+1:inds(j+1) , k ) , ends(j:j+1) };
+                    g.imps(j+1) = res(inds(j+1),k);
+                end
+                g.funs = simplify( fun( f ) );
+                % g.jacobian = anon( '[dF,nonConst]=diff(F,u,''linop''); der=dF*x;' , {'x','F'} , { F2(:,k) , F1 } , 1 , 'mtimes' );
+                Fout(:,k) = g;
+            end
+        
+        % Otherwise, don't use new features.
+        else
+            
+            % Straight-forward scaling and addition of chebfuns.
+            for j = 1:size(F2,2)
+                Fout(j) =  mtimescol(F2(1,j),F1(1));
+                for i = 2:size(F2,1)
+                    Fout(j) = Fout(j) + mtimescol(F2(i,j),F1(i));
+                end
+            end
+            
         end
     end
 else
