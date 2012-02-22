3e760e820a99c0c360ffaae9d584e1ae0178835f
gonnet <gonnet@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2012-02-22 14:30:17 +0000

    qr is fast enough for this now.

--- chebtests/level2/qrtest.m
+++ chebtests/level2/qrtest.m
@@ -9,8 +9,7 @@ function pass = qrtest
 tol = chebfunpref('eps');
 x = chebfun('x',[0 1]);
 A = [x 1i*x 1 1+1i (2-1i)*x];
-%pass(1) = (rank(A)==2);
-pass(1) = 1; % commented out to save time
+pass(1) = (rank(A)==2);
 [Q,R] = qr(A);
 pass(2) = (abs(cond(Q)-1)<1e-13*(tol/eps));
 pass(3) = (norm(A-Q*R)<1e-13*(tol/eps));
