7a1e339234057f10079a5435bd4e2d9de92fec4f
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2012-02-29 15:26:51 +0000

    Fixed bug in modified FUN/PROLONG method (coeffs were being treated in the wrong order). Closes #231.

--- @fun/prolong.m
+++ @fun/prolong.m
@@ -31,33 +31,33 @@ else % Use FFTs to prolong
         g.vals = chebpolyval(c); g.n = nout; g.coeffs = c;
     else
         
-        % Old version of prolong
-        % % To shorten a fun, we need to consider aliasing
-        % c = c(end:-1:1);
-        % calias = zeros(nout,1);
-        % nn = 2*nout-2;
-        % calias(1) = sum(c(1:nn:end));        
-        % for k = 2:nout-1
-        %     calias(k) = sum(c(k:nn:end))+sum(c(nn-k+2:nn:end));
-        % end
-        % calias(nout) = sum(c(nout:nn:end));
-        % calias = calias(end:-1:1);
-        % g.vals = chebpolyval(calias); g.n = nout; 
+%         % Old version of prolong
+%         % % To shorten a fun, we need to consider aliasing
+%         c = c(end:-1:1);
+%         calias = zeros(nout,1);
+%         nn = 2*nout-2;
+%         calias(1) = sum(c(1:nn:end));        
+%         for k = 2:nout-1
+%             calias(k) = sum(c(k:nn:end))+sum(c(nn-k+2:nn:end));
+%         end
+%         calias(nout) = sum(c(nout:nn:end));
+%         calias = calias(end:-1:1);
+%         g.vals = chebpolyval(calias); g.n = nout; 
 
         % Reduce to just one coeff?
+        c = c(end:-1:1);
         if nout == 1
             g.vals = sum( c(end:-2:1) );
             g.coeffs = g.vals;
             g.n = 1;
-        % Otherpread the trailing coeffs
+        % Otherwise spread the trailing coeffs
         else
-            c = c(end:-1:1);
             nn = 2*nout - 2;
             for j=nout+1:length(c)
                 k = abs( mod( j+nout-3 , nn ) - nout + 2 ) + 1;
                 c(k) = c(k) + c(j);
             end
-            c = c(1:nout);
+            c = c(nout:-1:1);
             g.vals = chebpolyval(c); g.n = nout; g.coeffs = c;
         end
         
