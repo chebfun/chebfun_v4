0e64bb0570e8b914a02705590ed08a85b7f59c42
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2012-03-07 10:14:52 +0000

    Added some support for vertical concatenation in jacobian information

---
+++ @anon/private/vertcatJac.m
@@ -0,0 +1,28 @@
+function L = vertcatJac(L,varargin)
+% VERTCATJAC - Vertical concatenation of jacobians
+%
+% VERTCATJAC(DERS{:}) is called by the jacobian info in chebfun/vertcat and 
+% returns the vertically concatenated jacobians of the input chebfuns DERS.
+
+while ~isempty(varargin)
+    A = L;
+    B = varargin{1}; varargin(1) = [];
+
+    opA = get(A,'oparray'); opB = get(B,'oparray');
+    varA = get(A,'varmat'); varB = get(B,'varmat');
+    op = @(u) [ opA(u) ; opB(u)];
+    mat = @(n) [ varA(n) ; varB(n) ];
+
+    domA = A.domain;  domB = B.domain;
+    dom = domain(union(domA.endsandbreaks,domB.endsandbreaks));
+
+    difforder = max(A.difforder, B.difforder);
+    iszero = A.iszero && B.iszero;
+    isdiag = A.isdiag && B.isdiag;
+
+    L = linop(mat, op, dom, difforder);
+    L.iszero = iszero;
+    L.isdiag = isdiag;
+end
+
+end
