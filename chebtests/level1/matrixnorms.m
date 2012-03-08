e62d8ed5e2f50f81d18eb4b8cbf0fe48a74be2f0
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2012-03-08 11:38:19 +0000

    Minor loosening of tolerance in this chebtest.

--- chebtests/level1/matrixnorms.m
+++ chebtests/level1/matrixnorms.m
@@ -3,7 +3,7 @@ function pass = matrixnorms
 % norms. It checks the computed matrix norms are correct.
 % Nick Trefethen  31 May 2008
 
-
+tol = 10*eps;
 x = chebfun(1,[0 1]);
 A = [0*x 0*x 0*x x 0*x];
 norms = zeros(4,1);
@@ -11,4 +11,4 @@ norms(1) = norm(A,1);
 norms(2) = norm(A,2);
 norms(3) = norm(A,inf);
 norms(4) = norm(A,'fro');
-pass = (norm(norms-ones(4,1))==0);
+pass = abs(norms-ones(4,1)) < tol;
