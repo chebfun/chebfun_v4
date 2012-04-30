35e1a696df24ac4ce319556a896a7d49461e06a6
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2012-04-30 08:37:35 +0000

    Removing buggy 'simple' plots from plot data. Closes #262.

--- @chebfun/private/plotdata.m
+++ @chebfun/private/plotdata.m
@@ -74,30 +74,9 @@ if isempty(f)
     % initialise y limits (auto adjusted if there are exps)
     top = -inf; bot = inf;
     
-    % If all chebfuns have one piece and the same map, we can simplify
-    % things and plot at an oversampled chebyshev grid, which is much
-    % faster to evaluate than equally spaced points
-    issimple = all([g.nfuns]==1);
-    if issimple
-        map = g(1).funs(1).map;
-        issimple = ~any(g(1).funs(1).exps);
-        for k = 2:numel(g)
-            issimple = strcmp(g(k).funs(1).map.name,map.name) && ~any(g(k).funs(1).exps);
-            if ~issimple, break, end
-        end
-    end
-    
     a = dom(1); b = dom(2);
-    if issimple
-        % oversampled (mapped) chebyshev grid
-        numpts = max(numpts,pi/2*length(g));
-        n = power(2,ceil(log2(numpts)));
-        fl = chebpts(n);
-        fl = map.for(fl);
-    else
-        % equispaced points over domain
-        fl = linspace(a,b,numpts).';
-    end
+    % equispaced points over domain
+    fl = linspace(a,b,numpts).';
     
     % find all the ends and get rid of high order imps
     ends = [];
@@ -122,15 +101,7 @@ if isempty(f)
     [ignored indx2] = sort(indx);
 
     % line values of g
-    if issimple
-        gl = zeros(n,numel(g));
-        for k = 1:numel(g)
-            gl(:,k) = get(prolong(g(k).funs(1),n),'vals');
-        end
-        gl(mask,:) = [];
-    else
-        gl = feval(g,fl);
-    end
+    gl = feval(g,fl);
     
     if fl(1) == a, 
         for k = 1:size(gl,2)
