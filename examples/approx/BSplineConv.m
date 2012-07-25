b5ea201cbfec2cc8cbbe8b61aa710778989c818b
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2012-07-25 12:43:55 +0000

    Tidying Examples.

--- examples/approx/BSplineConv.m
+++ examples/approx/BSplineConv.m
@@ -1,5 +1,5 @@
 %% B-splines and convolution
-% Nick Trefethen, July 2012
+% Nick Trefethen, 24th July 2012
 
 %%
 % (Chebfun example approx/BSplineConv.m)
@@ -36,13 +36,11 @@ hold on, plot(pts,B2(pts),'.k',MS,ms)
 title('B-spline of order 2',FS,fs)
 
 %%
-% As the titles of the plots indicate, these functions
-% are known as B-splines.  In our notation the B-spline
-% $B_n$ is a $C^{n-1}$ piecewise polynomial of degree $n$ with
-% support $[-(n+1)/2,(n+1)/2]$ and breakpoints at the half-integers in
-% this interval.
-% The B-splines form a good basis for numerical computation
-% with splines. Here is B3:
+% As the titles of the plots indicate, these functions are known as
+% B-splines.  In our notation the B-spline $B_n$ is a $C^{n-1}$ piecewise
+% polynomial of degree $n$ with support $[-(n+1)/2,(n+1)/2]$ and
+% breakpoints at the half-integers in this interval. The B-splines form a
+% good basis for numerical computation with splines. Here is B3:
 B3 = conv(B0,B2);
 hold off, plot(B3,LW,lw), axis(ax), grid on
 pts = [pts-.5 max(pts)+.5];
@@ -58,28 +56,25 @@ hold on, plot(pts,B4(pts),'.k',MS,ms)
 title('B-spline of order 4',FS,fs)
 
 %%
-% B-splines were introduced by Schoenberg and became a standard
-% method for numerical computation following the work of de Boor [1]
-% and Cox [2] in 1972.  In general there is no need for the
-% breakpoints to be uniformly spaced.  With uniformly spaced points
-% as in our example, however, the
-% shape approaches a Gaussian as $n\to\infty$.  One
-% could derive this fact by noting that in Fourier space, each
-% convolution is a multiplication by the Fourier transform of $B_0$,
-% namely a sinc function.  This is the same computation that arises
-% in the proof of the Central Limit Theorem, and in fact, the
-% B splines have a probabilistic interpretation:
-% if $B_0$ represents a random variable with
-% uniform distribution on the interval $[-1/2,1/2]$
-% then $B_1$ is the distribution for the sum of two independent samples
-% from $B_0$, $B_2$ is the distribution for the sum of three samples
-% from $B_0$, and so on.
+% B-splines were introduced by Schoenberg and became a standard method for
+% numerical computation following the work of de Boor [1] and Cox [2] in
+% 1972.  In general there is no need for the breakpoints to be uniformly
+% spaced.  With uniformly spaced points as in our example, however, the
+% shape approaches a Gaussian as $n\to\infty$.  One could derive this fact
+% by noting that in Fourier space, each convolution is a multiplication by
+% the Fourier transform of $B_0$, namely a sinc function.  This is the same
+% computation that arises in the proof of the Central Limit Theorem, and in
+% fact, the B splines have a probabilistic interpretation: if $B_0$
+% represents a random variable with uniform distribution on the interval
+% $[-1/2,1/2]$ then $B_1$ is the distribution for the sum of two
+% independent samples from $B_0$, $B_2$ is the distribution for the sum of
+% three samples from $B_0$, and so on.
 
 %%
 % References:
 %
-% [1] C. de Boor, On calculating with B-splines, Journal of
-% Approximation Theory 6 (1972), 50-62.
+% [1] C. de Boor, On calculating with B-splines, Journal of Approximation
+% Theory 6 (1972), 50-62.
 %
 % [2] M. Cox, The numerical evaluation of B-splines, Journal of the
 % Institute of Mathematics and its Applications 10 (1972), 134-149.
--- examples/stats/CentralLimitTheorem.m
+++ examples/stats/CentralLimitTheorem.m
@@ -1,37 +1,35 @@
 %% Central limit theorem
-% Nick Trefethen, July 2012
+% Nick Trefethen, 25th July 2012
 
 %%
 % (Chebfun example approx/CentralLimitTheorem.m)
 % [Tags: #centrallimittheorem, #convolution, #CONV, #probability]
 
 %%
-% The central limit theorem is one of the most striking results
-% in the theory of probability.  It says that if you take the mean of
-% $n$ independent samples from any random variable, then as $n\to\infty$,
-% the distribution of these means approaches a normal distribution, i.e.,
-% a Gaussian or bell curve.
-% For example, if you toss a coin $n$ times, the number of
-% heads you get is given by the binomial distribution, and this approaches
-% the bell curve.
+% The central limit theorem is one of the most striking results in the
+% theory of probability.  It says that if you take the mean of $n$
+% independent samples from any random variable, then as $n\to\infty$, the
+% distribution of these means approaches a normal distribution, i.e., a
+% Gaussian or bell curve. For example, if you toss a coin $n$ times, the
+% number of heads you get is given by the binomial distribution, and this
+% approaches the bell curve.
 
 %% 
-% More specifically, let $X_1, \dots , X_n$ be independent samples
-% from a distrubtion with mean $\mu$ and variance $\sigma^2<\infty$,
-% and consider the sample mean
-% $$ S_n = n^{-1} \sum_{k=1}^n X_n . $$
-% The law of large numbers asserts that $S_n \to  \mu$ as
-% almost surely as $n\to\infty$.
-% The central limit theorem asserts that the random variables
-% $\sqrt n (S_n-\mu)$ converge in distribution to the normal
-% distribution $N(0,\sigma^2)$.  Details are given in many
-% textbooks of probability and statistics.
+% More specifically, let $X_1, \dots , X_n$ be independent samples from a
+% distrubtion with mean $\mu$ and variance $\sigma^2<\infty$, and consider
+% the sample mean $$ S_n = n^{-1} \sum_{k=1}^n X_n . $$ The law of large
+% numbers asserts that $S_n \to  \mu$ as almost surely as $n\to\infty$. The
+% central limit theorem asserts that the random variables $\sqrt n
+% (S_n-\mu)$ converge in distribution to the normal distribution
+% $N(0,\sigma^2)$.  Details are given in many textbooks of probability and
+% statistics.
 
 %%
-% The Chebfun CONV command makes it possible to illustrate the
-% central limit theorem for general distributions, because the probability
-% distribution associated with the sum of random variables is given
-% by a convolution.  For example, consider this triangular probability distribution:
+% The Chebfun CONV command makes it possible to illustrate the central
+% limit theorem for general distributions, because the probability
+% distribution associated with the sum of random variables is given by a
+% convolution.  For example, consider this triangular probability
+% distribution:
 X = chebfun(0,'(4/3+x)/2',0,[-3 -4/3 2/3 3]);
 LW = 'linewidth'; lw = 1.6; ax = [-3 3 -.2 1.2];
 hold off, plot(X,LW,lw,'jumpline','b'), axis(ax), grid on
@@ -45,10 +43,10 @@ mu = sum(t.*X)
 variance = sum(t.^2.*X)
 
 %% 
-% Let us superimpose on the plot the normal distribution of
-% this mean and variance:
+% Let us superimpose on the plot the normal distribution of this mean and
+% variance:
 sigma = sqrt(variance);
-gauss = @(sigma) chebfun(@(t) exp(-.5*(t/sigma).^2)/(sigma*sqrt(2*pi)),[-3 3]);;
+gauss = @(sigma) chebfun(@(t) exp(-.5*(t/sigma).^2)/(sigma*sqrt(2*pi)),[-3 3]);
 hold on, plot(gauss(sigma),'r',LW,lw)
 title('Distribution of X compared with normal distribution',FS,fs)
 
@@ -79,7 +77,10 @@ hold on, plot(gauss(sigma),'r',LW,lw)
 
 %%
 % Convolutions like these appear in another Chebfun Example, called
-% "B-splines and convolutions" (approx/BSplineConv).  The only difference
-% is that in that case we start with a uniform rather than triangular
-% distribution.
+% "B-splines and convolutions" [1].  The only difference is that in that
+% case we start with a uniform rather than triangular distribution.
+
+%%
+% References
+% [1] http://www.chebfun.org/examples/approx/BSplineConv.shtml
 
