2b288c7616e9ffab70356ab64a4c96d512ad7d50
xuk <xuk@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2012-10-26 11:41:03 +0000

    a second sinc test

---
+++ chebtests/basic/sinctest2.m
@@ -0,0 +1,82 @@
+function pass = sinctest
+% Test sinc function and related functions
+% Kuan Xu, October 2012
+
+% Construct sinc function by definition.
+
+dom = [-10 10];
+fexact = chebfun('sin(x)/x',dom, 'vectorizing');
+
+% Set the tolerance
+tol = 100 * chebfunpref('eps') * norm(fexact, inf);
+
+% 1. Test the Chebfun sinc function
+
+x = chebfun('x', dom);
+f = sinc(x);
+pass(1) = norm(f - fexact, inf) < tol;
+
+% Test more sinc-like or sinc-related functions.
+% 2. Test sinc^2(x)
+
+fexact = chebfun('sin(x)^2/(x^2)', dom, 'vectorizing');
+f = sinc(x).^2;
+pass(2) = norm(f - fexact, inf) < tol;
+
+% 3. Test sinc(x^2)
+
+fexact = chebfun('sin(x^2)/(x^2)', dom, 'vectorizing');
+f = sinc(x.^2);
+pass(3) = norm(f - fexact, inf) < tol;
+
+% 4. Test on shift and frequency
+T = 5;
+for i = 1:T
+    a = 1e2*rand(2,1);
+    fexact = chebfun(@(x) sin(a(1)*(x-a(2)))/(a(1)*(x-a(2))), dom, 'vectorizing');
+    
+    f = sinc(a(1)*(x-a(2)));
+    pass(3+i) = norm(f - fexact, inf) < tol;
+end
+
+% 5. Test on differentiation of sinc(x)
+
+f = sinc(x);
+fp = diff(f);
+fpp = diff(f,2);
+
+fexact = chebfun('sin(x)/x', dom, 'vectorizing');
+fpexact = diff(fexact);
+fppexact = diff(fexact,2);
+
+pass(T+4) = norm(fp-fpexact, inf) < tol;
+pass(T+5) = norm(fpp-fppexact, inf) < tol;
+
+% 6. Test on integration of sinc(x)
+
+f = sinc(x);
+fi = sum(f);
+
+fexact = chebfun('sin(x)/x', dom, 'vectorizing');
+fiexact = sum(fexact);
+
+pass(T+6) = norm(fiexact - fi, inf) < tol;
+
+% Test on other relevant integrals
+% 7. Integration of sinc(x)^2
+
+f = sinc(x).^2;
+fi = sum(f);
+fexact = chebfun('sin(x)^2/(x^2)', dom, 'vectorizing');
+fiexact = sum(fexact);
+
+pass(T+7) = norm(fiexact - fi, inf) < tol;
+
+% 8. Integration of sinc(x^2)
+
+f = sinc(x.^2);
+fi = sum(f);
+fexact = chebfun('sin(x^2)/(x^2)', dom, 'vectorizing');
+fiexact = sum(fexact);
+
+pass(T+8) = norm(fiexact - fi, inf) < tol;
