abc4b0edfb6584db1b85d6f5aad08bad626fd01b
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2012-11-21 18:06:19 +0000

    Adding new example.

---
+++ examples/approx/InteractiveInterp.m
@@ -0,0 +1,95 @@
+%% Interactive Interpolation
+% Nick Hale, 21st November 2012
+
+%%
+% (Chebfun example approx/InteractiveInterp.m) 
+% [Tags: #interpolation, #INTERP1, #LEBESGUE]
+
+function InteractiveInterp
+
+%%
+% This example is based upon some code that Rodrigo Platte once gave me to
+% demostrate what makes a good set of interpolation points (and of course,
+% what doesn't!).
+
+%%
+% The game is to select interpolation points by clicking on the function.
+% The code will then compute the interpolant through these points and plot
+% it on the same graph. For those inteerested in the details, it computes
+% the interpolant in barycentric form using Chebfun's BARY and BARY_WEIGHTS
+% functions.
+
+%%
+% Unfortunately, I'm not allowed to play this game any more, but here's
+% an animation of me clicking:
+if any(strcmp({getenv('USER'),getenv('UserName')},'hale'))
+    plot(0) % ignore this!
+    return
+end
+
+%%
+% Now it's your turn! If you're reading this example, you probably already
+% know what kind of points to choose, but it's still fun to play!
+    
+ClickToInterpolate()
+
+%%
+
+    function ClickToInterpolate(F)
+        % Select interpolation points by right clicking.
+        % When you get bored, left or double click to finish.
+        if nargin == 0,
+          F = @(x) 1-.9*abs(x);          % default function 
+        end
+        
+        % initialise
+        x = []; d = domain(-1,1);
+        LW = 'LineWidth'; MS = 'MarkerSize'; FS = 'FontSize';
+        % loop 
+        while 1                          % keep clicking!
+          hold off
+          plot(xx,F(xx),'-k',LW,2), shg  % plot function F
+          hold on, axis([-1 1 -1 2])
+          if ~isempty(x)
+            plot(x,F(x),'.b',MS,20)      % plot interpolation points
+            plot(x,0*x,'.k',MS,6)        % plot x values alone
+            y = interp1(x,F(x),d);       % interpolate the data
+            plot(y,'-b',LW,2), shg       % plot interpolant
+            if numel(x) > 1,
+                [~,L] = lebesgue(x,d);   % lebesgue constant
+                title(['Lebesgue constant = ' num2str(L)],FS,14)
+            end
+          end
+          [gx,gy,button] = ginput(1);    % input new interpolation point
+          if button == 3, break, end     % if right button, stop
+          x = unique([x; gx]);           % #ok<AGROW>
+        end
+    end
+
+%%
+% P.S. If you want to see how a greedy person would choose their points,
+% check out the Greedy Interpolation example [1].
+
+end
+
+%%
+% References:
+%
+% [1] http://www.chebfun.org/examples/approx/html/GreedyInterp.shtml
+
+
+%%
+% junk
+%         % for writing only!
+%         n = n+1;
+%         frame = getframe(1);
+%         im = frame2im(frame);
+%         [imind,cm] = rgb2ind(im,256);
+%         set(gcf,'Color','w')
+%         if n == 1;
+%             imwrite(imind,cm,'InteractiveInterp_01.gif','gif', 'Loopcount',inf,'DelayTime',1,'BackgroundColor',1);
+%         else
+%             imwrite(imind,cm,'InteractiveInterp_01.gif','gif','WriteMode','append','DelayTime',1,'BackgroundColor',1);
+%         end
+%       end
+            
