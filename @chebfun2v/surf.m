89b2740430657954f1d7a21e97ed7aa8d28b7b3a
townsend <townsend@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2013-02-28 18:51:52 +0000

    Changing the surface plot command of a chebfun2v with three non-zero components. Now plots the represented surface.

--- @chebfun2v/surf.m
+++ @chebfun2v/surf.m
@@ -1,7 +1,13 @@
 function varargout = surf(f,varargin)
-%SURF, Surf plot of a chebfun2v.
+%SURF Plot of the surface represented by a chebfun2v.
 % 
-% SURF(F) is the surface plot of sqrt(F.^2)
+% SURF(F) is the surface plot of F, where F is a chebfun2v with three 
+% components.
+%
+% SURF(F,...) allows for the same plotting options as Matlab's SURF
+% command.
+%
+% See also CHEBFUN2/SURF. 
 
 % Copyright 2013 by The University of Oxford and The Chebfun Developers.
 % See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
@@ -11,36 +17,21 @@ if isempty(f)
    return; 
 end
 
-
-ish = ishold;
-
-% Just plot the magnitude for now. 
-minsample = 200; 
-fx = f.xcheb; fy = f.ycheb; 
-rect = fx.corners;
-
-x = linspace(rect(1),rect(2),minsample);
-y = linspace(rect(3),rect(4),minsample);
-[xx,yy] = meshgrid(x,y);
-
-%% 
-% Plot using Matlab's surf command. 
-
 if isempty(f.zcheb)
-    if ( isempty(varargin) )
-        h=surf(xx,yy,sqrt(fx(xx,yy).^2 + fy(xx,yy).^2));
-    else
-        h=surf(xx,yy,sqrt(fx(xx,yy).^2 + fy(xx,yy).^2),varargin);
-    end
-else
-    fz = f.zcheb;
-    if ( isempty(varargin) )
-        h=surf(xx,yy,sqrt(fx(xx,yy).^2 + fy(xx,yy).^2 + fz(xx,yy).^2));
-    else
-        h=surf(xx,yy,sqrt(fx(xx,yy).^2 + fy(xx,yy).^2 + fz(xx,yy).^2),varargin);
-    end
+    error('CHEBFUN2V:SURF','Chebfun2v does not represent a surface as it has only two components');
 end
 
-if ~ish, hold off; end;
-if nargout > 0, varargout = {h}; end
+% short code, by making varargin non-empty
+if ( isempty(varargin) )
+    varargin = {}; 
+end
+
+
+% straight call to chebfun2/surf
+h = surf(f.xcheb, f.ycheb, f.zcheb, varargin{:}); 
+
+if ( nargout > 1 )
+    varargout = {h}; 
+end
+
 end
