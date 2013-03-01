cec00928d4d80fe7ac60b0d93ac58992f55fe79d
townsend <townsend@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2013-03-01 09:01:49 +0000

    Changing the cylinder command so that it returns a CHEBFUN2V representing the surface of revolution. Big moment: First Chebfun command that relies on Chebfun2.

--- @chebfun/cylinder.m
+++ @chebfun/cylinder.m
@@ -1,29 +1,37 @@
-function varargout = cylinder(r, n, varargin)
+function varargout = cylinder(r)
 %CYLINDER Generate cylinder.
-%   [X, Y, Z] = CYLINDER(R, N) forms the unit cylinder based on the sampled
-%   generator curve in the chebfun R. The cylinder has N points around the 
-%   circumference.
-%   [X, Y, Z] = CYLINDER(R) defaults to N = 50. SURF(X,Y,Z) displays the
-%   cylinder.
+%
+%   [X, Y, Z] = CYLINDER(R) forms the unit cylinder based revolving the 
+%   function R about the z-axis. X, Y, and Z are chebfun2 objects such that
+%   surf(X,Y,Z) displays the cylinder. 
+%
+%   F = CYLINDER(R) constructs the chebfun2v that represents the surface of
+%   revolution. SURF(F) displays the cylinder.
 %
 %   Omitting output arguments causes the cylinder to be displayed with a SURF
 %   command and no outputs to be returned.
 
-% Copyright 2011 by The University of Oxford and The Chebfun Developers. 
+% Copyright 2013 by The University of Oxford and The Chebfun Developers. 
 % See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
 
 % Convert r to discrete values:
 ends = r.ends;
-r = feval(r, linspace(ends(1), ends(end), 101));
 
-% Make the resulution a bit higher:
-if ( nargin < 2 )
-    n = 50;
+d = [ends 0 2*pi];
+
+f = chebfun2(@(x,y) feval(r,x), d); 
+u = chebfun2(@(u,v) u, d);
+v = chebfun2(@(u,v) v, d);
+
+F = [f.*sin(v); f.*cos(v); u];  % surface of revolution.
+
+if ( nargout == 0 )
+    surf(F);  % plot
+elseif ( nargout == 1 )
+    % return chebfun2v
+    varargout = {F}; 
+else
+    varargout = {F(1), F(2), F(3)};  % return as a parameterisation. 
 end
 
-% Call the discrete cylinder function:
-if ( nargout == 0 )
-    cylinder(r, n, varargin{:});
-else
-    [varargout{1}, varargout{2}, varargout{3}] = cylinder(r, n, varargin{:});
 end
