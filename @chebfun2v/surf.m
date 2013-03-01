66a18c604dba55d84f156e12fbcbbdede024f8b7
townsend <townsend@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2013-03-01 15:49:42 +0000

    Implementing a good idea by LNT: Draw the seams in the parameterisations of surfaces.  This is accessed with the syntax surf(F,'-'), where F is a chebfun2v of three components.

--- @chebfun2v/surf.m
+++ @chebfun2v/surf.m
@@ -1,20 +1,23 @@
 function varargout = surf(f,varargin)
 %SURF Plot of the surface represented by a chebfun2v.
-% 
-% SURF(F) is the surface plot of F, where F is a chebfun2v with three 
+%
+% SURF(F) is the surface plot of F, where F is a chebfun2v with three
 % components.
 %
 % SURF(F,...) allows for the same plotting options as Matlab's SURF
 % command.
 %
-% See also CHEBFUN2/SURF. 
+% See also CHEBFUN2/SURF.
 
 % Copyright 2013 by The University of Oxford and The Chebfun Developers.
 % See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
 
+
+ish = ishold;
+
 if isempty(f)
-   surf([]);
-   return; 
+    surf([]);
+    return;
 end
 
 if isempty(f.zcheb)
@@ -23,15 +26,77 @@ end
 
 % short code, by making varargin non-empty
 if ( isempty(varargin) )
-    varargin = {}; 
+    varargin = {};
+end
+
+% plot seams in coordinate representation.
+if ( ~isempty(varargin) )
+    if ( length(varargin{1})<5 )       
+        % Only option with <=3 letters is a colour, marker, line
+        ll = regexp(varargin{1},'[-:.]+','match');
+        cc = regexp(varargin{1},'[bgrcmykw]','match');  % color
+        mm = regexp(varargin{1},'[.ox+*sdv^<>ph]','match');  % marker
+        
+        if ( ~isempty(ll) )
+            if ( strcmpi(ll{1},'.') )
+                % so we have . first. Don't plot a line.
+                ll = {};
+            elseif ( strcmpi(ll{1},'.-') )
+                % so we have . first. Don't plot a line.
+                ll{1} = '-';
+            end
+        end
+        plotline = ~isempty(ll);  % plot row and col pivot lines?
+        
+        
+        if isempty(cc), cc{1}= 'k';end  % default to black seam. 
+        
+        
+        % call to chebfun2/surf
+        h1 = surf(f.xcheb, f.ycheb, f.zcheb, varargin{2:end}); hold on
+        
+        
+        if ( plotline )
+            LW = 'linewidth'; lw = 2; 
+            f1 = f.xcheb; f2 = f.ycheb; f3 = f.zcheb;
+            rect = f1.corners;
+            
+            x = chebpts(100,rect(1:2));
+            lft = rect(3)*ones(length(x),1);
+            h2 = plot3(f1(x,lft),f2(x,lft),f3(x,lft),'-','Color',cc{1},LW,lw);
+        
+            rght = rect(4)*ones(length(x),1);
+            h3 = plot3(f1(x,rght),f2(x,rght),f3(x,rght),'k','Color',cc{1},LW,lw);
+            
+            y = chebpts(100,rect(3:4));
+            dwn = rect(1)*ones(length(x),1);
+            h4 = plot3(f1(dwn,y),f2(dwn,y),f3(dwn,y),'k','Color',cc{1},LW,lw);
+            
+            up = rect(2)*ones(length(x),1);
+            h5 = plot3(f1(up,y),f2(up,y),f3(up,y),'k','Color',cc{1},LW,lw);
+        
+            h = [h1 h2 h3 h4 h5];
+        else 
+            h = h1; 
+        end
+    else
+        % straight call to chebfun2/surf
+        h = surf(f.xcheb, f.ycheb, f.zcheb, varargin{:});
+    end
+else
+    % straight call to chebfun2/surf
+    h = surf(f.xcheb, f.ycheb, f.zcheb, varargin{:});
 end
 
 
-% straight call to chebfun2/surf
-h = surf(f.xcheb, f.ycheb, f.zcheb, varargin{:}); 
+
+
+if ~ish
+    hold off
+end
 
 if ( nargout > 1 )
-    varargout = {h}; 
+    varargout = {h};
 end
 
 end
