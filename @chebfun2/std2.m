aabd9a725ac3cacba4a007863a66534a6557c075
townsend <townsend@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2013-03-02 21:29:13 +0000

    Adding the Chebfun2 command STD.

--- @chebfun2/std2.m
+++ @chebfun2/std2.m
@@ -3,11 +3,11 @@ function v = std2(f)
 %
 % v = STD2(F) computes the standard deviation of a chebfun2, i.e., 
 % 
-%                d  b
-%               /  /
-%  STD2(F)^2 =  |  |  |f(x,y) - m|^2 dx dy
-%               /  /
-%              c  a 
+%                             d  b
+%                             /  /
+%  STD2(F)^2 = 1/(b-a)/(d-c)  |  |  |f(x,y) - m|^2 dx dy
+%                            /  /
+%                           c  a 
 % 
 % where the domain of F is [a,b]x[c,d], and m = mean2(F). 
 %
