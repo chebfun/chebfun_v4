fe9f2d0a957a426284476c6eec748f82c893144d
kleing <kleing@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2013-08-14 16:30:59 +0000

    Updating the help text on discrete input data.

--- @chebfun/chebfun.m
+++ @chebfun/chebfun.m
@@ -8,10 +8,12 @@
 %
 % If F is a doubles array, A = [A1,A2,...,An]', the numbers A1,...,An are 
 % used as function values at n Chebyshev points of the 2nd kind, i.e. 
-% chebpts(n). If F is a matrix CHEBFUN(F) returns a chebfun 'quasimatrix', 
-% taking each column of F as function values in the same way as above.
-% CHEBFUN(F,'equi') is similar, but here the data is assumed to come from
-% an equispaced grid linspace(-1,1,n).
+% chebpts(n). CHEBFUN(F,'equi') is similar, but here the data is assumed to 
+% come from an equispaced grid linspace(-1,1,n); in this case a smooth 
+% polynomial interpolant is constructed that is derived from adaptive 
+% Floater-Hormann interpolation [Numer. Math. 107, 315-331 (2007)]. If F is 
+% a matrix CHEBFUN(F) returns a chebfun 'quasimatrix', taking each column 
+% of F as function values in the same way as above.
 %
 % CHEBFUN(F,[A B]) specifies an interval [A B] where the function is
 % defined. A and/or B may be infinite.
