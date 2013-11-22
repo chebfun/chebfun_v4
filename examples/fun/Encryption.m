ba6a26309b5ff241e4ef532fe825e07ed539e887
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2013-11-22 13:49:50 +0000

    Fixing an Example.

--- examples/fun/Encryption.m
+++ examples/fun/Encryption.m
@@ -6,38 +6,36 @@
 % [Tags: #encryption, #message]
 
 %%
-% SCRIBBLE produces piecewise linear complex chebfun whose
-% plots look like words, like this:
+% SCRIBBLE produces piecewise linear complex chebfun whose plots look like
+% words, like this:
 message = scribble('This is the message');
-LW = 'linewidth';
-plot(message,LW,2.0), axis equal
+LW = 'linewidth'; lw = 2;
+plot(message, LW, lw), axis equal
 
 %%
 % Here's another string:
 key = scribble('Aardvarks eat ants');
-plot(key,'r',LW,2.0), axis equal
+plot(key, 'r', LW, lw), axis equal
 
 %%
 % Now if we plot the sum of the two, we get nonsense:
 encrypted = message + key;
-plot(encrypted,'m',LW,2.0), axis equal
+plot(encrypted, 'm', LW, lw), axis equal
 
 %%
-% So we've invented a new encryption scheme!  For of course
-% the original message can be recovered by subtracting off
-% that key:
+% So we've invented a new encryption scheme!  For of course the original message
+% can be recovered by subtracting off that key:
 message2 = encrypted - key;
-plot(message2,LW,2.0), axis equal
+plot(message2, LW, lw), axis equal
 
 %%
-% So long as we're investigating the world's most expensive
-% and least secure method of encryption, we might as well
-% tangle up the text in the complex plane a bit too.  I'll
-% bet you can't read this:
-scrambled = exp(2i*(encrypted+1));
-plot(scrambled,'g',LW,2.0), axis equal
+% So long as we're investigating the world's most expensive and least secure
+% method of encryption, we might as well tangle up the text in the complex plane
+% a bit too.  I'll bet you can't read this:
+scrambled = exp(1.5i*(encrypted));
+plot(scrambled, 'g', LW, lw), axis equal
 
 %%
 % But we can get the message back with a little unscramble:
-message3 = unwrap(log(scrambled))/2i - 1 - key;
-plot(message3,LW,2.0), axis equal
+message3 = unwrap(log(scrambled))/1.5i - 1 - key;
+plot(message3, LW, lw), axis equal
