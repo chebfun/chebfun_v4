36024d40870a104f37a74d3e4f117b7af3d86868
hale <hale@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2014-04-11 08:04:38 +0000

    Adding new examples list.

---
+++ examples/new_examples_list.m
@@ -0,0 +1,153 @@
+new_examples({'opt','OptimInt','approx','Entire','stats','LeastSquares'})
+new_examples('copy')
+
+%%
+
+new_examples({'roots','RootsNearAxis','ode','NonstandardBCs','geom','Ellipses',2})
+new_examples('copy')
+
+%%
+
+new_examples({'roots','WhiteCurves',2,'approx','OrthPolysLanczos',1,'ode','BlackScholes',1})
+new_examples('copy')
+
+%%
+
+new_examples({'quad','GaussQuad',2,'ode-eig','ThermoelasticRod',2,'roots','RandomPolynomials',1})
+new_examples('copy')
+
+%%
+
+new_examples({'ode','ChebfunAD',2,'ode','Orbits',2,'fun','AudibleChebfuns',4})
+new_examples('copy')
+
+%%
+
+new_examples({'approx','GreedyInterp',10,'ode','JumpConditions',5,'ode','PiecewiseDemo',2})
+new_examples('copy')
+
+%%
+
+new_examples({'fun','XmasCard',1,'ode-eig','NullSpace',7,'stats','ResamplingRandomVariables',3})
+new_examples('copy')
+
+%%
+
+new_examples({'stats','MercerKarhunenLoeve',3,'stats','GeneralizedPolynomialChaos',3,'stats','StochasticCollocationBurgers',2})
+new_examples('copy')
+
+%%
+
+new_examples({'ode-eig','Eigenstates',1,'ode','ParameterODE',1,'pde','SVDFrequencyResponse',1})
+new_examples('copy')
+
+%%
+new_examples({'fun','MathJax',1,'fun','ValentinesDay',1,'roots','BernoulliPolynomials',1})
+new_examples('copy')
+
+%%
+
+new_examples({'approx','RestrictedDenominatorApproximations',4,'complex','Hyperfuns',1,'fun','Encryption',2})
+new_examples('copy')
+
+%%
+
+new_examples({'quad','QuadratureConvergence',2,'approx','ExactChebCoeffs',1,'ode','LeeGreengardODEs',5})
+new_examples('copy')
+%%
+
+new_examples({'approx','ScalingAndSquaring',2,'opt','MercuryEarth',3,'linalg','MercuryEarthConjunctions',1})
+new_examples('copy')
+
+%%
+
+new_examples({'fun','ContourFun', 7,'stats','CentralLimitTheorem',8,'approx','BSplineConv',2})
+new_examples('copy')
+
+%%
+
+new_examples({'calc','DeltaDerivs',1,'approx','AbsoluteValueScaled',2,'fun','ContourFun', 7})
+new_examples('copy')
+
+%%
+
+new_examples({'approx','CommunicationSystem',6,'ode','ResonantVandal',1,'fun','BirthdayOdds', 1})
+new_examples('copy')
+
+%%
+
+new_examples({'calc','Integrals',3,'ode','DawsonIntegral',1,'linalg','SOR', 1})
+new_examples('copy')
+
+%%
+
+new_examples({'stats','KellyCriterion',1,'roots','NewtonRaphson',2,'calc','MeanValueTheorem', 1})
+new_examples('copy')
+
+%%
+
+new_examples({'complex','RoucheTheorem',6,'complex','ComplexArcLength',3,'calc','ForTheBirds',1})
+new_examples('copy')
+
+%%
+
+new_examples({'fun','KochSnowflake',2,'approx','InteractiveInterp',1,'geom','RoundingCorners',3})
+new_examples('copy')
+
+
+%%
+new_examples({'calc', 'SurfaceRevolution', 7, 'roots','BivariateRoots',1, 'complex','ConformalSquare',5})
+new_examples('copy')
+
+%%
+new_examples({'complex','AnalyticContinuation', 3, 'ode','BouncingBall', 1, 'approx', 'Splines', 2})
+new_examples('copy')
+
+%%
+new_examples({'stats', 'ExponentialExercises', 2, 'stats', 'NormalExercises', 1,'ode', 'LorenzAttractor', 1})
+new_examples('copy')
+
+%%
+new_examples({'approx', 'Other2DDomains', 4, 'complex', 'Sums', 2, 'roots', 'ResultantMethod', 4})
+new_examples('copy')
+
+%%
+new_examples({'stats', 'UniformExercises', 4, 'complex', 'FourierContour', 7, 'stats', 'BetaExercises', 4})
+new_examples('copy')
+
+%%
+
+new_examples({'fun', 'Scribble2', 3, 'approx', 'FastChebyshevLegendreTransform', 2, 'ode', 'Logistic', 6})
+new_examples('copy')
+
+%%
+new_examples({'roots', 'FundamentalTheoremOfAlgebra', 1, 'approx', 'OscError', 6, 'stats', 'MaxwellExercises', 2})
+new_examples('copy')
+
+%%
+new_examples({'calc', 'SnellsLaw', 'approx', 'WeierstrassFunction', 1, 'approx', 'ResolutionWiggly', 4})
+new_examples('copy')
+
+%%
+new_examples({'stats', 'BayesianGradebook', 2, 'complex', 'Singularities', 2, 'ode', 'FourBugs', 3})
+new_examples('copy')
+
+%%
+
+new_examples({'opt', 'ConstrainedOptimization', 6, 'stats', 'ProbabilityConvolution', 5, 'pde', 'AllenCahn2', 4})
+new_examples('copy')
+
+%%
+    
+new_examples({'ode', 'vdPorbit', 2, 'temp', 'LejaSequences', 2, 'approx', 'FastConvWeierstrass', 4})
+new_examples('copy')
+
+%%
+
+
+
+
+
+
+
+
